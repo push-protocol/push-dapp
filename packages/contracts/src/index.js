fec439a578fe4942620b4a666f836b029d8dc6ec
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2020-04-29 00:12:20 +0530

    Initial commit from Create Eth App

---
+++ packages/contracts/src/index.js
@@ -0,0 +1,9 @@
+import erc20Abi from "./abis/erc20";
+import ownableAbi from "./abis/ownable";
+
+export const abis = {
+  erc20: erc20Abi,
+  ownable: ownableAbi,
+};
+
+export { default as addresses } from "./addresses";
---
+++ packages/react-app/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+// You should replace this uri with your own and put it into a .env file
+const client = new ApolloClient({
+  uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root"),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
