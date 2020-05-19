d154380bc6f6217bdf4f2d69cefbb33a5113702f
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2020-05-20 01:15:29 +0530

    Minor Tweaks

--- packages/react-app/src/index.js
+++ packages/react-app/src/index.js
@@ -3,19 +3,29 @@ import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 import "./index.css";
-import App from "./App";
+import App from "pages/App";
 import * as serviceWorker from "./serviceWorker";
+
+import { Web3ReactProvider } from '@web3-react/core'
+import { ethers } from "ethers";
 
 // You should replace this uri with your own and put it into a .env file
 const client = new ApolloClient({
   uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
 });
 
+function getLibrary(provider, connector) {
+  return new ethers.providers.Web3Provider(provider); // this will vary according to whether you use e.g. ethers or web3.js
+}
+
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("root"),
+
+    <ApolloProvider client={client}>
+      <Web3ReactProvider getLibrary={getLibrary}>
+      { <App /> }
+      </Web3ReactProvider>
+    </ApolloProvider>,
+    document.getElementById("root"),
 );
 
 // If you want your app to work offline and load faster, you can change
