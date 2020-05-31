b57f437f37716c9c55da87cf6357977e89b697af
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2020-05-31 06:52:16 +0530

    Contract Address Changed

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -2,7 +2,7 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
-  epnscore: "0x42d10cdd9cdc4ae284e4bec54e204dbdc44adf9a",
+  epnscore: "0x428c7640d77d8bb2dacbbd2c409c064936a3a895",
   dai: "0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108",
 };
 
