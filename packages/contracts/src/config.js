2029c3328abd57ca7e8fc57d31ab83eb5a536840
rijusougata13 <rijusougata13@gmail.com>
2022-02-28 17:59:27 +0530

    change chain id to kovan

--- packages/contracts/src/config.js
+++ packages/contracts/src/config.js
@@ -1,12 +1,12 @@
 // environmental configurations for the dapp for different environments
 const config = {
-    apiUrl: "https://backend-prod.epns.io/apis", //the right backend URL to be used
+    apiUrl: "https://backend-kovan.epns.io/apis", //the right backend URL to be used
     allowedNetworks: [
-        1, //for eth
-        137 //for polygon
+        42, //for ropsten
+        80001 //for mumbai polygon
     ],
-    coreContractChain: 1, //the chain id of the network which the core contract relies on
-    coreRPC: "https://mainnet.infura.io/v3/4ff53a5254144d988a8318210b56f47a"
+    coreContractChain: 42, //the chain id of the network which the core contract relies on
+    coreRPC: "https://kovan.infura.io/v3/4ff53a5254144d988a8318210b56f47a"
 };
 
 export default config;
