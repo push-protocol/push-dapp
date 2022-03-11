a8959ff7506397085f7986f1837dfb83aa2de7a4
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-03-11 14:40:41 +0530

    Added Coming Soon Page

--- packages/react-app/src/pages/MasterInterfacePage.tsx
+++ packages/react-app/src/pages/MasterInterfacePage.tsx
@@ -19,6 +19,7 @@ import GovernancePage from "pages/GovernancePage";
 import YieldFarmingPage from "pages/YieldFarmingPage";
 import NFTPage from "pages/NFTPage";
 import AirdropPage from "pages/AirdropPage";
+import ComingSoonPage from "pages/ComingSoonPage";
 
 import {SupportPage} from "pages/SupportPage";
 
@@ -42,6 +43,7 @@ function MasterInterfacePage() {
           <Route path="yield" element={<YieldFarmingPage />} />
           <Route path="rockstar" element={<NFTPage />} />
           <Route path="gratitude" element={<AirdropPage />} />
+          <Route path="comingsoon" element={<ComingSoonPage />} />
           <Route
               path="/"
               element={<Navigate to="/channels" />}
