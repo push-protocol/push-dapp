903c814c08917796c4ec5e60dc9e3cb36b128c88
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-04-05 12:07:47 +0530

    Verifier Name missing Issue Resolved

--- packages/react-app/src/components/ViewChannelItem.js
+++ packages/react-app/src/components/ViewChannelItem.js
@@ -555,6 +555,14 @@ function ViewChannelItem({ channelObjectProp }) {
                   bgColor={themes.viewChannelSecondaryBG}
                 />
               }
+                  
+              {verifierDetails && (
+                <Subscribers>
+                  <VerifiedBy>Verified by:</VerifiedBy>
+                  <VerifierIcon src={verifierDetails.icon} />
+                  <VerifierName>{verifierDetails.name}</VerifierName>
+                </Subscribers>
+              )}
 
             </ItemH>
           )}
@@ -777,7 +785,7 @@ const VerifierIcon = styled.img`
 
 const VerifierName = styled.span`
   font-weight: 400;
-  color: black;
+  color: ${props => props.theme.color};
   font-size: 16px;
   letter-spacing: 0em;
 `;
