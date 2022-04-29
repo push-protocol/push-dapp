8ee663de42ddc2cc32a9690f561be13f28b27225
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-04-29 17:41:17 +0530

    Redirect to channels page from notavailable if on core chain

--- packages/react-app/src/pages/NotAvailablePage.tsx
+++ packages/react-app/src/pages/NotAvailablePage.tsx
@@ -4,6 +4,7 @@ import styled, { useTheme } from 'styled-components';
 import { ThemeProvider } from "styled-components";
 import { Section, Content, Item } from 'components/SharedStyling';
 import { envConfig } from "@project/contracts";
+import { useWeb3React } from '@web3-react/core'
 
 const networkName = {
   42: "Ethereum Kovan",
@@ -12,6 +13,15 @@ const networkName = {
 
 function NotAvailablePage(props) {
   const themes = useTheme();
+  const { chainId } = useWeb3React();
+  const onCoreNetwork = chainId === envConfig.coreContractChain;
+
+  React.useEffect(() => {
+    if (onCoreNetwork) {
+      const url = window.location.origin;
+      window.location.replace(`${url}/#/channels`);
+    }
+  })
 
   // Render
   return (
