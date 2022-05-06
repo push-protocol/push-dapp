27f238923b8e68e4ebb600d8b23120287d7fa77b
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-05-06 16:50:49 +0530

    Migration optimization in search api

--- packages/react-app/src/segments/ViewChannels.tsx
+++ packages/react-app/src/segments/ViewChannels.tsx
@@ -5,6 +5,7 @@ import { Waypoint } from "react-waypoint";
 import { useDispatch, useSelector } from "react-redux";
 import { postReq } from "api";
 import { useWeb3React } from "@web3-react/core";
+import { envConfig } from "@project/contracts";
 
 import { Item, ItemH } from "components/SharedStyling";
 import { AiOutlineSearch } from "react-icons/ai";
@@ -114,12 +115,21 @@ function ViewChannels({ loadTeaser, playTeaser }) {
     if (search) {
       setLoadingChannel(true); //begin loading here
       setChannelToShow([]); //maybe remove later
-      postReq("/channels/search", {
-        query: search,
-        op: "read",
-        page : 1,
-        pageSize : 1000
-      })
+      let payloadToSearchApiObj;
+      if (envConfig.coreContractChain === 42) {
+        payloadToSearchApiObj = {
+          query: search,
+          op: "read",
+          page: 1,
+          pageSize: 1000
+        };
+      } else {
+        payloadToSearchApiObj = {
+          query: search,
+          op: "read",
+        };
+      }
+      postReq("/channels/search", payloadToSearchApiObj)
         .then((data) => {
           setChannelToShow(data.data.channels || []);
           setLoadingChannel(false);
