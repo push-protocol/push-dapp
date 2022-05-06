2f9df0af669ce054aaf9144089b9f3924e1b3737
gaurav8628 <gauravverma0803@gmail.com>
2022-05-07 04:37:25 +0530

    description limit implemented

--- packages/react-app/src/components/CreateChannel.js
+++ packages/react-app/src/components/CreateChannel.js
@@ -738,10 +738,14 @@ function CreateChannel() {
                   bg="#fff"
                   value={channelInfo}
                   onChange={(e) => {
-                    setChannelInfo(e.target.value);
+                    setChannelInfo(e.target.value.slice(0,200));
                   }}
                   autocomplete="off"
                 />
+                
+                <SpanR>
+                  {200-channelInfo.length} characters remains
+                </SpanR>
               </Item>
 
               <ItemH
@@ -856,6 +860,14 @@ function CreateChannel() {
 }
 
 // css styles
+const SpanR = styled.div`
+color: #e20880;
+opacity: 0.7;
+position: absolute;
+bottom: 0px;
+right: 0.8rem;
+z-index: 1;
+`;
 const Step = styled.div`
   height: 12px;
   width: 12px;
