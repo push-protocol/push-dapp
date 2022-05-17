a030f767010049708585b27c386e0a93b5cbaa11
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-05-17 13:00:06 +0530

    Added register public key button and functionality

--- packages/react-app/src/pages/InboxPage.js
+++ packages/react-app/src/pages/InboxPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactGA from "react-ga";
-
-import styled, { css } from "styled-components";
+import { ethers } from "ethers";
+import styled, { useTheme, ThemeProvider } from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { useWeb3React } from "@web3-react/core";
+import { Item, Button } from "components/SharedStyling";
+import { addresses, abis ,envConfig } from "@project/contracts";
 
 import NotificationToast from "components/NotificationToast";
 
@@ -14,9 +16,12 @@ import UsersDataStore from "singletons/UsersDataStore";
 
 import {
   setPushAdmin,
+  setCoreReadProvider,
+  setCommunicatorReadProvider,
 } from "redux/slices/contractSlice";
 
 import GLOBALS from "config/Globals";
+export const ALLOWED_CORE_NETWORK = envConfig.coreContractChain;
 
 // Create Header
 function InboxPage() {
@@ -24,7 +29,7 @@ function InboxPage() {
   ReactGA.pageview("/inbox");
 
   const dispatch = useDispatch();
-  const { account, chainId } = useWeb3React();
+  const { account, chainId, library } = useWeb3React();
   const {
     epnsReadProvider,
     epnsCommReadProvider,
@@ -33,6 +38,9 @@ function InboxPage() {
   // toast related section
   const [toast, showToast] = React.useState(null);
   const clearToast = () => showToast(null);
+
+  const themes = useTheme();
+  const onCoreNetwork = ALLOWED_CORE_NETWORK === chainId;
   
   //clear toast variable after it is shown
   React.useEffect(() => {
@@ -41,6 +49,34 @@ function InboxPage() {
     }
   }, [toast]);
   // toast related section
+
+  React.useEffect(() => {
+    (async function init() {
+      const coreProvider = onCoreNetwork
+        ? library
+        : new ethers.providers.JsonRpcProvider(envConfig.coreRPC)
+      
+      // inititalise the read contract for the core network
+      const coreContractInstance = new ethers.Contract(
+        addresses.epnscore,
+        abis.epnscore,
+        coreProvider
+      );
+      // initialise the read contract for the communicator function
+      const commAddress = onCoreNetwork
+        ? addresses.epnsEthComm
+        : addresses.epnsPolyComm;
+      const commContractInstance = new ethers.Contract(
+        commAddress,
+        abis.epnsComm,
+        library
+      );
+      dispatch(setCommunicatorReadProvider(commContractInstance));
+      dispatch(setCoreReadProvider(coreContractInstance));
+    })();
+  }, [account, chainId]);
+
+  let EPNS_DOMAIN;
 
   /**
    * When we instantiate the contract instances, fetch basic information about the user
@@ -72,18 +108,100 @@ function InboxPage() {
         epnsCommReadProvider,
         chainId
       );
+      EPNS_DOMAIN = {
+        name: "EPNS COMM V1",
+        chainId: chainId,
+        verifyingContract: epnsCommReadProvider?.address,
+      };
     }
   }, [epnsReadProvider, epnsCommReadProvider]);
 
+  const registerPubKey = async (encryptionPublicKey) => {
+    console.log("called", encryptionPublicKey);
+    try {
+      const type = {
+        Register: [
+          { name: "user", type: "address" },
+          { name: "encryptionKey", type: "string" },
+          { name: "action", type: "string" },
+        ],
+      };
+
+      const message = {
+        user: account,
+        encryptionKey: encryptionPublicKey,
+        action: "Register",
+      };
+
+      console.log(message, type);
+
+      const signature = await library
+        .getSigner(account)
+        ._signTypedData(EPNS_DOMAIN, type, message);
+      
+      console.log(signature);
+      
+      console.log({
+        address: account,
+        encryptionKey: encryptionPublicKey,
+        signature,
+        message,
+        op: "write",
+        chainId,
+        contractAddress: epnsCommReadProvider.address
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const enableSecretNotif = async () => {
+    if (!epnsCommReadProvider) return;
+    let encryptionPublicKey;
+    await library.provider.request({
+      method: 'eth_getEncryptionPublicKey',
+      params: [account], // you must have access to the specified account
+    }).then((result) => {
+      encryptionPublicKey = result;
+      registerPubKey(encryptionPublicKey);
+      console.log(result);
+    })
+    .catch((error) => {
+      if (error.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        console.log("User Rejected the Request to the Key");
+      } else {
+        console.error(error);
+      }
+    });
+  }
+
   // Render
   return (
-    <Container>
-    <div className="joyride"></div>
-      <Feedbox />
-      {toast && (
-        <NotificationToast notification={toast} clearToast={clearToast} />
-      )}
-    </Container>
+    <ThemeProvider theme={themes}>
+      <Container>
+        <Item>
+          <Item margin="16px 20px 0px 0px" self="self-end">
+            <Button
+              padding="12px"
+              direction="row"
+              border={`1px solid ${themes.faucetBorder}`}
+              bg={themes.scheme === "light" ? GLOBALS.COLORS.GRADIENT_PRIMARY : GLOBALS.COLORS.GRADIENT_SECONDARY}
+              radius="50px"
+              onClick={enableSecretNotif}
+            >
+              <span style={{ color: "#fff" }}>Enable Secret Notifications</span>
+              <></>
+            </Button>
+          </Item>
+        </Item>
+        <div className="joyride"></div>
+        <Feedbox />
+        {toast && (
+          <NotificationToast notification={toast} clearToast={clearToast} />
+        )}
+      </Container>
+    </ThemeProvider>
   );
 }
 
