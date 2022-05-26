81f12797671657c149bf134169855b60d468c983
harshrajat <ProjectHydro@3602652.no-reply.drupal.org>
2022-05-26 16:09:50 +0400

    Polyfill and react-script upgrade

--- packages/react-app/config-overrides.js
+++ packages/react-app/config-overrides.js
@@ -1,5 +1,22 @@
-const {override, addBabelPlugin} = require('customize-cra')
-
-module.exports = override(
-  addBabelPlugin("@babel/plugin-proposal-nullish-coalescing-operator")
-);
+module.exports = function override (config, env) {
+  console.log('override')
+  let loaders = config.resolve
+  loaders.fallback = {
+      "assert": require.resolve('assert'),
+      "child_process": false,
+      "constants": require.resolve("constants-browserify"),
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "http": require.resolve("stream-http"),
+      "https": false,
+      "url": require.resolve('url'),
+      "util": require.resolve("util/"),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "os": false,
+      "path": false
+  }
+  
+  return config
+}
