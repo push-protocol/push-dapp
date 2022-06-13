11e8ab7d62e20b42f42d22faf2ae72e6c25be7e1
strykerin <dacosta.pereirafabio@gmail.com>
2022-06-14 03:05:50 +0400

    fix post message route

--- packages/react-app/src/helpers/w2wChatHelper.ts
+++ packages/react-app/src/helpers/w2wChatHelper.ts
@@ -1,82 +1,61 @@
 import { randomBytes } from '@stablelib/random';
 import { toString } from 'uint8arrays/to-string';
 
-export const getInbox = async (Did:string)=>{
-    try {
-        const response = await fetch('http://localhost:4000/apis/w2w/inbox/did/' + Did);
-        const data: any = await response.json();
-        return data;
-    }
-    catch (err) {
-        console.log(err);
-    }
+export const getInbox = async (Did: string) => {
+    const response = await fetch('http://localhost:4000/apis/w2w/inbox/did/' + Did);
+    const data: any = await response.json();
+    return data;
 }
 
-export const postMessageToServer = async (time:number,text:string,wallet:string)=>{
-    try {
-        const response = await fetch('/api/w2w/postMessage', {
-            method: 'POST',
-            headers: {
-                "content-Type": 'application/json'
-            },
-            body: JSON.stringify({
-                time,
-                text,
-                wallet
-            })
-        });
-        if (response.status !== 201) {
-            throw new Error("something went wrong")
-        }
-        const data: any = await response.json();
-        return data;
+export const postMessage = async (fromWallet: string, fromDID: string, toDID: string, 
+    messageContent: string, messageType: string, signature: string) => {
+    const response = await fetch('http://localhost:4000/apis/w2w/messages', {
+        method: 'POST',
+        headers: {
+            "content-Type": 'application/json'
+        },
+        body: JSON.stringify({
+            fromWallet,
+            fromDID,
+            toDID,
+            messageContent,
+            messageType,
+            signature
+        })
+    });
+    if (response.status > 299) {
+        throw new Error('Error posting message')
     }
-    catch (err) {
-        console.log(err);
-    }
+    const data: any = await response.json();
+    return data;
 }
 
-export const getAllWallets = async ()=>{
-    try {
-        const response = await fetch('http://localhost:4000/apis/w2w/users');
-        const data = await response.json();
-        return data;
-    }
-    catch (err) {
-        console.error(err);
-    }
+export const getAllWallets = async () => {
+    const response = await fetch('http://localhost:4000/apis/w2w/users');
+    const data = await response.json();
+    return data;
 }
 
 export const createUser = async (did: string, publicKey: string, encryptedPrivateKey: string) => {
-    try {
-        const response = await fetch('http://localhost:4000/apis/w2w/user', {
-            method: 'POST',
-            headers: {
-                "content-Type": 'application/json'
-            },
-            body: JSON.stringify({
-                did,
-                publicKey,
-                encryptedPrivateKey
-            })
-        });
-        const data = await response.json();
-        return data;
-    }
-    catch (err) {
-        console.error(err);
-    }
+    const response = await fetch('http://localhost:4000/apis/w2w/user', {
+        method: 'POST',
+        headers: {
+            "content-Type": 'application/json'
+        },
+        body: JSON.stringify({
+            did,
+            publicKey,
+            encryptedPrivateKey
+        })
+    });
+    const data = await response.json();
+    return data;
 }
 
 export const getKeys = async (Did: string) => {
-    try {
-        const response = await fetch('http://localhost:4000/apis/w2w/keys/did/' + Did);
-        const data: any = await response.json();
-        return data;
-    }
-    catch (err) {
-        console.log(err);
-    }
+    const response = await fetch('http://localhost:4000/apis/w2w/keys/did/' + Did);
+    const data: any = await response.json();
+    return data;
 }
 
 export function randomString() {
