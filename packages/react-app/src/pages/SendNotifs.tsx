2f38eb8c61cc9891b1221e196c58b22abc25979d
Arpitha Rajeev <105787510+Arpitha-Rajeev@users.noreply.github.com>
2022-07-29 18:51:08 +0530

    Update SendNotifs.tsx

--- packages/react-app/src/pages/SendNotifs.tsx
+++ packages/react-app/src/pages/SendNotifs.tsx
@@ -1,20 +1,18 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import { useSelector } from "react-redux";
 import SendNotifications from "components/SendNotifications";
 import { useWeb3React } from "@web3-react/core";
 import { envConfig } from "@project/contracts";
-import { NotificationContext } from "contexts/NotificationContext";
 
 function SendNotifs() {
-  const { channelDetails, delegatees } = useSelector((state) => state.admin);
-  
-  const aliasVerified = useContext(NotificationContext)
+  const { channelDetails, delegatees, aliasVerified } = useSelector((state) => state.admin);
+
   const { chainId } = useWeb3React();
   const CORE_CHAIN_ID = envConfig.coreContractChain;
   const onCoreNetwork = CORE_CHAIN_ID === chainId;
   return (
     <>
-    {(channelDetails && ((!onCoreNetwork && aliasVerified) || onCoreNetwork)) || delegatees?.length ? <SendNotifications /> : "Please create a channel to send notifications or become a delegate to any other channel"}
+    {(channelDetails && ((!onCoreNetwork && aliasVerified) || onCoreNetwork)) || delegatees?.length ? <SendNotifications /> : "Create"}
     </>
   )
 }
