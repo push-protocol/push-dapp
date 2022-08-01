3450543a1f7c6381f2bdc35bad4ef980ac64869a
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-08-01 13:40:06 +0530

    Added getCAIP function in helpers

---
+++ packages/react-app/src/helpers/UtilityHelper.ts
@@ -0,0 +1,39 @@
+import { envConfig } from "@project/contracts";
+
+// Utility Helper Functions
+const UtilityHelper = {
+  isMainnet : (chainId: number) => {
+    if (chainId === 1 || chainId === 137) {
+      return true;
+    }
+  },
+}
+
+// Disable the browser notification on Metamask iphone mobile
+export const isUserAgentIphone = (userAgent) =>{
+  return userAgent.indexOf("iPhone") !== -1
+}
+
+export const Eip155EnableIds = [137, 80001];
+
+type CAIPProps = {
+  chainId: number;
+  address: string;
+}
+
+// return caip obj from chainId and address
+export const getCAIP = ({ chainId, address }: CAIPProps) => {
+  if (chainId === envConfig.coreContractChain) return {};
+
+  if (Eip155EnableIds.includes(chainId)) {
+    const caip = 'eip155:' + chainId;
+
+    return {
+      [caip]: address
+    }
+  } else {
+    return {};
+  }
+}
+
+export default UtilityHelper;
