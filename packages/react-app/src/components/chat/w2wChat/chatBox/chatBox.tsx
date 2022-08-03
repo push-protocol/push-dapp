dfcb2f4549c1f1ecf147b39ef65564b1658b5d6a
strykerin <dacosta.pereirafabio@gmail.com>
2022-08-03 13:10:25 +0400

    fix indentation

--- packages/react-app/src/components/chat/w2wChat/chatBox/chatBox.tsx
+++ packages/react-app/src/components/chat/w2wChat/chatBox/chatBox.tsx
@@ -121,7 +121,9 @@ const ChatBox = (): JSX.Element => {
       setLoading(false)
     }
   }
+
   const { data, error, isError, isLoading } = useQuery<any>('current', getMessagesFromIPFS, { refetchInterval: 5000 })
+
   useEffect(() => {
     function updateData(): void {
       if (data !== undefined && currentChat?.wallets) {
