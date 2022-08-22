dbe459a93b3b3a36fb15ca9c68943a4e54fda2da
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-08-22 14:48:50 +0530

    Added IPFS upload helper function

--- packages/react-app/src/helpers/UtilityHelper.js
+++ packages/react-app/src/helpers/UtilityHelper.js
@@ -1,3 +1,5 @@
+import ipfsClient from 'ipfs-http-client';
+
 // Utility Helper Functions
 const UtilityHelper = {
   isMainnet : (chainId) => {
@@ -5,6 +7,26 @@ const UtilityHelper = {
       return true;
     }
   },
+}
+
+export const IPFSupload = async (input) => {
+  input = Buffer.from(input);
+  const projectId = process.env.REACT_APP_IPFS_PROJECT_ID;
+  const projectSecret = process.env.REACT_APP_IPFS_PROJECT_SECRET;
+  const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+
+  const client = ipfsClient({
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    apiPath: '/api/v0/add',
+    headers: {
+      authorization: auth
+    }
+  });
+
+  const storagePointer = await client.add(input, {pin: true});
+  return storagePointer[0]['hash'];
 }
 
 // Disable the browser notification on Metamask iphone mobile
