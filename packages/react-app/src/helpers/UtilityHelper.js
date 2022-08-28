e7852d927379a5c16393c7beb5d81279f261d34b
harshrajat <harsh@epns.io>
2022-08-28 04:29:22 +0400

    Merged with some TSX Errors

--- packages/react-app/src/helpers/UtilityHelper.js
+++ packages/react-app/src/helpers/UtilityHelper.js
@@ -7,4 +7,26 @@ const UtilityHelper = {
   },
 }
 
+const envUtil = {
+  prod: "app.epns.io",
+  staging: "staging.epns.io",
+  dev: "dev.epns.io",
+  localhost: "localhost"
+}
+
+export const EnvHelper = {
+  isProd: window.location.hostname === envUtil['prod'],
+  isStaging: window.location.hostname === envUtil['staging'],
+  isDev: window.location.hostname === envUtil['dev'],
+  isLocalHost: window.location.hostname === envUtil['localhost'],
+  isProduction() {
+    return (this.isProd || this.isStaging || this.isDev);
+  }
+};
+
+// Disable the browser notification on Metamask iphone mobile
+export const isUserAgentIphone = (userAgent) =>{
+  return userAgent.indexOf("iPhone") !== -1
+}
+
 export default UtilityHelper;
