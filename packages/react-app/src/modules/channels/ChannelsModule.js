45f259b98bf08727d67bdcfbdf3a92a189813eeb
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-09-16 14:25:35 +0530

    Removed unnecessary code from channelsModule

--- packages/react-app/src/modules/channels/ChannelsModule.js
+++ packages/react-app/src/modules/channels/ChannelsModule.js
@@ -1,89 +1,25 @@
-import { abis, addresses, envConfig } from "@project/contracts";
-import { useWeb3React } from "@web3-react/core";
-import { getReq } from "api";
-import AliasVerificationodal from "components/AliasVerificationModal";
-import GLOBALS, { device, globalsMargin } from "config/Globals";
-import { ethers } from "ethers";
-import { convertAddressToAddrCaip } from "helpers/CaipHelper";
-import EPNSCoreHelper from "helpers/EPNSCoreHelper";
-import hex2ascii from "hex2ascii";
-import NotificationToast from "primaries/NotificationToast";
+// React + Web3 Essentials
 import React from "react";
-import ReactGA from "react-ga";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  setCanVerify,
-  setDelegatees, setUserChannelDetails
-} from "redux/slices/adminSlice";
-import {
-  setCommunicatorReadProvider,
-  setCommunicatorWriteProvider, setCoreReadProvider,
-  setCoreWriteProvider, setPushAdmin
-} from "redux/slices/contractSlice";
-import { addNewNotification } from "redux/slices/notificationSlice";
-import ChannelOwnerDashboard from "segments/ChannelOwnerDashboard";
-import Feedbox from "segments/Feedbox";
-import Info from "segments/Info";
-import ViewChannels from "segments/ViewChannels";
-import ChannelsDataStore from "singletons/ChannelsDataStore";
-import UsersDataStore from "singletons/UsersDataStore";
+import ReactGA from "react-ga";;
+
+// External Packages
 import styled from "styled-components";
 
-export const ALLOWED_CORE_NETWORK = envConfig.coreContractChain; //chainId of network which we have deployed the core contract on
-const CHANNEL_TAB = 1; //Default to 1 which is the channel tab
+// Internal Components
+import ViewChannels from "segments/ViewChannels";
+    
+// Internal Configs
+import GLOBALS, { device, globalsMargin } from "config/Globals";
 
 // Create Channels Module
 const ChannelsModule = ({ loadTeaser, playTeaser }) => {
   ReactGA.pageview("/channels");
 
-  const dispatch = useDispatch();
-  const { account, library, chainId } = useWeb3React();
-  const {
-    epnsReadProvider,
-    epnsWriteProvider,
-    epnsCommReadProvider,
-  } = useSelector((state) => state.contracts);
-
-  const onCoreNetwork = ALLOWED_CORE_NETWORK === chainId;
-  const INITIAL_OPEN_TAB = CHANNEL_TAB; //if they are not on a core network.redirect then to the notifications page
-
-  const [controlAt, setControlAt] = React.useState(1);
-  const [modalOpen, setModalOpen] = React.useState(false);
-  const [adminStatusLoaded, setAdminStatusLoaded] = React.useState(false);
-  const [aliasVerified, setAliasVerified] = React.useState(null); // null means error, false means unverified and true means verified
-
-  // toast related section
-  const [toast, showToast] = React.useState(null);
-  const clearToast = () => showToast(null);
-
-  //clear toast variable after it is shown
-  React.useEffect(() => {
-    if (toast) {
-      clearToast();
-    }
-  }, [toast]);
-  // toast related section
-
-
-
   // Render
   return (
     <Container>
       <Interface>
-        {controlAt == 0 && <Feedbox />}
-        {controlAt == 1 && <ViewChannels loadTeaser={loadTeaser} playTeaser={playTeaser} />}
-        {controlAt == 2 && adminStatusLoaded && <ChannelOwnerDashboard />}
-        {controlAt == 3 && <Info />}
-        {toast && (
-          <NotificationToast notification={toast} clearToast={clearToast} />
-        )}
-        {modalOpen && (
-          <AliasVerificationodal
-            onClose={() => setModalOpen(false)}
-            onSuccess={() => setAliasVerified(true)}
-            verificationStatus={aliasVerified}
-          />
-        )}
+        <ViewChannels loadTeaser={loadTeaser} playTeaser={playTeaser} />
       </Interface>
     </Container>
   );
