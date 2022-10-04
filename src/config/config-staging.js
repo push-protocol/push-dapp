71c7c58fb765d6bb7d7ed541f8919b1c14afe502
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-10-04 14:53:09 +0530

    Staging goerli config changes

--- src/config/config-staging.js
+++ src/config/config-staging.js
@@ -22,15 +22,16 @@ export const config = {
    * Allowed Networks
    */
   allowedNetworks: [
-    42, //for ropsten
+    // 42, //for kovan
+    5, // for goerli
     80001, //for mumbai polygon
   ],
 
   /** 
    * Core Network Related Data
    */
-  coreContractChain: 42, //the chain id of the network which the core contract relies on
-  coreRPC: 'https://kovan.infura.io/v3/4ff53a5254144d988a8318210b56f47a',
+  coreContractChain: 5, //the chain id of the network which the core contract relies on
+  coreRPC: 'https://goerli.infura.io/v3/4ff53a5254144d988a8318210b56f47a',
   mainnetCoreRPC: "https://mainnet.infura.io/v3/4ff53a5254144d988a8318210b56f47a",
   mainnetCoreContractChain: 1,
   infuraApiUrl: 'https:/infura-ipfs.io/ipfs/',
@@ -67,22 +68,22 @@ export const config = {
  */
 export const addresses = {
   ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
-  epnscore: "0x97D7c5f14B8fe94Ef2b4bA589379f5Ec992197dA",
-  epnsEthComm: "0x87da9Af1899ad477C67FeA31ce89c1d2435c77DC", // the core contract
+  epnscore: "0xd4E3ceC407cD36d9e3767cD189ccCaFBF549202C",
+  epnsEthComm: "0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa", // the core contract
   epnsPolyComm: "0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa", // the ploygon comm contract
-  dai: "0xff795577d9ac8bd7d90ee22b6c1703490b6512fd", //address for dai
-  aDai: "0x58AD4cB396411B691A9AAb6F74545b2C5217FE6a",
+  dai: "0x75Ab5AB1Eef154C0352Fc31D2428Cef80C7F8B33", //address for dai
+  aDai: "0x31f30d9A5627eAfeC4433Ae2886Cf6cc3D25E772",
   staking: "0x8e129EF695F8761922F1AA38588292f8d8817bA5",
   yieldFarmPUSH: "0xb41cd6760d3b1d0d216851f63fda9573b04dd46e",
   yieldFarmLP: "0x604f629A6b5E85805D57f39493BBC2856cE8A540",
-  epnsToken: "0x9CE54E83c18f0a797F823905A3ec5CfC2F6d2CA9",
+  epnsToken: "0x2b9bE9259a4F5Ba6344c1b1c07911539642a2D33",
   epnsLPToken: "0x1f094Bd8c260D0C3fC79157096015990885af4d2",
   rockstar: "0x3f8C2152b79276b78315CAF66cCF951780580A8a",
   batchMintNFT: "0xCf733D547B9f7F87c1B1f63f45c197C007F8eedf",
   NFTRewards: "0xc4708BB6EC3B797344f123126171302e4e3E68E2",
   distributor: "0x5B548D33874DeABaC2F683A6E1864795E09932F0",
   uniswapV2Router02: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-  WETHAddress: "0xd0A1E359811322d97991E03f863a0C30C2cF029C",
+  WETHAddress: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
   USDTAddress: "0x13512979ade267ab5100878e2e0f485b568328a4",
   rockstarV2:"0xA2b885e7065EA59a3251489715ca80DE5Ff642f8",//mainnet address
   NFTRewardsV2: "0xdc66567a990B7fa10730459537620857c9e03287", //mainnet address
