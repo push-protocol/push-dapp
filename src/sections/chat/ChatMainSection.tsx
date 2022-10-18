4e3deba932643e476a6618a54578f4bf75ced2d3
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-10-18 13:42:43 +0530

    Replaced all occurrences of blockedLoading and its reducer

--- src/sections/chat/ChatMainSection.tsx
+++ src/sections/chat/ChatMainSection.tsx
@@ -2,6 +2,7 @@
 import { useWeb3React } from '@web3-react/core';
 import { ethers } from 'ethers';
 import React, { useContext, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 // External Packages
 import { ThreeIdConnect } from '@3id/connect';
@@ -34,6 +35,7 @@ import VideoCallSection, { VideoCallInfoI } from 'sections/video/VideoCallSectio
 // Internal Configs
 import GLOBALS, { device } from 'config/Globals';
 import CryptoHelper from 'helpers/CryptoHelper';
+import { setBlockedLoading } from 'redux/slices/chatSlice';
 
 export interface InboxChat {
   name: string;
@@ -84,7 +86,7 @@ export interface AppContext {
   setHasUserBeenSearched: (searched: boolean) => void;
   loadingMessage: string;
   setLoadingMessage: (loadingMessage: string) => void;
-  setBlockedLoading: (blockedLoading: BlockedLoadingI) => void;
+  // setBlockedLoading: (blockedLoading: BlockedLoadingI) => void;
   activeTab: number;
   setActiveTab: (active: number) => void;
   userShouldBeSearched: boolean;
@@ -109,16 +111,17 @@ const ChatMainSection = () => {
   const { connector, account, chainId, library } = useWeb3React<ethers.providers.Web3Provider>();
 
   const theme = useTheme();
+  const dispatch = useDispatch();
 
   const [viewChatBox, setViewChatBox] = useState<boolean>(false);
   const [currentChat, setCurrentChat] = useState<Feeds>();
   const [receivedIntents, setReceivedIntents] = useState<Feeds[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [loadingMessage, setLoadingMessage] = useState<string>('');
-  const [blockedLoading, setBlockedLoading] = useState<BlockedLoadingI>({
-    enabled: false,
-    title: null,
-  });
+  // const [blockedLoading, setBlockedLoading] = useState<BlockedLoadingI>({
+  //   enabled: false,
+  //   title: null,
+  // });
   const [user, setUser] = useState();
   const [did, setDID] = useState<DID>();
   const [searchedUser, setSearchedUser] = useState<string>('');
@@ -129,6 +132,8 @@ const ChatMainSection = () => {
   const [hasUserBeenSearched, setHasUserBeenSearched] = useState<boolean>(false);
   const [activeTab, setCurrentTab] = useState<number>(0);
   const [userShouldBeSearched, setUserShouldBeSearched] = useState<boolean>(false);
+
+  const { blockedLoading } = useSelector((state:any) => state.chat);
 
   const chatBoxToast = useToast();
   const queryClient = new QueryClient({});
@@ -183,13 +188,13 @@ const ChatMainSection = () => {
 
   const connectUser = async (): Promise<void> => {
     // Getting User Info
-    setBlockedLoading({
+    dispatch(setBlockedLoading({
       enabled: true,
       title: 'Step 1/4: Getting Account Info',
       progressEnabled: true,
       progress: 25,
       progressNotice: 'Reminder: Push Chat is in alpha, you might need to sign a decrypt transaction to continue',
-    });
+    }));
 
     const caip10: string = w2wHelper.walletToCAIP10({ account, chainId });
     const user: User = await PushNodeClient.getUser({ caip10 });
@@ -234,13 +239,13 @@ const ChatMainSection = () => {
       };
     }
 
-    setBlockedLoading({
+    dispatch(setBlockedLoading({
       enabled: false,
       title: "Step 4/4: Let's Chat ;)",
       spinnerType: LOADER_SPINNER_TYPE.COMPLETED,
       progressEnabled: true,
       progress: 100,
-    });
+    }));
 
     setConnectedUser(connectedUser);
     setIsLoading(false);
@@ -294,7 +299,7 @@ const ChatMainSection = () => {
               setHasUserBeenSearched,
               loadingMessage,
               setLoadingMessage,
-              setBlockedLoading,
+              // setBlockedLoading,
               activeTab,
               setActiveTab,
               userShouldBeSearched,
