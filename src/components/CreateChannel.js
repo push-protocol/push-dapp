3b54ae5a64bb213e27d6d454e0c2f8f8b266c3f9
Nilesh Gupta <guptanilesh2312@gmail.com>
2023-01-04 19:01:16 +0530

    Changed config for push token contract address

--- src/components/CreateChannel.js
+++ src/components/CreateChannel.js
@@ -49,7 +49,7 @@ function CreateChannel() {
   const [channelURL, setChannelURL] = React.useState('');
   const [channelFile, setChannelFile] = React.useState(undefined);
   const [channelStakeFees, setChannelStakeFees] = React.useState(minStakeFees);
-  const [daiAmountVal, setDaiAmountVal] = useState('');
+  const [pushTokenAmountVal, setPushTokenAmountVal] = useState('');
   const [txStatus, setTxStatus] = useState(2);
   const [progress, setProgress] = React.useState(0);
   const [progressInfo, setProgressInfo] = React.useState('');
@@ -66,18 +66,18 @@ function CreateChannel() {
   //checking DAI for user
   React.useEffect(() => {
     if (!onCoreNetwork) return;
-    const checkDaiFunc = async () => {
-      let checkDaiAmount = new ethers.Contract(addresses.dai, abis.dai, library);
+    const checkPushTokenApprovalFunc = async () => {
+      let checkPushTokenApprovedAmount = new ethers.Contract(addresses.pushToken, abis.pushToken, library);
 
-      let value = await checkDaiAmount.allowance(account, addresses.epnscore);
+      let value = await checkPushTokenApprovedAmount.allowance(account, addresses.epnscore);
       value = value?.toString();
       const convertedVal = ethers.utils.formatEther(value);
-      setDaiAmountVal(convertedVal);
+      setPushTokenAmountVal(convertedVal);
       if (convertedVal >= minStakeFees) {
         setChannelStakeFees(convertedVal);
       }
     };
-    checkDaiFunc();
+    checkPushTokenApprovalFunc();
   }, []);
 
   // timer
@@ -209,14 +209,14 @@ function CreateChannel() {
     var signer = library.getSigner(account);
     console.log(signer);
 
-    let daiContract = new ethers.Contract(addresses.dai, abis.erc20, signer);
+    let pushTokenContract = new ethers.Contract(addresses.pushToken, abis.pushToken, signer);
 
     // Pick between 50 DAI AND 25K DAI
     const fees = ethers.utils.parseUnits(channelStakeFees.toString(), 18);
 
     try {
-      if (daiAmountVal < 50.0) {
-        var sendTransactionPromise = daiContract.approve(addresses.epnscore, fees);
+      if (pushTokenAmountVal < 50.0) {
+        var sendTransactionPromise = pushTokenContract.approve(addresses.epnscore, fees);
         const tx = await sendTransactionPromise;
 
         console.log(tx);
@@ -235,7 +235,7 @@ function CreateChannel() {
 
       setProgress(50);
 
-      const tx = await contract.createChannelWithFees(channelType, identityBytes, fees, {
+      const tx = await contract.createChannelWithPUSH(channelType, identityBytes, fees, 0, {
         gasLimit: 1000000,
       });
 
