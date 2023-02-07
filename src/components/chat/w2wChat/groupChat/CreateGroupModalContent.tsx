b511112c42c0a3b1ce7ab9e5ec66798011a89f76
deekshithmd <deekshithmogra@gmail.com>
2023-02-07 23:06:48 +0530

    added content

--- src/components/chat/w2wChat/groupChat/CreateGroupModalContent.tsx
+++ src/components/chat/w2wChat/groupChat/CreateGroupModalContent.tsx
@@ -11,13 +11,20 @@ import ModalInput from 'primaries/SharedModalComponents/ModalInput';
 import ModalConfirmButton from 'primaries/SharedModalComponents/ModalConfirmButton';
 import { ModalInnerComponentType } from 'hooks/useModalBlur';
 import { ItemHV2, ItemVV2, SpanV2 } from 'components/reusables/SharedStylingV2';
+import { TextField } from 'components/SharedStyling';
+import { ReactComponent as AddGroupIcon } from 'assets/chat/group-chat/creategroupicon.svg';
 
 export const CreateGroupModalContent = ({ onClose, onConfirm: createGroup, toastObject }: ModalInnerComponentType) => {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
-  const subgraphIdInputRef = React.useRef<HTMLInputElement>();
-  const pollTimeInputRef = React.useRef<HTMLInputElement>();
+  const groupNameInputRef = React.useRef<HTMLInputElement>();
+  const groupDescriptionInputRef = React.useRef<HTMLInputElement>();
+  const fileUploadInputRef = React.useRef<HTMLInputElement>();
 
   const handleClose = () => onClose();
+
+  const handleUpload=(e)=>{
+    fileUploadInputRef.current.click()
+  }
 
   // to close the modal upon a click on backdrop
   const containerRef = React.useRef(null);
@@ -25,17 +32,19 @@ export const CreateGroupModalContent = ({ onClose, onConfirm: createGroup, toast
 
   return (
     <ModalContainer ref={containerRef}>
-      <ModalHeader
-        heading="Create Group"
-        subHeading=""
-      />
+      <SpanV2 fontWeight="500" fontSize="24px" margin="0px 0px 0px 42px">Create Group</SpanV2>
+      <GroupIconContainer>
+      <AddGroupIcon onClick={handleUpload}/>
+      <FileInput type="file" ref={fileUploadInputRef}/>
+      </GroupIconContainer>
+      
       <ModalInput
-        ref={subgraphIdInputRef}
+        ref={groupNameInputRef}
         title="Group Name"
         focusBorder="2px solid #FFDBF0"
       />
       <ModalInput
-        ref={pollTimeInputRef}
+        ref={groupDescriptionInputRef}
         title="Group Description"
         focusBorder="2px solid #FFDBF0"
       />
@@ -43,16 +52,20 @@ export const CreateGroupModalContent = ({ onClose, onConfirm: createGroup, toast
         <ItemVV2
           border="1px solid black"
           borderRadius="12px 0px 0px 12px"
+          padding="8px"
+          hover="#C2CBDB"
         >
-          <SpanV2>Public</SpanV2>
-          <SpanV2>Chats are not encrypted</SpanV2>
+          <SpanV2 fontWeight="500" fontSize="18px">Public</SpanV2>
+          <SpanV2 fontWeight="400" fontSize="12px">Chats are not encrypted</SpanV2>
         </ItemVV2>
         <ItemVV2
           border="1px solid black"
           borderRadius="0px 12px 12px 0px"
+          padding="8px"
+          hover="#C2CBDB"
         >
-          <SpanV2>Private</SpanV2>
-          <SpanV2>Chats are encrypted</SpanV2>
+          <SpanV2 fontWeight="500" fontSize="18px">Private</SpanV2>
+          <SpanV2 fontWeight="400" fontSize="12px">Chats are encrypted</SpanV2>
         </ItemVV2>
       </ItemHV2>
       <ModalConfirmButton
@@ -72,3 +85,13 @@ const ModalContainer = styled.div`
   box-sizing: border-box;
   margin: 6% 1%;
 `;
+
+const GroupIconContainer=styled.div`
+  width:100%;
+  display:flex;
+  justify-content:center;
+  `;
+
+  const FileInput=styled.input`
+  display:none;
+  `
