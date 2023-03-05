1326c77fd46be254bd4f176e81749f37a3b8f7ef
Abhishek <abhish3k@Abhisheks-MacBook-Air.local>
2023-03-05 10:02:42 +0530

    Channel Deactivation UI issues are resolved

--- src/components/ChannelDeactivateModalContent.tsx
+++ src/components/ChannelDeactivateModalContent.tsx
@@ -13,15 +13,17 @@ import styled, { ThemeProvider, useTheme } from "styled-components";
 import { ModalInnerComponentType } from "hooks/useModal";
 import { setUserChannelDetails } from "redux/slices/adminSlice";
 import LoaderSpinner, { LOADER_TYPE } from 'components/reusables/loaders/LoaderSpinner';
+import CloseButtonSvg from "../assets/XCircle.svg";
+import BellIconSvg from "../assets/BellIcon.svg";
 
-const ChannelDeactivateModalContent = ({ onConfirm, onClose, toastObject } : ModalInnerComponentType)=>{
+const ChannelDeactivateModalContent = ({ onConfirm, onClose, toastObject }: ModalInnerComponentType) => {
     const themes = useTheme();
 
     const dispatch = useDispatch();
     const { channelDetails } = useSelector((state: any) => state.admin);
     const {
         CHANNNEL_DEACTIVATED_STATE,
-      } = useSelector((state: any) => state.channels);
+    } = useSelector((state: any) => state.channels);
 
     const [isLoading, setIsLoading] = React.useState(false);
 
@@ -31,87 +33,105 @@ const ChannelDeactivateModalContent = ({ onConfirm, onClose, toastObject } : Mod
     const containerRef = React.useRef(null);
     useClickAway(containerRef, () => handleClose())
 
-    const handleConfirm = ()=>{
+    const handleConfirm = () => {
         setIsLoading(true);
 
         onConfirm()
-        .then(async (tx: any) => {
-            console.log(tx);
-            console.log("Transaction Sent!");
-            
-            toastObject.showMessageToast({
-                toastTitle:"Channel Deactivated", 
-                toastMessage:"Please Activate Channel to Send Notifications from it", 
-                toastType:  "ERROR", 
-                getToastIcon: (size) => <MdError size={size} color="red" />
-            });
+            .then(async (tx: any) => {
+                console.log(tx);
+                console.log("Transaction Sent!");
 
-            await tx.wait(1);
-            console.log("Transaction Mined!");
-            dispatch(
-                setUserChannelDetails({
-                    ...channelDetails,
-                    channelState: CHANNNEL_DEACTIVATED_STATE,
-                })
+                toastObject.showMessageToast({
+                    toastTitle: "Channel Deactivated",
+                    toastMessage: "Please Activate Channel to Send Notifications from it",
+                    toastType: "ERROR",
+                    getToastIcon: (size) => <MdError size={size} color="red" />
+                });
+
+                await tx.wait(1);
+                console.log("Transaction Mined!");
+                dispatch(
+                    setUserChannelDetails({
+                        ...channelDetails,
+                        channelState: CHANNNEL_DEACTIVATED_STATE,
+                    })
                 );
-                
-            setIsLoading(false);
-            onClose();
-          })
-          .catch((err: any) => {
-            console.log("!!!Error deactivateChannel() --> %o", err);
-            console.log({
-              err,
-            });
 
-            toastObject.showMessageToast({
-                toastTitle:"Transaction Failed", 
-                toastMessage: "Channel deactivation failed.", 
-                toastType:  "ERROR", 
-                getToastIcon: (size) => <MdError size={size} color="red" />
+                setIsLoading(false);
+                onClose();
             })
+            .catch((err: any) => {
+                console.log("!!!Error deactivateChannel() --> %o", err);
+                console.log({
+                    err,
+                });
 
-            setIsLoading(false);
-          })
+                toastObject.showMessageToast({
+                    toastTitle: "Transaction Failed",
+                    toastMessage: "Channel deactivation failed.",
+                    toastType: "ERROR",
+                    getToastIcon: (size) => <MdError size={size} color="red" />
+                })
+
+                setIsLoading(false);
+            })
     }
 
-    return(
+    return (
         <ThemeProvider theme={themes}>
             <ModalContainer ref={containerRef}>
                 <ModalMessage style={{
-                    color:themes.modalMessageColor,
+                    color: themes.modalMessageColor,
                 }}>
                     Are you sure you want to deactivate the channel? You will no longer be able to send notifications from it.
                 </ModalMessage>
                 <ButtonContainer>
-                    <IconButton
+                    {/* <IconButton
                         onClick={handleClose}
                         style={{ padding: "0", marginRight: "0.5rem" }}
                         sx={{ "&:hover": { backgroundColor: "transparent" } }}
                         children={
                             <MdHighlightOff size="2.6rem" style={{
-                                color:themes.modalIconColor,
+                                color: themes.modalIconColor,
                             }} />}
-                    />
-                    <IconButton
+                    /> */}
+
+
+                    <CloseButtonSVG src={CloseButtonSvg} onClick={handleClose} />
+
+                    {/* <IconButton
                         onClick={handleConfirm}
                         style={{ padding: "0" }}
-                        sx={{ "&:hover": { backgroundColor: "transparent" } }}
+                        sx={{ "&:hover": { backgroundColor: "#E93636" } }}
                         children={
-                            isLoading 
-                            ?
-                            <div style={{
-                                height:"34px", width:"34px", background: "red", borderRadius:"50%", padding:"3px"
+                            isLoading
+                                ?
+                                <div style={{
+                                    height: "34px", width: "34px", background: "red", borderRadius: "50%", padding: "3px"
                                 }}>
-                                <LoaderSpinner type={LOADER_TYPE.SEAMLESS} spinnerSize={34} />
-                            </div> 
-                            :
-                            <RiNotificationOffLine size="2.2rem" style={{
-                                color: "white", background: "red", borderRadius: "50%", padding: "8px"
-                            }}
-                            />
+                                    <LoaderSpinner type={LOADER_TYPE.SEAMLESS} spinnerSize={34} />
+                                </div>
+                                :
+                                // <RiNotificationOffLine size="2.2rem" style={{
+                                //     color: "white", background: "red", borderRadius: "50%", padding: "8px"
+                                // }}
+                                // />
+                                <BellIconImage src={BellIconSvg} />
                         }
-                    />
+                    /> */}
+
+                    <BellIconContainer onClick={handleConfirm}>
+                        {isLoading ? (
+                            <>
+                                <LoaderSpinner type={LOADER_TYPE.SEAMLESS} spinnerSize={30} spinnerColor={"#ffff"} />
+                            </>
+                        ) : (
+                        <BellIconImage src={BellIconSvg} />
+
+                        )}
+                    </BellIconContainer>
+
+
                 </ButtonContainer>
             </ModalContainer>
         </ThemeProvider>
@@ -119,21 +139,56 @@ const ChannelDeactivateModalContent = ({ onConfirm, onClose, toastObject } : Mod
 }
 
 const ModalContainer = styled.div`
-    width: 28vw;
+    // width: 28vw;
+    width:446px;
     display: flex;
     justify-content: space-between;
+    padding:5px 10px;
+
+    @media (max-width:500px){
+        width:350px;
+    }
+
 `
 
 const ModalMessage = styled.div`
-    width: 70%;
-    font-size: 0.95rem;
-    font-weight: 500;
-    line-height: 20px;
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 21px;
     text-align: left;
+    margin-right:40px;
+    @media (max-width:500px){
+        margin-right:20px;
+    }
 `
+
+const CloseButtonSVG = styled.img`
+    cursor:pointer;
+    width:36px;
+    height:36px;
+    margin-right:5px;
+`;
 
 const ButtonContainer = styled.div`
     display: flex;
+    justify-content: center;
+    align-items: center;
 `
+
+const BellIconContainer = styled.div`
+    width: 36px;
+    height: 36px;
+    background: #E93636;
+    display: flex;
+    justify-content: center;
+    border-radius:50%;
+    cursor:pointer;
+`
+
+const BellIconImage = styled.img`
+    height: 36px;
+    width: 17px;
+   
+`;
 
 export default ChannelDeactivateModalContent
