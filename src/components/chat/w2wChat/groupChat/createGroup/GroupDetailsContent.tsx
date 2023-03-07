67e6f915b68b62f1c6054c74b4f0494991618f84
deekshithepns <deekshith@epns.io>
2023-03-07 17:01:52 +0530

    added automatic image resizing

--- src/components/chat/w2wChat/groupChat/createGroup/GroupDetailsContent.tsx
+++ src/components/chat/w2wChat/groupChat/createGroup/GroupDetailsContent.tsx
@@ -15,6 +15,7 @@ import { isLengthValid } from 'helpers/ValidationHelper';
 import ErrorMessage from 'components/reusables/errorMessageLabel/errorMessageLabel';
 import { appConfig } from 'config';
 import { device } from 'config/Globals';
+import AutoImageClipper from 'primaries/AutoImageClipper';
 
 export const GroupDetailsContent = ({
   groupNameData,
@@ -27,6 +28,8 @@ export const GroupDetailsContent = ({
   handleGroupTypeObject,
   handleCreateGroupState,
 }) => {
+  const [imageSrc, setImageSrc] = React.useState();
+  const [isImageUploaded, setIsImageUploaded] = React.useState<boolean>(false)
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const fileUploadInputRef = React.useRef<HTMLInputElement>();
   const [errorInfo, setErrorInfo] = React.useState<{ name: string; description: string }>({
@@ -51,14 +54,31 @@ export const GroupDetailsContent = ({
 
   const themes = useTheme();
 
-  const getFileString = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
+  // const getFileString = (e) => {
+  //   const file = e.target.files[0];
+  //   const reader = new FileReader();
+  //   reader.readAsDataURL(file);
 
-    reader.onload = () => {
-      handleGroupImageData(reader.result);
-    };
+  //   reader.onload = () => {
+  //     handleGroupImageData(reader.result);
+  //   };
+  // };
+
+  const handleFile = async (e) => {
+    setIsImageUploaded(true)
+    handleGroupImageData(undefined);
+
+    //you can carry out any file validations here...
+    if (e.target.files[0]) {
+      var reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+
+      reader.onloadend = function (e) {
+        setImageSrc(reader.result);
+      };
+    } else {
+      return "Nothing....";
+    }
   };
 
   const handleValidation = async () => {
@@ -72,7 +92,7 @@ export const GroupDetailsContent = ({
 
         return false;
       }
-    } catch (e) {}
+    } catch (e) { }
 
     if (!isLengthValid(groupNameData, 50)) {
       setErrorInfo((x) => ({
@@ -109,22 +129,34 @@ export const GroupDetailsContent = ({
     <ThemeProvider theme={themes}>
       <Container>
         <GroupIconContainer onClick={handleUpload}>
-          {groupImageData ? (
-            <ImageV2
-              src={groupImageData}
-              width="128px"
-              height="128px"
-              style={{ objectFit: 'contain' }}
-            />
-          ) : themes.scheme == 'light' ? (
-            <AddGroupIcon />
-          ) : (
-            <AddGroupIconDark />
-          )}
+          {isImageUploaded
+            ? groupImageData
+              ?
+              <ItemVV2
+                maxWidth="128px"
+                height="128px"
+                borderRadius="32px"
+                overflow="hidden"
+              >
+                <ImageV2
+                  src={groupImageData}
+                  style={{ objectFit: 'contain' }}
+                />
+              </ItemVV2>
+              :
+              <AutoImageClipper
+                imageSrc={imageSrc}
+                onImageCropped={(croppedImage) => handleGroupImageData(croppedImage)}
+              />
+            : themes.scheme == 'light' ? (
+              <AddGroupIcon />
+            ) : (
+              <AddGroupIconDark />
+            )}
           <FileInput
             type="file"
             ref={fileUploadInputRef}
-            onChange={getFileString}
+            onChange={handleFile}
             accept="image/*"
           />
         </GroupIconContainer>
