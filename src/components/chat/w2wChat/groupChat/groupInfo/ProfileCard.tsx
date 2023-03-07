873f63e92b33f116092b47043c4cabae1bf8dd4e
Monalisha Mishra <42746736+mishramonalisha76@users.noreply.github.com>
2023-03-07 14:32:08 +0530

    fixed placement of dropdown in group info (#900)

--- src/components/chat/w2wChat/groupChat/groupInfo/ProfileCard.tsx
+++ src/components/chat/w2wChat/groupChat/groupInfo/ProfileCard.tsx
@@ -13,6 +13,7 @@ import { ImageV2, ItemHV2, ItemVV2, SpanV2 } from 'components/reusables/SharedSt
 import { shortenText } from 'helpers/UtilityHelper';
 import Dropdown from 'components/Dropdown';
 import { caip10ToWallet } from '../../../../../helpers/w2w';
+import { device } from 'config/Globals';
 
 
 export const ProfileCard = ({ key, member, dropdownValues, selectedMemeberAddress, setSelectedMemeberAddress, dropdownRef }) => {
@@ -87,14 +88,16 @@ const ProfileCardItem = styled(ItemHV2)`
 
 const DropdownContainer = styled(ItemVV2)`
   position: absolute;
-  left: 86%;
-  top: 72%;
+  left: 85.5%;
+  top: 69%;
   border-radius: 16px;
   padding: 14px 8px;
   background: ${(props) => props.theme.modalContentBackground};
   z-index: 11;
-  @media (max-width: 480px) {
-    top: 44px;
-    right: 7px;
+  @media ${device.mobileL} {
+    left: 27%;
+  }
+  @media (min-width:426px) and (max-width:1150px) {
+    left: 48%;
   }
 `;
