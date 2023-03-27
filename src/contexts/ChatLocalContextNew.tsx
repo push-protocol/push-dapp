962f5dc8516d98b828c9b3b2d98262c4bd65e627
Nilesh Gupta <guptanilesh2312@gmail.com>
2023-03-27 17:37:36 +0530

    Added comments

--- src/contexts/ChatLocalContextNew.tsx
+++ src/contexts/ChatLocalContextNew.tsx
@@ -1,19 +1,22 @@
 import React, { createContext, useState } from 'react';
 
+// Define an enum for the available tab options
 export enum TabOption {
   Inbox = 'inbox',
   Requests = 'requests',
 }
 
+// Define the type for the local context
 type ChatLocalContextType = {
-  activeTab: TabOption;
-  setActiveTab: React.Dispatch<React.SetStateAction<TabOption>>;
-  selectedChat: string;
-  setSelectedChat: React.Dispatch<React.SetStateAction<string>>;
-  blockedLoading: boolean;
-  setBlockedLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  activeTab: TabOption; // The currently active tab option
+  setActiveTab: React.Dispatch<React.SetStateAction<TabOption>>; // Function to set the active tab
+  selectedChat: string; // The ID of the currently selected chat
+  setSelectedChat: React.Dispatch<React.SetStateAction<string>>; // Function to set the selected chat
+  blockedLoading: boolean; // Flag indicating if the chat is currently blocked due to something (true when chat is blocked and some work is happening like user creation modal)
+  setBlockedLoading: React.Dispatch<React.SetStateAction<boolean>>; // Function to set the blocked loading state
 };
 
+// Create the local context with default values
 export const ChatLocalContext = createContext<ChatLocalContextType>({
   activeTab: TabOption.Inbox,
   setActiveTab: () => {},
@@ -23,15 +26,19 @@ export const ChatLocalContext = createContext<ChatLocalContextType>({
   setBlockedLoading: () => {},
 });
 
+// Define the props type for the local context provider
 type ChatLocalContextProviderProps = {
   children: React.ReactNode;
 };
 
+// Create the local context provider component
 const ChatLocalContextProviderNew: React.FC<ChatLocalContextProviderProps> = ({ children }) => {
+  // Define the state for the local context
   const [activeTab, setActiveTab] = useState<TabOption>(TabOption.Inbox);
   const [selectedChat, setSelectedChat] = useState<string>('');
   const [blockedLoading, setBlockedLoading] = useState<boolean>(false);
 
+  // Combine the state and setter functions into a single context value object
   const tabContextValue: ChatLocalContextType = {
     activeTab,
     setActiveTab,
@@ -41,9 +48,11 @@ const ChatLocalContextProviderNew: React.FC<ChatLocalContextProviderProps> = ({ 
     setBlockedLoading,
   };
 
+  // Render the provider with the context value and children
   return (
     <ChatLocalContext.Provider value={tabContextValue}>{children}</ChatLocalContext.Provider>
   );
 };
 
+// Export the local context provider component
 export default ChatLocalContextProviderNew;
