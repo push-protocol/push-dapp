e2a105666233d3f5e27268bbbb18c650cf9b9dc3
harshrajat <harsh@epns.io>
2023-03-29 17:44:37 +0400

    Partial commit

---
+++ src/pages/IncentivePage.tsx
@@ -0,0 +1,31 @@
+// React + Web3 Essentials
+import React from 'react';
+
+// External Packages
+import styled, { useTheme } from 'styled-components';
+
+// Internal Components
+import { SectionV2 } from 'components/reusables/SharedStylingV2';
+import ChatModule from 'modules/chat/ChatModule';
+
+// Internal Configs
+import GLOBALS from 'config/Globals';
+
+// Chat page
+const ChatPage = () => {
+  // RENDER
+  return (
+    <Container>
+      <ChatModule />
+    </Container>
+  );
+}
+export default ChatPage;
+
+// This defines the page settings, toggle align-self to center if not covering entire stuff, align-items to place them at center
+// justify content flex start to start from top, height is defined by module as well as amount of margin, padding
+const Container = styled(SectionV2)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+`;
