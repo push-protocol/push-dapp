904ecaa53705b01da0f7e020ad2c245d6de18244
Abhishek <abhish3k@192.168.1.2>
2023-04-04 16:42:56 +0530

    UI for yield staking

--- src/sections/yield/YieldSnapshotSection.tsx
+++ src/sections/yield/YieldSnapshotSection.tsx
@@ -9,37 +9,51 @@ import { ItemHV2, ItemVV2 } from 'components/reusables/SharedStylingV2';
 
 // Internal Configs
 import GLOBALS from 'config/Globals';
+import YieldStakingPoolComponent from 'components/yield/YieldStakingPoolComponent';
+import styled from 'styled-components';
 
 // Chat Sections
 // Divided into two, left and right
 const YieldSnapshotSection = () => {
   // RENDER
   return (
-    <>
+    <Container>
       <ItemHV2>
         {/* UNI V3 Liquidity */}
-        <ItemVV2 justifyContent="stretch">
-          Uni V3
-        </ItemVV2>
+        
+        <YieldStakingPoolComponent isV2={false}/>
+        {/* <YieldStakingPoolComponent/> */}
 
         {/* Push Fee Accrual Staking */}
-        <ItemVV2 justifyContent="stretch">
+        {/* <ItemVV2 justifyContent="stretch">
           Push Fee Accrual Staking
-        </ItemVV2>
+          
+        </ItemVV2> */}
+
+        <YieldStakingPoolComponent  isV2={false}/>
       </ItemHV2>
 
       <ItemHV2>
         {/* UNI V2 Liquidity - Deprecated */}
-        <ItemVV2 justifyContent="stretch">
+        {/* <ItemVV2 justifyContent="stretch">
           Uni V2
-        </ItemVV2>
+        </ItemVV2> */}
+
+        <YieldStakingPoolComponent isV2={true}/>
 
         {/* Push Staking - Deprecated */}
-        <ItemVV2 justifyContent="stretch">
+        {/* <ItemVV2 justifyContent="stretch">
           Push Staking
-        </ItemVV2>
+        </ItemVV2> */}
+
+        <YieldStakingPoolComponent isV2={true}/>
       </ItemHV2>
-    </>
+    </Container>
   );
 }
 export default YieldSnapshotSection;
+
+const Container = styled.div`
+  overflow: scroll;
+  height: 1000px;
+`
