dad33dd0582acbe2f17aed0c9fe745b561eeed6c
Nilesh Gupta <guptanilesh2312@gmail.com>
2023-04-04 16:49:56 +0530

    Added yield stats section (#1021)

--- src/sections/yield/YieldStatsSection.tsx
+++ src/sections/yield/YieldStatsSection.tsx
@@ -2,35 +2,90 @@
 import React from 'react';
 
 // External Packages
+import styled from 'styled-components';
 
 // Internal Compoonents
-import ChatBox from 'components/chat/w2wChat/chatBox/ChatBox';
-import { ItemHV2, ItemVV2 } from 'components/reusables/SharedStylingV2';
+import { ItemHV2, ItemVV2, SpanV2 } from 'components/reusables/SharedStylingV2';
 
 // Internal Configs
 import GLOBALS from 'config/Globals';
 
-// Chat Sections
-// Divided into two, left and right
 const YieldStatsSection = () => {
   // RENDER
   return (
-    <ItemHV2>
+    <ItemHV2Modified>
       {/* Total Value Locked */}
-      <ItemVV2 justifyContent="stretch">
-        TVL
-      </ItemVV2>
+      <StatsContainer justifyContent="stretch" background="linear-gradient(0deg, #7ADDB3, #7ADDB3), #FFFFFF">
+        <BgCircleEffect  background="radial-gradient(70% 90% at 40% 16.25%, #7CDCB4 2.6%, #7ADDB3 53.65%, #E888F8 85.42%, #F99DEA 100%)" />
+        <StatsText>
+          <SpanV2 color="#fff" fontSize="18px" fontWeight="600" lineHeight="141%">Total Value Locked</SpanV2>
+          <SpanV2 color="#fff" fontSize="32px" fontWeight="700" lineHeight="141%">$ 3,914,526.55</SpanV2>
+        </StatsText>
+      </StatsContainer>
 
       {/* Push Rewards Given */}
-      <ItemVV2 justifyContent="stretch">
-        PRG
-      </ItemVV2>
+      <StatsContainer justifyContent="stretch" background="#F58DF8">
+        <BgCircleEffect background="radial-gradient(70% 90% at 40% 16.25%, #F58DF8 2.6%, #F47EF8 53.65%, #A17DEF 85.42%, #AE68F4 100%)" />
+        <StatsText>
+          <SpanV2 color="#fff" fontSize="18px" fontWeight="600" lineHeight="141%">Push Rewards Given</SpanV2>
+          <TextBox>
+            <SpanV2 color="#fff" fontSize="32px" fontWeight="700" lineHeight="141%">5,510,000</SpanV2>
+            <SpanV2 color="#fff" fontSize="16px" fontWeight="700" lineHeight="141%">out of 5,510,000</SpanV2>
+          </TextBox>
+        </StatsText>
+      </StatsContainer>
 
       {/* Time Left */}
-      <ItemVV2 justifyContent="stretch">
-        Time Left
-      </ItemVV2>
-    </ItemHV2>
+      <StatsContainer justifyContent="stretch" background="#A17DEF">
+        <BgCircleEffect background="radial-gradient(70% 90% at 40% 16.25%, #A17DEF 2.6%, #9770ED 53.65%, #DF7EEF 85.42%, #F488E3 100%)" />
+        <StatsText>
+          <SpanV2 color="#fff" fontSize="18px" fontWeight="600" lineHeight="141%">Time Left</SpanV2>
+          <TextBox>
+            <SpanV2 color="#fff" fontSize="32px" fontWeight="700" lineHeight="141%">00h 00M 00S</SpanV2>
+            <SpanV2 color="#fff" fontSize="16px" fontWeight="700" lineHeight="141%">until next epoch</SpanV2>
+          </TextBox>
+        </StatsText>
+      </StatsContainer>
+    </ItemHV2Modified>
   );
 }
 export default YieldStatsSection;
+
+// css styles
+const ItemHV2Modified = styled(ItemHV2)`
+  column-gap: 12px;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    row-gap: 12px;
+  }
+`;
+
+const StatsContainer = styled(ItemVV2)`
+  height: 140px;
+  position: relative;
+  border-radius: 16px;
+  overflow:hidden;
+`;
+
+const BgCircleEffect = styled(ItemVV2)`
+  position: absolute;
+  width: 170px;
+  height: 170px;
+  right: 5px;
+  top: -35px;
+  border-radius: 50%;
+
+  box-shadow: 0px 30px 50px rgba(240, 86, 254, 0.5);
+  filter: blur(2.5px);
+`;
+
+const TextBox = styled(ItemVV2)`
+  justify-content: flex-start;
+  align-items: flex-start;
+`
+
+const StatsText = styled(TextBox)`
+  padding: 25px 0 25px 15px;
+  gap: 8px;
+`;
