165184a70b301a2da87f00f58d900206f1888c60
Abhishek <abhish3k@Abhisheks-MacBook-Air.local>
2023-04-06 18:00:18 +0530

    Dark Mode Ui fix

--- src/components/yield/YieldPushFeeV2.tsx
+++ src/components/yield/YieldPushFeeV2.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 // External Packages
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 
 // Internal Compoonents
 import { ButtonV2, H2V2, ImageV2, ItemHV2, ItemVV2,SectionV2,SpanV2 } from 'components/reusables/SharedStylingV2';
@@ -10,6 +10,9 @@ import InfoLogo from "../../assets/inforWithoutBG.svg";
 import { Button } from 'primaries/SharedStyling';
 
 const YieldPushFeeV2 = () => {
+
+    const theme = useTheme();
+
     return (
         <Container>
         {/* Top Section */}
@@ -25,10 +28,7 @@ const YieldPushFeeV2 = () => {
             
         >
             {/* Reward Section */}
-            <ItemHV2
-                border="1px solid #BAC4D6"
-                borderRadius="16px"
-            >
+            <RewardsContainer>
                 <ItemVV2 margin="0px 18px 0px 0px" padding="10px">
                     <SecondaryText>Current Reward</SecondaryText>
                     <H2V2
@@ -49,21 +49,17 @@ const YieldPushFeeV2 = () => {
                         fontSize="24px"
                         fontWeight="700"
                         letterSpacing="-0.03em"
+                        color={theme.fontColor}
                     >
                         12.725 Uni-V2
                     </H2V2>
                 </ItemVV2>
-            </ItemHV2>
+            </RewardsContainer>
 
             {/* Epoch Text */}
-            <ItemHV2
-                alignSelf="end"
-                margin="12px 13px 24px 0px"
-                color="#575D73"
-                letterSpacing="-0.03em"
-            >
+            <EpochText>
                 Current Epoch 100/100
-            </ItemHV2>
+            </EpochText>
 
             {/* Deposit Cash Data */}
             <ItemVV2 >
@@ -119,7 +115,7 @@ const YieldPushFeeV2 = () => {
 export default YieldPushFeeV2;
 
 const Container = styled(SectionV2)`
-    border: 1px solid #BAC4D6;
+    border: 1px solid ${(props)=>props.theme.modalSearchBarBorderColor};
     border-radius: 24px;
     padding:20px;
     margin:10px;
@@ -130,12 +126,10 @@ const Container = styled(SectionV2)`
 `;
 
 const Heading = styled(H2V2)`
-   
-   
     font-size: 24px;
     line-height: 141%;
     letter-spacing: -0.03em;
-    color: #333333;
+    color: ${(props)=>props.theme.modalMessageColor};
 
 `
 const SecondaryText = styled.p`
@@ -143,19 +137,24 @@ const SecondaryText = styled.p`
     font-size: 18px;
     line-height: 141%;
     letter-spacing: -0.03em;
-    color: #333333;
+    color: ${(props)=>props.theme.modalMessageColor};
+`
+
+const RewardsContainer = styled(ItemHV2)`
+    border: 1px solid ${(props)=>props.theme.modalSearchBarBorderColor};
+    border-radius:16px;
 `
 
 const Line = styled.div`
     width: 1px;
     height: 100%;
-    background:#BAC4D6;
+    background:${(props)=>props.theme.modalSearchBarBorderColor};
 `
 const DataTitle = styled.div`
     font-size: 18px;
     line-height: 141%;
     letter-spacing: -0.03em;
-    color: rgba(87, 93, 115, 0.8);
+    color: ${(props)=>props.theme.modalDescriptionTextColor};
     display: flex;
     justify-content: center;
     align-items: center;
@@ -166,7 +165,14 @@ const DataValue = styled(H2V2)`
     font-size: 18px;
     line-height: 141%;
     letter-spacing: -0.03em;
-    color: #333333;
+    color:${(props)=>props.theme.modalMessageColor};
+`
+
+const EpochText = styled(ItemHV2)`
+    align-self:end;
+    margin:12px 13px 24px 0px;
+    letter-spacing:-0.03em;
+    color: ${(props)=>props.theme.modalDescriptionTextColor};
 `
 
 const ButtonsContainer = styled.div`
@@ -194,23 +200,26 @@ const FilledButton = styled(ButtonV2)`
 `;
 
 const EmptyButton = styled(Button)`
-    border: 1px solid #D53A94;
+    border: 1px solid ${(props)=>props.theme.default.secondaryColor};
     border-radius: 8px;
     padding: 12px;
-    background:#ffffff;
+    background:transparent;
     font-size: 18px;
     line-height: 141%;
     letter-spacing: -0.03em;
-    color: #D53A94;
+    color:${(props)=>props.theme.default.secondaryColor};   
     flex:1;
     cursor:pointer;
     & > div{
         display:block;
     }
 
+    &:after{
+        background:transparent;
+    }
+
     &:hover{
-        background: #D53A94;
-        opacity:1;
-        color: #FFFFFF;
+        border-color:#D53A94;
+        color: #D53A94;
     }
 `
