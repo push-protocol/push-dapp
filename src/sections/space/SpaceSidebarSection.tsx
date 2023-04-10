c726cb835e8cac95d599567a5ff8f7ddaf134707
deekshithepns <deekshith@epns.io>
2023-04-10 18:05:17 +0530

    added tabs and profile

--- src/sections/space/SpaceSidebarSection.tsx
+++ src/sections/space/SpaceSidebarSection.tsx
@@ -1,23 +1,37 @@
 // React + Web3 Essentials
-import React from 'react';
+import React,{useContext} from 'react';
 
 // External Packages
+import styled,{useTheme} from 'styled-components';
 
-// Internal Compoonents
+// Internal Components
 import { SpaceSidebar, SpaceSidebarTabs } from 'components/space';
-
-// Internal Configs
+import {ItemHV2, ItemVV2 } from 'components/reusables/SharedStylingV2';
+import { SpaceLocalContext } from 'contexts';
+import ProfileHeader from 'components/chat/w2wChat/profile';
 
 export const SpaceSidebarSection = () => {
-
+  const { setActiveTab } = useContext(SpaceLocalContext);
+  const theme = useTheme();
   // RENDER
   return (
-    <>
-    <div>
-      Space Sidebar Section
-    </div>
-    <SpaceSidebarTabs />
-    <SpaceSidebar />
-    {/* Profile */}
-    </>);
+    <ItemVV2>
+      <SpaceSidebarTabs />
+      <SpaceSidebar />
+      <ProfileContainer borderTop={`2px solid ${theme.default.border}`}>
+        <ProfileHeader
+          setActiveTab={setActiveTab}
+          showQR={false}
+        />
+      </ProfileContainer>
+    </ItemVV2>
+  );
 };
+
+const ProfileContainer = styled(ItemHV2)`
+   flex: initial;
+   justify-content: space-between;
+   margin: 15px 0px 5px 0px;
+   padding: 14px 10px 0px 10px;
+   border-top: ${props => props.borderTop};
+`;
