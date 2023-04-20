74091ce18a8f4497371269607dda7bbf7cbf6199
deekshithepns <deekshith@epns.io>
2023-04-20 12:11:15 +0530

    fixed positioning and added draggable

--- src/components/space/spaceWidget/spaceWidget.tsx
+++ src/components/space/spaceWidget/spaceWidget.tsx
@@ -1,8 +1,10 @@
 // React + Web3 Essentials
 import React, { useContext, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 // External Packages
 import styled from 'styled-components';
+import Draggable from 'react-draggable';
 
 // Internal Components
 import { ImageV2, ItemHV2, ItemVV2, SpanV2 } from 'components/reusables/SharedStylingV2';
@@ -19,6 +21,7 @@ import { device } from 'config/Globals';
 const SpaceWidget = () => {
   const [exitOption, setExitOption] = useState<boolean>(false);
   const { setUserSpaceId } = useContext(SpaceGlobalContext);
+  const { pathname } = useLocation();
   const selectedSpace = spaces[0];
 
   const exitSpace = () => {
@@ -26,88 +29,93 @@ const SpaceWidget = () => {
   };
 
   return (
-    <WidgetContainer>
-      {!exitOption ? (
-        <>
-          <WidgetData>
-            <SpanV2
-              color="#F5F5F5"
-              fontSize="15px"
-              fontWeight="500"
-            >
-              {shortenText(selectedSpace?.spaceCreator, 6)}'s Space
-            </SpanV2>
-            <Close
-              style={{ cursor: 'pointer' }}
-              onClick={() => setExitOption(true)}
-            />
-          </WidgetData>
-          <WidgetData>
-            <ItemHV2 justifyContent="flex-start">
-              <Space />
+    <Draggable>
+      <WidgetContainer
+        bottom={pathname.split('/')[2]?.includes('chatid') ? '138px' : '20px'}
+        right={pathname.split('/')[2]?.includes('chatid') ? '47px' : '20px'}
+      >
+        {!exitOption ? (
+          <>
+            <WidgetData>
               <SpanV2
                 color="#F5F5F5"
-                fontSize="14px"
+                fontSize="15px"
                 fontWeight="500"
               >
-                Live
+                {shortenText(selectedSpace?.spaceCreator, 6)}'s Space
               </SpanV2>
-            </ItemHV2>
+              <Close
+                style={{ cursor: 'pointer' }}
+                onClick={() => setExitOption(true)}
+              />
+            </WidgetData>
+            <WidgetData>
+              <ItemHV2 justifyContent="flex-start">
+                <Space />
+                <SpanV2
+                  color="#F5F5F5"
+                  fontSize="14px"
+                  fontWeight="500"
+                >
+                  Live
+                </SpanV2>
+              </ItemHV2>
+              <ItemHV2 justifyContent="flex-end">
+                {selectedSpace?.members?.slice(0, 3).map((member, index) => {
+                  return (
+                    <ItemVV2
+                      width="31px"
+                      height="31px"
+                      maxWidth="31px"
+                      borderRadius="100%"
+                      overflow="hidden"
+                      margin="0px 0px 0px -18px"
+                      zIndex={4 - index}
+                      key={index}
+                    >
+                      <ImageV2
+                        height="100%"
+                        objectFit="cover"
+                        src={member?.image}
+                        alt="Member Image"
+                      />
+                    </ItemVV2>
+                  );
+                })}
+                <SpanV2
+                  color="#FFFFFF"
+                  fontSize="14px"
+                  fontWeight="500"
+                  margin="0px 0px 0px 3px"
+                >
+                  +190
+                </SpanV2>
+              </ItemHV2>
+            </WidgetData>
+          </>
+        ) : (
+          <WarningContainer>
+            <SpanV2
+              fontSize="15px"
+              fontWeight="500"
+              color="#F5F5F5"
+            >
+              Do you want to leave this space?
+            </SpanV2>
             <ItemHV2 justifyContent="flex-end">
-              {selectedSpace?.members?.slice(0, 3).map((member, index) => {
-                return (
-                  <ItemVV2
-                    width="31px"
-                    height="31px"
-                    maxWidth="31px"
-                    borderRadius="100%"
-                    overflow="hidden"
-                    margin="0px 0px 0px -18px"
-                    zIndex={4 - index}
-                    key={index}
-                  >
-                    <ImageV2
-                      height="100%"
-                      objectFit="cover"
-                      src={member?.image}
-                      alt="Member Image"
-                    />
-                  </ItemVV2>
-                );
-              })}
-              <SpanV2
-                color="#FFFFFF"
-                fontSize="14px"
-                fontWeight="500"
-                margin="0px 0px 0px 3px"
-              >
-                +190
-              </SpanV2>
+              <Check
+                style={{ marginRight: '12px', cursor: 'pointer' }}
+                onClick={() => exitSpace()}
+              />
+              <Close
+                style={{ cursor: 'pointer' }}
+                onClick={() => setExitOption(false)}
+              />
             </ItemHV2>
-          </WidgetData>
-        </>
-      ) : (
-        <WarningContainer>
-          <SpanV2
-            fontSize="15px"
-            fontWeight="500"
-            color="#F5F5F5"
-          >
-            Do you want to leave this space?
-          </SpanV2>
-          <ItemHV2 justifyContent="flex-end">
-            <Check
-              style={{ marginRight: '12px', cursor: 'pointer' }}
-              onClick={() => exitSpace()}
-            />
-            <Close
-              style={{ cursor: 'pointer' }}
-              onClick={() => setExitOption(false)}
-            />
-          </ItemHV2>
-        </WarningContainer>
-      )}
-    </WidgetContainer>
+          </WarningContainer>
+        )}
+      </WidgetContainer>
+    </Draggable>
   );
 };
 
@@ -125,9 +133,10 @@ const WidgetContainer = styled.div`
   border-radius: 17px;
   background: linear-gradient(87.17deg, #b6a0f5 0%, #f46ef7 57.29%, #ff95d5 100%);
   position: fixed;
-  right: 47px;
-  bottom: 124px;
-  z-index: 10;
+  right: ${(props) => props.right || '47px'};
+  bottom: ${(props) => props.bottom || '84px'};
+  z-index: 1001;
+  cursor: pointer;
   @media (${device.mobileL}) {
     right: 20px;
   }
