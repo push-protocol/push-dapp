73adc1c0252e1c90008194a9dc5e2cd42babca5b
corlard3y <corlardey@gmail.com>
2023-04-25 01:30:29 +0100

    merge

--- src/sections/space/SpaceSidebarSection.tsx
+++ src/sections/space/SpaceSidebarSection.tsx
@@ -20,15 +20,6 @@ export const SpaceSidebarSection = ({ showCreateSpaceModal }) => {
   const theme = useTheme();
   // RENDER
   return (
-    // <SpaceSidebarContainer
-    //   flex="1"
-    //   maxWidth="310px"
-    //   minWidth="280px"
-    //   padding="10px 7px 10px 20px"
-    //   boxSizing="border-box"
-    //   background={theme.default.bg}
-    //   spaceActive={!!selectedSpace}
-    // >
       <ItemVV2>
         <SpaceSidebarTabs />
         <SpaceSidebar showCreateSpaceModal={showCreateSpaceModal} />
@@ -38,23 +29,6 @@ export const SpaceSidebarSection = ({ showCreateSpaceModal }) => {
           setShowQR={setShowQR}
         />
       </ItemVV2>
-    // </SpaceSidebarContainer>
   );
 };
 
-const SpaceSidebarContainer = styled(ItemVV2)`
-  @media ${device.tablet} {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    width: 95%;
-    margin-right: ${(props) => (props.spaceActive ? '20%' : '0%')};
-    opacity: ${(props) => (props.spaceActive ? '0' : '1')};
-    opacity: 1;
-    transition: margin-right 0.25s;
-    max-width: initial;
-    min-width: auto;
-    z-index: 1;
-  }
-`;
