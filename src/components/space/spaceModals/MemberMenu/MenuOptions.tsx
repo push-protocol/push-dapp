aed3a56b5009317a09aa8524531472689f51f325
Abhishek <abhish3k@Abhisheks-MacBook-Air.local>
2023-05-02 14:40:37 +0530

    Height of the dropdown and position is changed

--- src/components/space/spaceModals/MemberMenu/MenuOptions.tsx
+++ src/components/space/spaceModals/MemberMenu/MenuOptions.tsx
@@ -61,10 +61,11 @@ const MenuOptions = ({
 
     const dropdownValues = optionData?.dropdownOptions;
 
-    // const handleHeight = () => {
-    //     const containerHeight = document.getElementById(member?.wallet)?.getBoundingClientRect();
-    //     setDropDownHeight(containerHeight?.top);
-    // };
+    const handleHeight = (id) => {
+        const containerHeight = document.getElementById(id?.wallet)?.getBoundingClientRect();
+        console.log("containerHeight",containerHeight)
+        setDropDownHeight(containerHeight?.top);
+    };
 
     const isMobile = useDeviceWidthCheck(600);
 
@@ -76,22 +77,22 @@ const MenuOptions = ({
                 {optionData?.members?.map((member) => {
                     return (
                         <Members key={member.wallet} id={member.wallet}>
-                            <ItemHV2 position='unset' width='auto' padding='8px 16px 8px 8px' background={theme.chat.snapFocusBg} borderRadius='16px' margin='10px 0 0 0'>
+                            <ItemHV2 position='relative' width='auto' padding='8px 16px 8px 8px' background={theme.chat.snapFocusBg} borderRadius='16px' margin='10px 0 0 0'>
                                 <Image src={member?.image}></Image>
                                 <P flex='1' color={theme.modalProfileTextColor} margin="0px 20px" size="17px" weight="500">{shortenText(member?.wallet, 5, 5)}</P>
 
 
                                 {optionData.id === 'requests' ? (
                                     <div>
-                                        <XCircle width={isMobile ? '30px' : '48px'} onClick={declineRequest} />
-                                        <CheckCircle width={isMobile ? '30px' : '48px'} onClick={acceptRequest} />
+                                        <XCircle style={{cursor:'pointer'}} width={isMobile ? '30px' : '48px'} onClick={declineRequest} />
+                                        <CheckCircle style={{cursor:'pointer'}} width={isMobile ? '30px' : '48px'} onClick={acceptRequest} />
                                     </div>
                                 ) : (
                                     <ItemVV2
                                         maxWidth="4px"
                                         padding="0 20px 0 0"
                                         onClick={() => {
-                                            // handleHeight();
+                                            handleHeight(member);
                                             setSelectedWalletAddress(member.wallet)
                                         }
                                         }
@@ -105,6 +106,9 @@ const MenuOptions = ({
 
                                 {(selectedWalletAddress === member.wallet) && (
                                     <DropdownContainer
+                                    style={{
+                                        top:dropdownheight>550 ? '-84%' : '15%'
+                                    }}
                                         key={member.wallet}
                                         ref={dropdownRef}>
                                         <Dropdown
@@ -168,8 +172,10 @@ const Image = styled.img`
 
 const DropdownContainer = styled(ItemVV2)`
   position: absolute;
-  right:26px;
-  top:50%;
+//   right:26px;
+//   top:40%;
+  right:10px;
+  top:0%;
   border-radius: 16px;
   border: 1px solid ${(props) => props.theme.default.border};
   padding: 14px 8px;
