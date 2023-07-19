efc49185db6ae8d7f266f6e2701780240dfe0ba4
corlard3y <corlardey@gmail.com>
2023-07-19 17:17:20 +0100

    save

---
+++ src/contexts/SpaceComponentsContext.tsx
@@ -0,0 +1,46 @@
+// React + Web3 Essentials
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { ethers } from 'ethers';
+
+// External imports
+import * as PushAPI from '@pushprotocol/restapi';
+
+// Internal Components imports
+import { appConfig } from 'config';
+
+import {
+    ISpaceBannerProps,
+    ISpaceCreateWidgetProps,
+    ISpaceFeedProps,
+    ISpaceInvitesProps,
+    ISpaceWidgetProps,
+    SpacesUI,
+  } from '@pushprotocol/uiweb';
+
+export interface IUseSpaceReturnValues {
+    SpaceInvitesComponent: React.FC<ISpaceInvitesProps>;
+    SpaceWidgetComponent: React.FC<ISpaceWidgetProps>;
+    SpaceFeedComponent: React.FC<ISpaceFeedProps>;
+    SpaceBannerComponent: React.FC<ISpaceBannerProps>;
+    CreateSpaceComponent: React.FC<ISpaceCreateWidgetProps>;
+}
+
+export const SpaceComponentContext = createContext({});
+
+const SpaceComponentContextProvider: React.FC<{
+    spaceUI: SpacesUI,
+    children: React.ReactNode
+}> = ({ spaceUI, children }) => {
+
+
+  return <SpaceComponentContext.Provider value={{
+        SpaceInvitesComponent: spaceUI.SpaceInvites,
+        SpaceWidgetComponent: spaceUI.SpaceWidget,
+        SpaceBannerComponent: spaceUI.SpaceBanner,
+        SpaceFeedComponent: spaceUI.SpaceFeed,
+        CreateSpaceComponent: spaceUI.SpaceCreationButtonWidget,
+  }}>{children}</SpaceComponentContext.Provider>;
+};
+
+export default SpaceComponentContextProvider;
