dace4ad3c759bd7be8f4e446f37c7e09e601c740
Deekshith M D <110546074+deekshithepns@users.noreply.github.com>
2023-08-30 16:14:32 +0530

    updated discord links (#1209)
    Co-authored-by: deekshithepns <70252174+deekshithmd@users.noreply.github.com>

--- src/config/config-localhost.js
+++ src/config/config-localhost.js
@@ -62,7 +62,7 @@ export const config = {
    * External links
    */
   links: {
-    ios: 'https://discord.com/invite/YVPB99F9W5',
+    ios: 'https://discord.gg/pushprotocol',
     android: 'https://play.google.com/store/apps/details?gl=US&hl=en&id=io.epns.epnsstaging',
     extension: 'https://chrome.google.com/webstore/detail/epns-staging-protocol-alp/bjiennpmhdcandkpigcploafccldlakj',
     howto: 'https://docs.epns.io',
