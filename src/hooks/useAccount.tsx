e95cffe5e1b4fa64a8a4ca9cf81058078da3b279
kalashshah <202051096@iiitvadodara.ac.in>
2023-09-13 17:21:20 +0530

    fix: format address using signer

--- src/hooks/useAccount.tsx
+++ src/hooks/useAccount.tsx
@@ -1,9 +1,11 @@
+import { useEffect, useState } from 'react';
 import { useConnectWallet, useSetChain } from '@web3-onboard/react';
 import { ethers } from 'ethers';
 
 export const useAccount = () => {
   const [{ wallet, connecting }, connect, disconnect, updateBalances, setWalletModules, setPrimaryWallet] =
     useConnectWallet();
+  const [address, setAddress] = useState<string>();
 
   const [{ chains, connectedChain, settingChain }, setChain] = useSetChain();
 
@@ -15,6 +17,18 @@ export const useAccount = () => {
     setChain({ chainId: ethers.utils.hexValue(desiredChain) });
   };
 
+  useEffect(() => {
+    if(wallet && wallet.accounts.length > 0) {
+      setAddress(wallet.accounts[0].address);
+      (async () => {
+        const ethersProvider = new ethers.providers.Web3Provider(wallet.provider);
+        const signer = ethersProvider.getSigner();
+        const walletAddress = await signer.getAddress();
+        setAddress(walletAddress);
+      })();
+    }
+  }, [wallet]);
+
   return {
     wallet,
     connecting,
@@ -24,7 +38,7 @@ export const useAccount = () => {
     setWalletModules,
     setPrimaryWallet,
     provider: wallet ? new ethers.providers.Web3Provider(wallet.provider, 'any') : undefined,
-    account: wallet && wallet.accounts.length > 0 ? wallet.accounts[0].address : undefined,
+    account: address,
     chainId: connectedChain ? Number(connectedChain.id) : undefined,
     isActive,
     setChain,
