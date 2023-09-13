7e14f85c836770080917faafef7fa9b8c8ad7466
kalashshah <202051096@iiitvadodara.ac.in>
2023-09-13 18:20:10 +0530

    chore: add push logo to onboard

--- src/connectors/web3Onboard.tsx
+++ src/connectors/web3Onboard.tsx
@@ -2,7 +2,7 @@ import Onboard from '@web3-onboard/core';
 import injectedModule from '@web3-onboard/injected-wallets';
 import { getWeb3OnboardChains } from './chains';
 import { walletConnect } from './walletConnect';
-import PushLogo from '../assets/PushLogo.svg';
+import PushBlocknativeLogo from '../assets/PushBlocknativeLogo.svg';
 
 const injected = injectedModule();
 
@@ -10,8 +10,8 @@ const chains = getWeb3OnboardChains();
 export const web3Onboard = Onboard({
   appMetadata: {
     name: 'Push (EPNS)',
-    icon: PushLogo,
-    logo: PushLogo,
+    icon: PushBlocknativeLogo,
+    logo: PushBlocknativeLogo,
     description: 'The Communication Protocol of Web3',
     explore: "https://app.push.org",
     agreement: {
