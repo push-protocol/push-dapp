cf1ebcf4a1bf9b83752c3c67ef76eb99dd210528
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-05-14 11:10:55 +0530

    fixed imports

--- src/modules/receiveNotifs/ReceiveNotifsModule.tsx
+++ src/modules/receiveNotifs/ReceiveNotifsModule.tsx
@@ -1,32 +1,27 @@
-// React + Web3 Essentials
-import React from "react";
-
 // External Packages
-import ReactGA from "react-ga";
-import styled, { useTheme } from "styled-components";
+import ReactGA from 'react-ga';
+import styled from 'styled-components';
 
 // Internal Components
-import { ItemVV2 } from "components/reusables/SharedStylingV2";
-import { H2, Item, Section, Span } from "../../primaries/SharedStyling";
-import Info from "segments/Info";
+import { ItemVV2 } from 'components/reusables/SharedStylingV2';
+import { Section } from '../../primaries/SharedStyling';
 
 // Internal Configs
-import GLOBALS, { device, globalsMargin } from "config/Globals";
-import useModalBlur, { MODAL_POSITION } from "hooks/useModalBlur";
-import MetamaskPushSnapModal from "./MetamaskPushSnapModal";
+import GLOBALS, { device } from 'config/Globals';
+import MetamaskPushSnapModal from './MetamaskPushSnapModal';
 
 // Create Header
 function ReceiveNotifsModule() {
-  ReactGA.pageview("/receive");
-
-  // get theme
-  const theme = useTheme();
+  ReactGA.pageview('/receive');
 
   // Render
   return (
     <Container>
       <ItemVV2 alignItems="stretch">
-        <MetamaskPushSnapModal onClose={() => {}} closeEnabled={false} />
+        <MetamaskPushSnapModal
+          onClose={() => {}}
+          closeEnabled={false}
+        />
       </ItemVV2>
     </Container>
   );
@@ -34,19 +29,19 @@ function ReceiveNotifsModule() {
 
 // css style
 const Container = styled(Section)`
-	align-items: center;
-	align-self: center;
-	background: ${(props) => props.theme.default.bg};
-	border-radius: ${GLOBALS.ADJUSTMENTS.RADIUS.LARGE};
-	box-shadow: ${GLOBALS.ADJUSTMENTS.MODULE_BOX_SHADOW};
-	display: flex;
-	flex-direction: column;
-	flex: initial;
-	justify-content: center;
+  align-items: center;
+  align-self: center;
+  background: ${(props) => props.theme.default.bg};
+  border-radius: ${GLOBALS.ADJUSTMENTS.RADIUS.LARGE};
+  box-shadow: ${GLOBALS.ADJUSTMENTS.MODULE_BOX_SHADOW};
+  display: flex;
+  flex-direction: column;
+  flex: initial;
+  justify-content: center;
   max-width: 1200px;
   width: auto;
   padding: ${GLOBALS.ADJUSTMENTS.PADDING.BIG};
-	position: relative;
+  position: relative;
   margin: ${GLOBALS.ADJUSTMENTS.MARGIN.MINI_MODULES.DESKTOP};
 
   @media ${device.laptop} {
@@ -59,7 +54,6 @@ const Container = styled(Section)`
     padding: ${GLOBALS.ADJUSTMENTS.PADDING.DEFAULT};
   }
 `;
-
 
 // Export Default
 export default ReceiveNotifsModule;
