5ec6f0aca24ddd816136aa3766d93b8a30f18c68
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-05-18 14:33:52 +0530

    changed basepath

--- basePath.ts
+++ basePath.ts
@@ -3,10 +3,10 @@ export function getPreviewBasePath(): string {
   if (typeof import.meta !== 'undefined' && import.meta.env) {
     // Vite environment
     // @ts-ignore
-    return import.meta.env.VITE_PR_PREVIEW_BASE ? `/push-dapp/pr-preview/${import.meta.env.VITE_PR_PREVIEW_BASE}` : '';
+    return import.meta.env.VITE_PR_PREVIEW_BASE ? `/push-dapp/pr-preview/${import.meta.env.VITE_PR_PREVIEW_BASE}/` : '';
   } else if (typeof process !== 'undefined' && process.env) {
     // Node.js environment (e.g., during the build process)
-    return process.env.VITE_PR_PREVIEW_BASE ? `/push-dapp/pr-preview/${process.env.VITE_PR_PREVIEW_BASE}` : '';
+    return process.env.VITE_PR_PREVIEW_BASE ? `/push-dapp/pr-preview/${process.env.VITE_PR_PREVIEW_BASE}/` : '';
   }
   return '';
 }
