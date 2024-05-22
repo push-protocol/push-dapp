c78300d73df335b758b8aabb1c4e9e8237af4dca
HarshRajat <HarshRajat@users.noreply.github.com>
2024-05-22 16:27:46 +0000

    Deploy preview for PR 1531 🛫

---
+++ pr-preview/pr-1531/assets/NotFoundPage-B8K-7kQ2.js
@@ -0,0 +1,53 @@
+import{cc as g,b4 as t,aV as e,aW as i,__tla as h}from"./index-DQ9NSsoY.js";let a,m=Promise.all([(()=>{try{return h}catch{}})()]).then(async()=>{let r,o;r="/push-dapp/pr-preview/pr-1531/assets/404Graphic-BY-vXDBY.svg",a=()=>{const n=g();return t.jsx(o,{children:t.jsxs(c,{children:[t.jsx(p,{src:r,alt:"Page not found"}),t.jsxs(x,{children:[t.jsx(s,{children:"Oops..."}),t.jsx(l,{children:"The page you're trying to reach doesn't exist."})]}),t.jsx(d,{onClick:()=>n("/channels"),children:"Go to Home"})]})})},o=e.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(100vh - ${i.CONSTANTS.HEADER_HEIGHT}px);
+  width: 80%;
+  border-radius: ${i.ADJUSTMENTS.RADIUS.LARGE};
+  padding: 20px;
+  box-sizing: border-box;
+  // margin: ${i.ADJUSTMENTS.MARGIN.MINI_MODULES.DESKTOP};
+  margin: auto;
+  `,e.p`
+  color: ${n=>n.theme.default.text};
+  font-size: 1.2rem;
+  margin-bottom: 1.5rem;
+`;const s=e.p`
+    font-size: 48px;
+    font-weight: 500;
+    margin: 0;
+    text-align: center;
+    color: black;
+`,c=e.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 32px;
+`,p=e.img`
+    width: 355.7px;
+    height: 205.79px;
+    border-radius: 16px;
+    margin: 0 auto;
+`,l=e.p`
+    font-size: 24px;
+    font-weight: 400;
+    width: 100%;
+    text-align: center;
+`,d=e.button`
+    padding: 14px 24px 14px 24px;
+    border-radius: 16px;
+    background-color: #DD44B9;
+    color: #FFFFFF;
+    border: none;
+    height: 54px;
+    width: 201px;
+    font-size: 18px;
+    display: flex;
+    text-decoration: none;
+    align-items: center;
+    justify-content: center;
+`,x=e.div`
+    gap: 16px;
+`});export{m as __tla,a as default};
