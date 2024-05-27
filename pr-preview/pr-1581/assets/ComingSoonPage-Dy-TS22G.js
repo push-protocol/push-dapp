2acc4ae3311da5424984289e22735264043265f6
HarshRajat <HarshRajat@users.noreply.github.com>
2024-05-27 12:45:28 +0000

    Deploy preview for PR 1581 🛫

---
+++ pr-preview/pr-1581/assets/ComingSoonPage-Dy-TS22G.js
@@ -0,0 +1,16 @@
+import{a_ as r,b4 as t,bQ as s,bd as o,cD as c,b7 as l,aV as h,__tla as m}from"./index-xu31DHN6.js";let i,x=Promise.all([(()=>{try{return m}catch{}})()]).then(async()=>{i=function(e){const n=r();return t.jsx(s,{theme:n,children:t.jsx(o,{style:{minHeight:"60vh"},children:t.jsx(c,{padding:"0px 20px 0px",children:t.jsxs(l,{align:"center",children:[console.info(e.theme),t.jsx(a,{color:"#333",spacing:"0.05em",size:"4rem",weight:700,children:"Coming soon."})]})})})})};const a=h.h2`
+  color: ${e=>e.theme.scheme==="dark"?e.theme.color:e.color||"#000"};
+  text-shadow: 2px 0 ${e=>e.theme.scheme==="dark"?e.theme.color:e.color||"#000"};
+  font-weight: ${e=>e.weight||600};
+  font-size: ${e=>e.size||"2rem"};
+  text-transform: ${e=>e.textTransform||"inherit"};
+  margin: ${e=>e.margin||"20px 0px"};
+  padding: ${e=>e.padding||"0px"};
+  letter-spacing: ${e=>e.spacing||"inherit"};
+  font-family: ${e=>e.family||"'Strawford', 'Source Sans Pro', Helvetica, sans-serif"};
+  text-align: ${e=>e.textAlign||"inherit"};
+
+  @media (max-width: 768px) {
+		font-size: 2.8rem;
+	}
+`});export{x as __tla,i as default};
---
+++ pr-preview/pr-1581/assets/auryn@2x-CaNWdGvb.js
@@ -0,0 +1,1 @@
+const p="/push-dapp/pr-preview/pr-1581/assets/auryn@2x-CIso5_SL.jpg";export{p as default};
