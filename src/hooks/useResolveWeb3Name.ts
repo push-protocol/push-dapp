7bf1b8c06acb74683912f3c670e4a09cb553c327
Monalisha Mishra <mishramonalisha76@gmail.com>
2024-05-27 17:12:27 +0530

    added space-id

--- src/hooks/useResolveWeb3Name.ts
+++ src/hooks/useResolveWeb3Name.ts
@@ -1,6 +1,7 @@
 // React + Web3 Essentials
 import { ethers } from 'ethers';
 import { useContext, useEffect, useState } from 'react';
+import { createWeb3Name } from '@web3-name-sdk/core';
 
 // Internal Components
 import { caip10ToWallet } from 'helpers/w2w';
@@ -13,21 +14,23 @@ import { AppContextType } from 'types/context';
 import { appConfig } from '../config/index.js';
 import { getUdResolver } from 'helpers/w2w/udResolver';
 
-const getEnsName = async (
-  provider: ethers.providers.BaseProvider | any,
-  checksumWallet: string,
-  setWeb3NameList: any
-) => {
-  let ensName: string = '';
-  provider.lookupAddress(checksumWallet).then(async (ens) => {
-    if (ens) {
-      ensName = ens;
-      setWeb3NameList((prev) => ({ ...prev, [checksumWallet]: ens }));
-    } else {
-      ensName = null;
-    }
-  });
-  return ensName;
+const getDomainName = async (checksumWallet: string, setWeb3NameList: any) => {
+  let domainName: string | null = '';
+  const web3NameClient = createWeb3Name();
+  web3NameClient
+    .getDomainName({
+      address: checksumWallet,
+      queryChainIdList: appConfig.allowedNetworks,
+    })
+    .then(async (domain) => {
+      if (domain) {
+        domainName = domain;
+        setWeb3NameList((prev) => ({ ...prev, [checksumWallet]: domain }));
+      } else {
+        domainName = null;
+      }
+    });
+  return domainName;
 };
 
 const getUnstoppableName = async (checksumWallet: string, setWeb3NameList: any) => {
@@ -45,7 +48,7 @@ const getUnstoppableName = async (checksumWallet: string, setWeb3NameList: any) 
 };
 
 export function useResolveWeb3Name(address?: string) {
-  const [web3Name, setWeb3Name] = useState<string>(null);
+  const [web3Name, setWeb3Name] = useState<string | null>(null);
 
   const ctx: ContextType = useContext<ContextType>(Context);
 
@@ -54,7 +57,6 @@ export function useResolveWeb3Name(address?: string) {
   useEffect(() => {
     (async () => {
       setWeb3Name(null);
-      let provider = new ethers.providers.InfuraProvider(appConfig.coreContractChain, appConfig.infuraAPIKey);
       if (address) {
         const walletLowercase = address.includes(':nft')
           ? caip10ToWallet(
@@ -68,8 +70,6 @@ export function useResolveWeb3Name(address?: string) {
         const checksumWallet = ethers.utils.getAddress(walletLowercase);
         if (ethers.utils.isAddress(checksumWallet)) {
           try {
-            // attempt ENS name resolution first, with a fallback to Unstoppable Domains if
-            // a value is not found from ENS.
             Object.keys(web3NameList).forEach((element) => {
               if (web3NameList[checksumWallet]) {
                 setWeb3Name(web3NameList[checksumWallet]);
@@ -78,7 +78,7 @@ export function useResolveWeb3Name(address?: string) {
             });
 
             let web3Response =
-              (await getEnsName(provider, checksumWallet, setWeb3NameList)) ||
+              (await getDomainName(checksumWallet, setWeb3NameList)) ||
               (await getUnstoppableName(checksumWallet, setWeb3NameList));
             // store result
             if (web3Response) {
@@ -86,7 +86,7 @@ export function useResolveWeb3Name(address?: string) {
               return;
             }
           } catch (e) {
-            // console.error('Error fetching web3 name from indexDB', e);
+            console.debug('Error fetching web3 name from indexDB', e);
           }
         }
       }
