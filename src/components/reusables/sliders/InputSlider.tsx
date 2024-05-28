75c87a51670d4157d056e82141a045057e7ce4e9
Abhishek <77395788+abhishek-01k@users.noreply.github.com>
2024-05-28 18:44:47 +0530

    Subscribe change to subscribev2 (#1524)
    * Subscribe change to subscribev2
    
    * Changed the code from for loop to array.reduce
    
    * Removed Comments

--- src/components/reusables/sliders/InputSlider.tsx
+++ src/components/reusables/sliders/InputSlider.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, useEffect, useRef, MouseEvent, TouchEvent } from 'react';
+import React, { ComponentPropsWithoutRef, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 interface InputSliderProps extends Omit<ComponentPropsWithoutRef<'div'>, 'children' | 'onChange'> {
@@ -10,8 +10,8 @@ interface InputSliderProps extends Omit<ComponentPropsWithoutRef<'div'>, 'childr
   defaultVal: number;
   preview?: boolean;
   onChange: (value: { x: number }) => void;
-  onDragStart?: (e: MouseEvent | TouchEvent) => void;
-  onDragEnd?: (e: MouseEvent | TouchEvent) => void;
+  onDragStart?: (e: React.MouseEvent | React.TouchEvent) => void;
+  onDragEnd?: (e: React.MouseEvent | React.TouchEvent) => void;
 }
 
 const InputSlider = ({
@@ -33,7 +33,7 @@ const InputSlider = ({
   const containerRef = useRef<HTMLDivElement>(null);
   const previewSliderRef = useRef<HTMLDivElement>(null);
 
-  const handleMouseDown = (e: MouseEvent | TouchEvent) => {
+  const handleMouseDown = (e: React.MouseEvent | React.TouchEvent) => {
     if (disabled) return;
     if (onDragStart) onDragStart(e);
     document.addEventListener('mousemove', handleMouseMove);
