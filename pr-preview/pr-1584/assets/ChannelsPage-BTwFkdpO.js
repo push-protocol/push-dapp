c7fe2ebe18a3a89ffd30f207cc20b632beba4130
corlard3y <corlard3y@users.noreply.github.com>
2024-05-28 17:49:54 +0000

    Deploy preview for PR 1584 🛫

---
+++ pr-preview/pr-1584/assets/ChannelsPage-BTwFkdpO.js
@@ -0,0 +1,5 @@
+import{cQ as o,b4 as a,aV as n,bj as m,__tla as i}from"./index-DubHxc_O.js";import{C as h,__tla as f}from"./ChannelsModule-XubeKQ4F.js";import{__tla as p}from"./DisplayNotice-CzSLzWZZ.js";import{__tla as u}from"./Skeleton-BBJBL1IA.js";import{__tla as y}from"./cloneDeep-CaYrYxlx.js";import{__tla as d}from"./index.esm-DMeFBKcx.js";import{__tla as x}from"./RedCircle-D2faVNdT.js";import{__tla as T}from"./ManageNotifSettingDropdown-Du8tV3Wq.js";import{__tla as j}from"./RangeSlider-B7LTA-is.js";let r,b=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return x}catch{}})(),(()=>{try{return T}catch{}})(),(()=>{try{return j}catch{}})()]).then(async()=>{let t;r=({loadTeaser:_,playTeaser:l,channelID:e})=>{let{channelid:c}=o();const s=c||e;return a.jsx(t,{children:a.jsx(h,{channelID:s,loadTeaser:_,playTeaser:l})})},t=n(m)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+`});export{b as __tla,r as default};
