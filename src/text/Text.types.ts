fc75e9b2d5c46c63442f663b0d63a080ed223b91
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-05-30 16:11:03 +0530

    Added Text component

---
+++ src/text/Text.types.ts
@@ -0,0 +1,32 @@
+import { textVariants } from './Text.constants';
+
+export type TextVariants = keyof typeof textVariants;
+
+// export type TypographyColors = keyof typeof COLORS;
+
+export type TextHTMLTags =
+  | 'span'
+  | 'div'
+  | 'p'
+  | 'b'
+  | 'i'
+  | 'u'
+  | 'abbr'
+  | 'cite'
+  | 'kbd'
+  | 's'
+  | 'sample'
+  | 'sub'
+  | 'sup'
+  | 'ins'
+  | 'del'
+  | 'mark'
+  | 'em'
+  | 'i'
+  | 'strong'
+  | 'small'
+  | 'label';
+
+export type TextAlign = 'left' | 'center' | 'right';
+
+export type TextTransform = 'lowercase' | 'uppercase' | 'capitalize' | 'inherit';
