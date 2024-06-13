8948cb8ea406803b0701992a8d875ce900af336c
Monalisha Mishra <mishramonalisha76@gmail.com>
2024-06-13 16:40:03 +0530

    added ui changes

--- src/queries/types/user.ts
+++ src/queries/types/user.ts
@@ -2,7 +2,7 @@ import { UserSetting } from '@pushprotocol/restapi';
 
 export type UserSubscriptionData = {
   channel: string;
-  user_settings: UserSetting[];
+  user_settings: string;
 };
 
 export type UserSubscriptionsResponse = Array<UserSubscriptionData>;
