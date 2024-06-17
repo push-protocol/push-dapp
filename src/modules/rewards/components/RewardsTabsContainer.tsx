e99f8872872b33db9ae06692fd9255ae286baef1
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-06-17 21:30:52 +0530

    added leadership board UI

--- src/modules/rewards/components/RewardsTabsContainer.tsx
+++ src/modules/rewards/components/RewardsTabsContainer.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import { RewardsTabs } from './RewardsTabs';
 import { RewardsTabs as RewardsTabsType } from '../Rewards.types';
 import { DashboardSection } from './DashboardSection';
+import { LeaderBoardSection } from './LeaderBoardSection';
 
 export type RewardsTabsContainerProps = {
   activeTab: RewardsTabsType;
@@ -28,6 +29,7 @@ const RewardsTabsContainer: FC<RewardsTabsContainerProps> = ({ activeTab, handle
           handleSetActiveTab={handleSetActiveTab}
         />
         {activeTab === 'dashboard' && <DashboardSection onGetStarted={() => handleSetActiveTab('activites')} />}
+        {activeTab === 'leaderboard' && <LeaderBoardSection />}
       </Box>
     </Box>
   );
