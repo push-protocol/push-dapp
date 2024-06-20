bcdc5a229f4346eaa1cb8cd194b0b9d5a78a86b5
corlard3y <corlardey@gmail.com>
2024-06-21 00:35:14 +0100

    remove function

--- src/App.tsx
+++ src/App.tsx
@@ -274,20 +274,6 @@ export default function App() {
     };
   }, []);
 
-  useEffect(() => {
-    function setViewportHeight() {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    }
-
-    setViewportHeight();
-    window.addEventListener('resize', setViewportHeight);
-
-    return () => {
-      window.removeEventListener('resize', setViewportHeight);
-    };
-  }, []);
-
   const steps = UserJourneySteps({ darkMode });
 
   const handleJoyrideCallback = (data: CallBackProps) => {
