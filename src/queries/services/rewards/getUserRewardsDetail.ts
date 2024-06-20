a024b49927b4ba5c4b7030c367703c5810bf5d9d
corlard3y <corlardey@gmail.com>
2024-06-20 23:43:42 +0100

    update code

--- src/queries/services/rewards/getUserRewardsDetail.ts
+++ src/queries/services/rewards/getUserRewardsDetail.ts
@@ -4,8 +4,11 @@ import { appConfig } from '../../../config/index';
 import { getUserRewardsDetailModel } from '../../models';
 import { UserRewardsDetailParams } from '../../types/rewards';
 
+/* this url will change as per the env once the apis are ready */
+const apiUrl = 'https://us-east1-push-dev-apps.cloudfunctions.net/helloWorld/users/wallet/';
+
 export const getUserRewardsDetail = ({ walletAddress }: UserRewardsDetailParams) =>
   axios({
     method: 'GET',
-    url: `https://us-east1-push-dev-apps.cloudfunctions.net/helloWorld/users/wallet/${walletAddress}`,
+    url: `${apiUrl}${walletAddress}`,
   }).then((response) => getUserRewardsDetailModel(response.data));
