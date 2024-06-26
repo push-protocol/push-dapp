ba4af87065604a34f3aa1a755edc59fd64f68d46
Kalash Shah <81062983+kalashshah@users.noreply.github.com>
2024-06-26 14:12:36 +0530

    DApp-1551 blocks/Tooltip (#1635)
    * feat: tooltip blocks component
    
    * chore: implement tooltip using radix tooltip
    
    * chore: rename hook file extension
    
    * updated lock file
    
    * chore: basic style and func fixes
    
    ---------
    
    Co-authored-by: rohitmalhotra1420 <rohit.malhotra1420@gmail.com>

---
+++ src/blocks/tooltip/Tooltip.constants.ts
@@ -0,0 +1,10 @@
+import type { TooltipProps } from './Tooltip.types';
+
+export const tooltipCSSPropsKeys: (keyof TooltipProps)[] = [
+  'height',
+  'maxHeight',
+  'minHeight',
+  'maxWidth',
+  'minWidth',
+  'width',
+];
