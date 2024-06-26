0b65805131464c0bd5340014cc7d789325388d42
abhishek-01k <abhishek-01k@users.noreply.github.com>
2024-06-26 14:33:50 +0000

    Deploy preview for PR 1664 🛫

---
+++ pr-preview/pr-1664/assets/ChannelsPage-DksuD6j1.js
@@ -0,0 +1,5 @@
+import{u as o,m as a,q as m,S as n,__tla as i}from"./index-BHE_IUA0.js";import{C as h,__tla as f}from"./ChannelsModule-Bcm5Ljrp.js";import{__tla as u}from"./DisplayNotice-WMOxipik.js";import{__tla as p}from"./Skeleton-COY3o6bW.js";import{__tla as y}from"./cloneDeep-BohpLqGO.js";import{__tla as d}from"./index.esm-D9Bs1T7b.js";import{__tla as x}from"./RedCircle-Bm7b2ssj.js";import{__tla as T}from"./ManageNotifSettingDropdown-I3LLbA1E.js";import{__tla as D}from"./RangeSlider-BPKGpJVp.js";import{__tla as j}from"./OptinNotifSettingDropdown-fqnJDyWA.js";let r,I=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return x}catch{}})(),(()=>{try{return T}catch{}})(),(()=>{try{return D}catch{}})(),(()=>{try{return j}catch{}})()]).then(async()=>{let t;r=({loadTeaser:_,playTeaser:l,channelID:e})=>{let{channelid:c}=o();const s=c||e;return a.jsx(t,{children:a.jsx(h,{channelID:s,loadTeaser:_,playTeaser:l})})},t=m(n)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+`});export{I as __tla,r as default};
---
+++ pr-preview/pr-1664/assets/TutorialPage-C8Y3_DJS.js
@@ -0,0 +1,93 @@
+import{q as a,dc as e,dd as i,de as h,df as s,E as I,X as M,D as A,m as t,dh as l,di as m,dj as N,n as u,dk as U,I as T,dx as $,S as L,__tla as j}from"./index-BHE_IUA0.js";let E,G=Promise.all([(()=>{try{return j}catch{}})()]).then(async()=>{function S(){const r=I(),n=M(),{run:f}=A(p=>p.userJourney),g=()=>{f===!1&&r($(!0))};return t.jsx(d,{children:t.jsxs(l,{children:[t.jsxs(l,{align:"stretch",justify:"flex-start",margin:"0px 0px 40px 0px",children:[t.jsx(m,{children:t.jsx(s,{weight:"400",size:"32px",color:n.color,children:"Live Walkthrough"})}),t.jsx(s,{color:n.default.secondaryColor,weight:"400",size:"16px",textTransform:"none",textAlign:"center",spacing:"0.03em",margin:"0px 0px",children:"Let's walk you through the core functions of Push (EPNS), the communication protocol of Web3!"})]}),t.jsx(l,{justify:"flex-start",children:t.jsx(x,{children:t.jsxs(o,{onClick:()=>{g()},hover:"#e20880",children:[t.jsx(s,{color:n.color,children:"\u{1F449} Show me everything!"}),t.jsx(N,{size:20,color:"#ddd"})]})})})]})})}let d,o,c,x,D;d=a(u)`
+  align-items: center;
+  align-self: center;
+  background: ${r=>r.theme.default.bg};
+  border-radius: ${e.ADJUSTMENTS.RADIUS.LARGE};
+  box-shadow: ${e.ADJUSTMENTS.MODULE_BOX_SHADOW};
+  display: flex;
+  flex-direction: column;
+  flex: initial;
+  justify-content: center;
+  max-width: 1200px;
+  width: calc(
+    100% - ${i.MINI_MODULES.DESKTOP.RIGHT} - ${i.MINI_MODULES.DESKTOP.LEFT} -
+      ${e.ADJUSTMENTS.PADDING.BIG} - ${e.ADJUSTMENTS.PADDING.BIG}
+  );
+  padding: ${e.ADJUSTMENTS.PADDING.BIG};
+  position: relative;
+  margin: ${e.ADJUSTMENTS.MARGIN.MINI_MODULES.DESKTOP};
+
+  @media ${h.laptop} {
+    margin: ${e.ADJUSTMENTS.MARGIN.MINI_MODULES.TABLET};
+    padding: ${e.ADJUSTMENTS.PADDING.DEFAULT};
+    width: calc(
+      100% - ${i.MINI_MODULES.TABLET.RIGHT} - ${i.MINI_MODULES.TABLET.LEFT} -
+        ${e.ADJUSTMENTS.PADDING.DEFAULT} - ${e.ADJUSTMENTS.PADDING.DEFAULT}
+    );
+  }
+
+  @media ${h.mobileM} {
+    margin: ${e.ADJUSTMENTS.MARGIN.MINI_MODULES.MOBILE};
+    padding: ${e.ADJUSTMENTS.PADDING.DEFAULT};
+    width: calc(
+      100% - ${i.MINI_MODULES.MOBILE.RIGHT} - ${i.MINI_MODULES.MOBILE.LEFT} -
+        ${e.ADJUSTMENTS.PADDING.DEFAULT} - ${e.ADJUSTMENTS.PADDING.DEFAULT}
+    );
+  }
+`,o=a(U)`
+  align-items: stretch;
+  align-self: stretch;
+`,c=a(T)`
+  align-items: stretch;
+  align-self: stretch;
+`,x=a(T)`
+  align-items: stretch;
+  align-self: stretch;
+  flex: auto;
+  margin: 15px 0px;
+  border: 1px solid ${r=>r.theme.qnaBgBorder};
+  border-radius: 10px;
+  box-shadow: 0px 5px 20px -10px rgb(0 0 0 / 0.2);
+  overflow: hidden;
+
+  & ${o} {
+    background: ${r=>r.theme.qnaBg};
+    justify-content: flex-start;
+    text-transform: uppercase;
+
+    & ${s} {
+      font-weight: 400;
+      letter-spacing: 0.2em;
+      margin-left: 10px;
+      flex: 1;
+    }
+
+    &:hover {
+      & ${s} {
+        color: #fff;
+      }
+    }
+  }
+
+  & ${c} {
+    border: 1px solid #e6e6e6;
+    border-top: 1px solid #e6e6e6;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    padding: 10px 15px;
+    align-items: flex-start;
+    background: #fff;
+
+    & ${s} {
+      line-height: 1.5em;
+      margin: 10px;
+      color: #000;
+      font-size: 1.05em;
+    }
+  }
+`,E=()=>t.jsx(D,{children:t.jsx(S,{})}),D=a(L)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+  justify-content: flex-start;
+`});export{G as __tla,E as default};
