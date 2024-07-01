a128eca5ed3ba6d767fb355d25508c9211174ecb
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-07-01 14:22:25 +0530

    used color function for getting the color

--- src/blocks/tabs/Tabs.styled.ts
+++ src/blocks/tabs/Tabs.styled.ts
@@ -1,6 +1,7 @@
 import { Tabs as ReachTabs, TabList, Tab } from '@reach/tabs';
 import styled from 'styled-components';
 import { ModeProp } from '../Blocks.types';
+import { getBlocksColor } from '../Blocks.utils';
 
 export const StyledFillTabs = styled(ReachTabs)`
   display: flex;
@@ -12,7 +13,7 @@ export const StyledFillTabList = styled(TabList)<ModeProp>`
   display: flex;
   width: fit-content;
   padding: var(--s1);
-  background-color: var(--${({ mode }) => (mode === 'light' ? 'gray-100' : 'gray-1000')});
+  background-color: ${({ mode }) => getBlocksColor(mode, { light: 'gray-100', dark: 'gray-1000' })};
   border-radius: var(--r4);
   gap: var(--s2);
 `;
@@ -26,15 +27,15 @@ export const StyledFillTab = styled(Tab)<ModeProp>`
   gap: var(--s2);
   align-self: stretch;
   cursor: pointer;
-  color: var(--${({ mode }) => (mode === 'light' ? 'gray-800' : 'gray-300')});
-  background-color: var(--transparent);
+  color: ${({ mode }) => getBlocksColor(mode, { light: 'gray-800', dark: 'gray-300' })};
+  background-color: ${getBlocksColor('light', 'transparent')};
   border-radius: var(--r3);
   transition: background-color 0.3s, color 0.3s;
   border-bottom: none;
 
   &[data-selected] {
-    background-color: var(--${({ mode }) => (mode === 'light' ? 'white' : 'gray-800')});
-    color: var(--${({ mode }) => (mode === 'light' ? 'gray-1000' : 'white')});
+    background-color: ${({ mode }) => getBlocksColor(mode, { light: 'white', dark: 'gray-800' })};
+    color: ${({ mode }) => getBlocksColor(mode, { light: 'gray-1000', dark: 'white' })};
   }
 
   &:focus {
@@ -42,21 +43,21 @@ export const StyledFillTab = styled(Tab)<ModeProp>`
   }
 
   &:hover {
-    color: var(--${({ mode }) => (mode === 'light' ? 'gray-1000' : 'white')});
+    color: ${({ mode }) => getBlocksColor(mode, { light: 'gray-1000', dark: 'white' })};
   }
 
   &:focus-visible {
-    outline: 1px solid var(--pink-300);
+    outline: 1px solid ${getBlocksColor('light', 'pink-300')};
   }
 
   &:active {
-    background-color: var(--transparent);
-    color: var(--${({ mode }) => (mode === 'light' ? 'gray-1000' : 'white')});
+    background-color: ${getBlocksColor('light', 'transparent')};
+    color: ${({ mode }) => getBlocksColor(mode, { light: 'gray-1000', dark: 'white' })};
   }
 
   &[aria-disabled='true'] {
     cursor: not-allowed;
-    color: var(--${({ mode }) => (mode === 'light' ? 'gray-300' : 'gray-700')});
+    color: ${({ mode }) => getBlocksColor(mode, { light: 'gray-300', dark: 'gray-700' })};
     opacity: 1;
   }
 `;
@@ -69,10 +70,10 @@ export const StyledLineTabs = styled(ReachTabs)`
 
 export const StyledLineTabList = styled(TabList)<ModeProp>`
   display: flex;
-  background-color: var(--transparent);
+  background-color: ${getBlocksColor('light', 'transparent')};
   gap: var(--s3);
   justify-content: flex-start;
-  border-bottom: 1px solid var(--${({ mode }) => (mode === 'light' ? 'gray-200' : 'gray-800')});
+  border-bottom: 1px solid ${({ mode }) => getBlocksColor(mode, { light: 'gray-200', dark: 'gray-800' })};
 `;
 
 export const StyledLineTab = styled(Tab)<ModeProp>`
@@ -84,36 +85,36 @@ export const StyledLineTab = styled(Tab)<ModeProp>`
   gap: var(--s2);
   cursor: pointer;
   margin-bottom: -1px;
-  background-color: var(--transparent);
-  color: var(--${({ mode }) => (mode === 'light' ? 'gray-800' : 'gray-300')});
+  background-color: ${getBlocksColor('light', 'transparent')};
+  color: ${({ mode }) => getBlocksColor(mode, { light: 'gray-800', dark: 'gray-300' })};
   transition: background-color 0.3s, color 0.3s;
-  border-bottom: 2px solid var(--transparent);
+  border-bottom: 2px solid ${getBlocksColor('light', 'transparent')};
 
   &[data-selected] {
-    border-bottom: 2px solid var(--pink-400);
-    color: var(--${({ mode }) => (mode === 'light' ? 'gray-1000' : 'gray-100')});
+    border-bottom: 2px solid ${getBlocksColor('light', 'pink-400')};
+    color: ${({ mode }) => getBlocksColor(mode, { light: 'gray-1000', dark: 'gray-100' })};
   }
 
   &:hover {
-    color: var(--${({ mode }) => (mode === 'light' ? 'gray-1000' : 'gray-100')});
+    color: ${({ mode }) => getBlocksColor(mode, { light: 'gray-1000', dark: 'gray-100' })};
   }
 
   &:focus-visible {
-    outline: 2px solid var(--pink-300);
-    border-bottom: 2px solid var(--transparent);
+    outline: 2px solid ${getBlocksColor('light', 'pink-300')};
+    border-bottom: 2px solid ${getBlocksColor('light', 'transparent')};
     border-radius: var(--r3);
     margin-bottom: -2px;
   }
 
   &:active {
-    background-color: var(--transparent);
-    color: var(--gray-1000);
+    background-color: ${getBlocksColor('light', 'transparent')};
+    color: ${getBlocksColor('light', 'gray-1000')};
   }
 
   &[aria-disabled='true'] {
     cursor: not-allowed;
-    color: var(--${({ mode }) => (mode === 'light' ? 'gray-400' : 'gray-700')});
-    border-bottom: 2px solid var(--${({ mode }) => (mode === 'light' ? 'gray-300' : 'gray-800')});
+    color: ${({ mode }) => getBlocksColor(mode, { light: 'gray-400', dark: 'gray-700' })};
+    border-bottom: 2px solid ${({ mode }) => getBlocksColor(mode, { light: 'gray-300', dark: 'gray-800' })};
     opacity: 1;
   }
 `;
