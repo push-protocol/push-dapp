584e742992766b4cf81f3337fbfee15374ffbfe7
Monalisha Mishra <mishramonalisha76@gmail.com>
2024-07-05 19:25:04 +0530

    added color and leftover changes

--- src/blocks/textInput/TextInput.constants.tsx
+++ src/blocks/textInput/TextInput.constants.tsx
@@ -1,26 +1,43 @@
 import { TextInputStyles } from './TextInput.types';
 
-//optimisation needed for this part
-export const backgroundColor: TextInputStyles = {
-  error: { light: 'red-100', dark: 'red-100' },
+const backgroundColor: TextInputStyles = {
+  error: { light: 'red-100', dark: 'gray-800' },
+  success: { light: 'green-100', dark: 'gray-800' },
   disabled: { light: 'gray-200', dark: 'gray-800' },
   default: { light: 'white', dark: 'gray-800' },
 };
 
-export const borderColor: TextInputStyles = {
-  error: { light: 'red-400', dark: 'red-400' },
+const borderColor: TextInputStyles = {
+  error: { light: 'red-400', dark: 'red-500' },
+  success: { light: 'green-500', dark: 'green-400' },
   disabled: { light: 'gray-300', dark: 'gray-900' },
   default: { light: 'gray-200', dark: 'gray-800' },
 };
 
-export const textColor: TextInputStyles = {
-  error: { light: 'red-700', dark: 'red-700' },
+const textColor: TextInputStyles = {
+  error: { light: 'gray-1000', dark: 'gray-100' },
+  success: { light: 'gray-1000', dark: 'gray-100' },
   disabled: { light: 'gray-400', dark: 'gray-700' },
   default: { light: 'gray-1000', dark: 'gray-100' },
 };
 
-export const placeholderColor: TextInputStyles = {
-  error: { light: 'red-600', dark: 'red-600' },
+const descriptionColor: TextInputStyles = {
+  error: { light: 'gray-1000', dark: 'gray-100' },
+  success: { light: 'gray-1000', dark: 'gray-100' },
+  disabled: { light: 'gray-400', dark: 'gray-600' },
+  default: { light: 'gray-400', dark: 'gray-600' },
+};
+const iconColor: TextInputStyles = {
+  error: { light: 'red-700', dark: 'red-500' },
+  success: { light: 'green-600', dark: 'green-400' },
   disabled: { light: 'gray-400', dark: 'gray-700' },
-  default: { light: 'gray-400', dark: 'gray-500' },
+  default: { light: 'gray-300', dark: 'gray-700' },
+};
+
+export const textInputColor = {
+  backgroundColor: backgroundColor,
+  borderColor: borderColor,
+  textColor: textColor,
+  iconColor: iconColor,
+  descriptionColor: descriptionColor,
 };
