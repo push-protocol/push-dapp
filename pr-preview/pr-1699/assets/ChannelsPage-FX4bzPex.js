4b0775a8f6e8f8edc53e287097172a8f9cf020cf
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-07-08 15:18:13 +0000

    Deploy preview for PR 1699 🛫

---
+++ pr-preview/pr-1699/assets/ChannelsPage-FX4bzPex.js
@@ -0,0 +1,5 @@
+import{u as n,m as a,q as o,S as m,__tla as i}from"./index-C5B9sbv2.js";import{C as h,__tla as f}from"./ChannelsModule-D30XRcFn.js";import{__tla as u}from"./DisplayNotice-C066HdS7.js";import{__tla as p}from"./Skeleton-2vfuM0WL.js";import{__tla as y}from"./cloneDeep-DV0uZRoK.js";import{__tla as d}from"./index.esm-8X20WmG6.js";import{__tla as x}from"./RedCircle-DmhNDY3M.js";let r,T=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return x}catch{}})()]).then(async()=>{let t;r=({loadTeaser:_,playTeaser:l,channelID:e})=>{let{channelid:s}=n();const c=s||e;return a.jsx(t,{children:a.jsx(h,{channelID:c,loadTeaser:_,playTeaser:l})})},t=o(m)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+`});export{T as __tla,r as default};
---
+++ pr-preview/pr-1699/assets/SendNotifsPage-BHiSOlrI.js
@@ -0,0 +1,44 @@
+import{q as s,c_ as t,c$ as l,d0 as D,d2 as m,dc as u,x as N,dd as T,R as a,m as e,de as h,n as L,z as _,S as f,__tla as $}from"./index-C5B9sbv2.js";import{S as x,__tla as O}from"./SendNotifications-FG7uQFrz.js";import{__tla as p}from"./index-Bf8Eeu0X.js";import{__tla as R}from"./Tag-BVTXktKI.js";import{__tla as g}from"./TransitionGroupContext-Bdkt4NDJ.js";let d,G=Promise.all([(()=>{try{return $}catch{}})(),(()=>{try{return O}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return R}catch{}})(),(()=>{try{return g}catch{}})()]).then(async()=>{_.coreContractChain;function A(){m.pageview("/send_notificaiton"),u(),N(),T(n=>n.contracts),T(n=>n.admin),_.coreContractChain;const[r,J]=a.useState(2),[M,y]=a.useState(!0);a.useState(null),a.useState(null);const[o,j]=a.useState(!0);a.useState([]);const[E,I]=a.useState(null),U=()=>I(null);return a.useEffect(()=>{E&&U()},[E]),e.jsx(i,{children:r===2&&M?e.jsxs(e.Fragment,{children:[!o&&e.jsx(h,{replace:!0,to:"/dashboard"}),o&&e.jsx(x,{})]}):e.jsx(S,{children:"Channel details are being loaded, please wait\u2026"})})}let i,S,c;i=s(L)`
+  align-items: center;
+  align-self: center;
+  background: ${r=>r.theme.default.bg};
+  border-radius: ${t.ADJUSTMENTS.RADIUS.LARGE} ${t.ADJUSTMENTS.RADIUS.LARGE}
+    ${t.ADJUSTMENTS.RADIUS.LARGE} ${t.ADJUSTMENTS.RADIUS.LARGE};
+  box-shadow: ${t.ADJUSTMENTS.MODULE_BOX_SHADOW};
+  display: flex;
+  flex-direction: column;
+  flex: initial;
+  justify-content: center;
+  max-width: 1200px;
+  width: calc(100% - ${l.MINI_MODULES.DESKTOP.RIGHT} - ${l.MINI_MODULES.DESKTOP.LEFT});
+  position: relative;
+  margin: ${t.ADJUSTMENTS.MARGIN.MINI_MODULES.DESKTOP};
+
+  @media ${D.laptop} {
+    margin: ${t.ADJUSTMENTS.MARGIN.MINI_MODULES.TABLET};
+  }
+
+  @media ${D.mobileL} {
+    margin: ${t.ADJUSTMENTS.MARGIN.BIG_MODULES.MOBILE};
+    padding: ${t.ADJUSTMENTS.PADDING.DEFAULT};
+    width: calc(
+      100% - ${l.MINI_MODULES.MOBILE.RIGHT} - ${l.MINI_MODULES.MOBILE.LEFT} -
+        ${t.ADJUSTMENTS.PADDING.DEFAULT} - ${t.ADJUSTMENTS.PADDING.DEFAULT}
+    );
+    min-height: calc(100vh - ${t.CONSTANTS.HEADER_HEIGHT}px - ${l.BIG_MODULES.MOBILE.TOP});
+    overflow-y: scroll;
+    border-radius: ${t.ADJUSTMENTS.RADIUS.LARGE} ${t.ADJUSTMENTS.RADIUS.LARGE} 0 0;
+  }
+`,S=s.div`
+  width: 100%;
+  padding: 40px;
+  font-size: 1.5em;
+  font-weight: 300;
+  text-align: center;
+  color: ${r=>r.theme.color};
+`,d=()=>e.jsx(c,{children:e.jsx(A,{})}),c=s(f)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+  justify-content: flex-start;
+`});export{G as __tla,d as default};
