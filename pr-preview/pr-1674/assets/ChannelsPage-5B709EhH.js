7b504ac2a1681c540a444308310632cd9bb17b15
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-07-10 08:28:43 +0000

    Remove preview for PR 1674 🛬

--- pr-preview/pr-1674/assets/ChannelsPage-5B709EhH.js
+++
@@ -1,5 +0,0 @@
-import{u as n,m as a,q as o,S as m,__tla as i}from"./index-B2pKDXzh.js";import{C as h,__tla as f}from"./ChannelsModule-Bz04OlWR.js";import{__tla as u}from"./DisplayNotice-DTXEpuYL.js";import{__tla as p}from"./Skeleton-DwPGFHsF.js";import{__tla as y}from"./cloneDeep-Cpxza9uX.js";import{__tla as d}from"./index.esm-ofc_ZlUb.js";import{__tla as x}from"./RedCircle-CpvGVN84.js";let r,T=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return x}catch{}})()]).then(async()=>{let t;r=({loadTeaser:_,playTeaser:l,channelID:e})=>{let{channelid:s}=n();const c=s||e;return a.jsx(t,{children:a.jsx(h,{channelID:c,loadTeaser:_,playTeaser:l})})},t=o(m)`
-  flex: 1;
-  flex-direction: column;
-  align-self: stretch;
-`});export{T as __tla,r as default};
