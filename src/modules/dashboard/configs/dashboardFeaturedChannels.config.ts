0c92a23938cb9393aebc9fc8c843ac3957785d69
Abhishek <77395788+abhishek-01k@users.noreply.github.com>
2024-07-10 12:09:01 +0530

    Carousel is Implemented in the Featured Channels Sections in dashboard (#1702)
    * Featured Channels Carousel is implemented
    
    * fixes done
    
    * Fixed the responsive issue
    
    ---------
    
    Co-authored-by: rohitmalhotra1420 <rohit.malhotra1420@gmail.com>

--- src/modules/dashboard/configs/dashboardFeaturedChannels.config.ts
+++ src/modules/dashboard/configs/dashboardFeaturedChannels.config.ts
@@ -8,157 +8,157 @@ export type FeaturedChannelDetailsProps = {
 const devFeaturedChannelsList: FeaturedChannelDetailsProps[] = [
   {
     id: 1,
-    channel: '0x74415Bc4C4Bf4Baecc2DD372426F0a1D016Fa924',
+    channel: '0x74415Bc4C4Bf4Baecc2DD372426F0a1D016Fa924'
   },
   {
     id: 2,
-    channel: '0x0000000000000000000000000000000000000000',
+    channel: '0x0000000000000000000000000000000000000000'
   },
   {
     id: 3,
-    channel: '0x778D3206374f8AC265728E18E3fE2Ae6b93E4ce4',
+    channel: '0x778D3206374f8AC265728E18E3fE2Ae6b93E4ce4'
   },
   {
     id: 4,
-    channel: '0xf9dF4b44Bb6BAf88074bb97C654bec0e4f137fE6',
+    channel: '0xf9dF4b44Bb6BAf88074bb97C654bec0e4f137fE6'
   },
   {
     id: 5,
-    channel: '0x35B84d6848D16415177c64D64504663b998A6ab4',
+    channel: '0x35B84d6848D16415177c64D64504663b998A6ab4'
   },
   {
     id: 6,
-    channel: '0x4e97Bc6129dfe5b4b5f527049BC18DcC4925Fd62',
+    channel: '0x4e97Bc6129dfe5b4b5f527049BC18DcC4925Fd62'
   },
   {
     id: 7,
-    channel: '0xD8634C39BBFd4033c0d3289C4515275102423681',
+    channel: '0xD8634C39BBFd4033c0d3289C4515275102423681'
   },
   {
     id: 8,
-    channel: '0xfFA1aF9E558B68bBC09ad74058331c100C135280',
+    channel: '0xfFA1aF9E558B68bBC09ad74058331c100C135280'
   },
   {
     id: 9,
-    channel: '0x2177cFc66474bBEce7Cbf114d780A5cfE78485De',
-  },
+    channel: '0x2177cFc66474bBEce7Cbf114d780A5cfE78485De'
+  }
 ];
 
 const stagingFeaturedChannelsList: FeaturedChannelDetailsProps[] = [
   {
     id: 1,
-    channel: '0x74415Bc4C4Bf4Baecc2DD372426F0a1D016Fa924',
+    channel: '0x74415Bc4C4Bf4Baecc2DD372426F0a1D016Fa924'
   },
   {
     id: 2,
-    channel: '0x9601f08b9EcB981D273B72e7f33964Cb98f977fe',
+    channel: '0x9601f08b9EcB981D273B72e7f33964Cb98f977fe'
   },
   {
     id: 3,
-    channel: '0x94c3016ef3e503774630fC71F59B8Da9f7D470B7',
+    channel: '0x94c3016ef3e503774630fC71F59B8Da9f7D470B7'
   },
   {
     id: 4,
-    channel: '0xa1016081D6Da53b4246178eD83922C55F7171e54',
+    channel: '0xa1016081D6Da53b4246178eD83922C55F7171e54'
   },
   {
     id: 5,
-    channel: '0x08D77bD7500a07d791dD1323919C22e1FDb72224',
+    channel: '0x08D77bD7500a07d791dD1323919C22e1FDb72224'
   },
   {
     id: 6,
-    channel: '0x463308c3CF49C0C7947D4fA5C94E2077a6572068',
+    channel: '0x463308c3CF49C0C7947D4fA5C94E2077a6572068'
   },
   {
     id: 7,
-    channel: '0x0000000000000000000000000000000000000000',
+    channel: '0x0000000000000000000000000000000000000000'
   },
   {
     id: 8,
-    channel: '0x5ac9E6205eACA2bBbA6eF716FD9AabD76326EEee',
+    channel: '0x5ac9E6205eACA2bBbA6eF716FD9AabD76326EEee'
   },
   {
     id: 9,
-    channel: '0xD8634C39BBFd4033c0d3289C4515275102423681',
-  },
+    channel: '0xD8634C39BBFd4033c0d3289C4515275102423681'
+  }
 ];
 
 const prodFeaturedChannelsList: FeaturedChannelDetailsProps[] = [
   {
     id: 1,
-    channel: '0xB88460Bb2696CAb9D66013A05dFF29a28330689D',
+    channel: '0x0f0aE1ceEBc4b5aB14A47202eD6A52D3ef698b5B'
   },
   {
     id: 2,
-    channel: '0x0f0aE1ceEBc4b5aB14A47202eD6A52D3ef698b5B',
+    channel: '0x48De2669db5C2da4bf36C29ef9CFd62057B4b6C8'
   },
   {
     id: 3,
-    channel: '0xe56f1D3EDFFF1f25855aEF744caFE7991c224FFF',
+    channel: '0xe56f1D3EDFFF1f25855aEF744caFE7991c224FFF'
   },
   {
     id: 4,
-    channel: '0x90A48D5CF7343B08dA12E067680B4C6dbfE551Be',
+    channel: '0x4E15B14B9950A04370E36f2Ec05546ED5867ADeF'
   },
   {
     id: 5,
-    channel: '0xDBc5936E4daaE94F415C39D284f6a69c4d553F2F',
+    channel: '0xdbBc2Ac8cb8D02B26F165b4BC120fd4b14DA6cDA'
   },
   {
     id: 6,
-    channel: '0x2B8ffb4460550Dbe8Ec1cEA9C1B61322dB56B082',
+    channel: '0x1a9dDEf485674fC60a269cf97FAbc6b8728A3497'
   },
   {
     id: 7,
-    channel: '0x26C8C84adDA6dbA89E93f139D48c0E39B3a6A8fF',
+    channel: '0x76bA9825A5F707F133124E4608F1F2Dd1EF4006a'
   },
   {
     id: 8,
-    channel: '0xef6426D522CfE5B7Ae5dB05623aB0Ef78023dBe0',
+    channel: '0xef6426D522CfE5B7Ae5dB05623aB0Ef78023dBe0'
   },
   {
     id: 9,
-    channel: '0x03EAAAa48ea78d1E66eA3458364d553AD981871E',
-  },
+    channel: '0xde3e447E125FA2391DC9BCbfA0B821424422FEAF'
+  }
 ];
 
 const alphaFeaturedChannelsList: FeaturedChannelDetailsProps[] = [
   {
     id: 1,
-    channel: '0xB88460Bb2696CAb9D66013A05dFF29a28330689D',
+    channel: '0x0f0aE1ceEBc4b5aB14A47202eD6A52D3ef698b5B'
   },
   {
     id: 2,
-    channel: '0x0f0aE1ceEBc4b5aB14A47202eD6A52D3ef698b5B',
+    channel: '0x48De2669db5C2da4bf36C29ef9CFd62057B4b6C8'
   },
   {
     id: 3,
-    channel: '0xe56f1D3EDFFF1f25855aEF744caFE7991c224FFF',
+    channel: '0xe56f1D3EDFFF1f25855aEF744caFE7991c224FFF'
   },
   {
     id: 4,
-    channel: '0x90A48D5CF7343B08dA12E067680B4C6dbfE551Be',
+    channel: '0x4E15B14B9950A04370E36f2Ec05546ED5867ADeF'
   },
   {
     id: 5,
-    channel: '0xDBc5936E4daaE94F415C39D284f6a69c4d553F2F',
+    channel: '0xdbBc2Ac8cb8D02B26F165b4BC120fd4b14DA6cDA'
   },
   {
     id: 6,
-    channel: '0x2B8ffb4460550Dbe8Ec1cEA9C1B61322dB56B082',
+    channel: '0x1a9dDEf485674fC60a269cf97FAbc6b8728A3497'
   },
   {
     id: 7,
-    channel: '0x26C8C84adDA6dbA89E93f139D48c0E39B3a6A8fF',
+    channel: '0x76bA9825A5F707F133124E4608F1F2Dd1EF4006a'
   },
   {
     id: 8,
-    channel: '0xef6426D522CfE5B7Ae5dB05623aB0Ef78023dBe0',
+    channel: '0xef6426D522CfE5B7Ae5dB05623aB0Ef78023dBe0'
   },
   {
     id: 9,
-    channel: '0x03EAAAa48ea78d1E66eA3458364d553AD981871E',
-  },
+    channel: '0xde3e447E125FA2391DC9BCbfA0B821424422FEAF'
+  }
 ];
 
 export const featuredChannelsList: {
@@ -167,7 +167,7 @@ export const featuredChannelsList: {
   dev: devFeaturedChannelsList,
   staging: stagingFeaturedChannelsList,
   prod: prodFeaturedChannelsList,
-  alpha: alphaFeaturedChannelsList,
+  alpha: alphaFeaturedChannelsList
 };
 
 export const mobileFeaturedChannelsList: FeaturedChannelDetailsProps[][] = featuredChannelsList[
---
+++ src/modules/dashboard/hooks/useFeaturedChannelsCarouselButtons.ts
@@ -0,0 +1,45 @@
+import { EmblaCarouselType } from 'embla-carousel';
+import { useCallback, useEffect, useState } from 'react';
+
+type useFeaturedChannelsCarouselButtonsType = {
+  prevBtnDisabled: boolean;
+  nextBtnDisabled: boolean;
+  onPrevButtonClick: () => void;
+  onNextButtonClick: () => void;
+};
+
+export const useFeaturedChannelsCarouselButtons = (
+  emblaApi: EmblaCarouselType | undefined
+): useFeaturedChannelsCarouselButtonsType => {
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+
+  const onPrevButtonClick = useCallback(() => {
+    if (!emblaApi) return;
+    emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const onNextButtonClick = useCallback(() => {
+    if (!emblaApi) return;
+    emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onSelect(emblaApi);
+    emblaApi.on('reInit', onSelect).on('select', onSelect);
+  }, [emblaApi, onSelect]);
+
+  return {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  };
+};
