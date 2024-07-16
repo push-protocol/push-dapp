e2d04f816002372ca4983cfb43abcdabbd1df8ea
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-07-16 07:32:44 +0000

    Remove preview for PR 1732 🛬

--- pr-preview/pr-1732/assets/ContentLayout-D8ClWdSj.js
+++
@@ -1,4 +0,0 @@
-import{m as e,B as l,D as n,__tla as r}from"./index-DojJxtvD.js";let t,i=Promise.all([(()=>{try{return r}catch{}})()]).then(async()=>{t=({children:a})=>e.jsx(l,{alignItems:"center",display:"flex",flexDirection:"column",justifyContent:"center",alignSelf:"center",maxWidth:"1200px",backgroundColor:"transparent",width:"calc(100% - (var(--s4) * 2))",css:n`
-        flex: initial;
-        margin: 0 0 auto 0;
-      `,children:a})});export{t as C,i as __tla};
