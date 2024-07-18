3f986ff4f4a2632406fffc6c9b4e8aff6d6193fd
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-07-18 15:48:09 +0000

    Remove preview for PR 1757 🛬

--- pr-preview/pr-1757/assets/ContentLayout-X3vdNObm.js
+++
@@ -1,4 +0,0 @@
-import{aZ as e,bf as l,bg as n,__tla as r}from"./index-BH6Jm5Rc.js";let a,i=Promise.all([(()=>{try{return r}catch{}})()]).then(async()=>{a=({children:t})=>e.jsx(l,{alignItems:"center",display:"flex",flexDirection:"column",justifyContent:"center",alignSelf:"center",maxWidth:"1200px",backgroundColor:"transparent",width:"calc(100% - (var(--s4) * 2))",css:n`
-        flex: initial;
-        margin: 0 0 auto 0;
-      `,children:t})});export{a as C,i as __tla};
--- pr-preview/pr-1757/assets/ashwani@2x-DKGDCSIY.js
+++
@@ -1,1 +0,0 @@
-const p="/push-dapp/pr-preview/pr-1757/assets/ashwani@2x-BxMepywI.jpg";export{p as default};
