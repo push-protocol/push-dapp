1bd9c4fb9aa4ad74e9c1e35176e54e25385bd0a9
kalashshah <kalashshah@users.noreply.github.com>
2024-07-18 15:35:01 +0000

    Deploy preview for PR 1739 🛫

---
+++ pr-preview/pr-1739/assets/ContentLayout-DEHr_LaA.js
@@ -0,0 +1,4 @@
+import{aZ as e,bf as l,bg as n,__tla as r}from"./index-DDt7gM4Z.js";let a,i=Promise.all([(()=>{try{return r}catch{}})()]).then(async()=>{a=({children:t})=>e.jsx(l,{alignItems:"center",display:"flex",flexDirection:"column",justifyContent:"center",alignSelf:"center",maxWidth:"1200px",backgroundColor:"transparent",width:"calc(100% - (var(--s4) * 2))",css:n`
+        flex: initial;
+        margin: 0 0 auto 0;
+      `,children:t})});export{a as C,i as __tla};
