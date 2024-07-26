9f8924f20f12844971ffebc0019b95015fc9fc23
abhishek-01k <abhishek@push.org>
2024-07-26 18:52:06 +0530

    Added light and dark mode to listItem semantic

--- src/blocks/theme/semantics/semantics.listItem.ts
+++ src/blocks/theme/semantics/semantics.listItem.ts
@@ -4,22 +4,34 @@ import { surfaceSemantics } from './semantics.surface';
 import { textSemantics } from './semantics.text';
 
 export const listItemSemantics = {
-  'background-default': { light: surfaceSemantics['primary'], dark: surfaceSemantics['primary'] },
-  'background-hover': { light: surfaceSemantics['secondary'].light, dark: surfaceSemantics['secondary'].dark },
+  'background-default': {
+    light: surfaceSemantics['primary'].light,
+    dark: surfaceSemantics['primary'].dark
+  },
+  'background-hover': {
+    light: surfaceSemantics['secondary'].light,
+    dark: surfaceSemantics['secondary'].dark
+  },
 
   'text-default': { light: textSemantics['primary'].light, dark: textSemantics['primary'].dark },
   'text-success': {
     light: textSemantics['state-success-bold'].light,
     dark: textSemantics['state-success-subtle'].dark
   },
-  'text-error': { light: textSemantics['state-danger-bold'].light, dark: textSemantics['state-danger-bold'].dark },
+  'text-error': {
+    light: textSemantics['state-danger-bold'].light,
+    dark: textSemantics['state-danger-bold'].dark
+  },
 
   'icon-default': { light: iconSemantics['primary'].light, dark: iconSemantics['primary'].dark },
   'icon-success': {
     light: iconSemantics['state-success-bold'].light,
     dark: iconSemantics['state-success-bold'].dark
   },
-  'icon-error': { light: iconSemantics['state-danger-bold'].light, dark: iconSemantics['state-danger-bold'].dark },
+  'icon-error': {
+    light: iconSemantics['state-danger-bold'].light,
+    dark: iconSemantics['state-danger-bold'].dark
+  },
 
   'stroke-default': { light: strokeSemantics['secondary'].light, dark: strokeSemantics['secondary'].dark }
 };
