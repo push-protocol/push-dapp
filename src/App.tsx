eca60a9ac05302b219076c8eb02e5b23fccfe5c2
corlard3y <corlardey@gmail.com>
2024-07-27 13:38:55 +0100

    unlock profile

--- src/App.tsx
+++ src/App.tsx
@@ -350,7 +350,7 @@ export default function App() {
           <ChatUIProvider
             user={userPushSDKInstance}
             theme={darkMode && darkChatTheme}
-            debug={true}
+            debug={false}
             uiConfig={{
               suppressToast: false,
             }}
