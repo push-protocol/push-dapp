7f932e1db4cab3793dea433a94f2a8342c4a8aae
corlard3y <corlardey@gmail.com>
2024-07-28 19:29:05 +0100

    update useDailyRewards hook

--- src/modules/rewards/components/DailyRewardsSection.tsx
+++ src/modules/rewards/components/DailyRewardsSection.tsx
@@ -1,123 +1,31 @@
 // React and other libraries
-import { FC, useCallback, useEffect, useState } from 'react';
-
-// third party libraries
+import { FC } from 'react';
 import { css } from 'styled-components';
 
 // hooks
-import { useGetRewardsActivities, useGetUserRewardsDetails, useSendRecentActivities } from 'queries';
-import { useAccount } from 'hooks';
-
-//helpers
-import { walletToCAIP10 } from 'helpers/w2w';
-import { checkTimeToCurrent, getActivityStatus, getDayNumber } from '../utils/getDailyActivityStatus';
+import useLockedStatus from '../hooks/useLockedStatus';
+import useDailyRewards from '../hooks/useDailyRewards';
 
 // components
 import { Box, Button, Text } from 'blocks';
 import { DailyRewardsItem } from './DailyRewardsItem';
 import { ActivityVerificationButton } from './ActivityVerificationButton';
-import useLockedStatus from '../hooks/useLockedStatus';
 
 export type DailyRewardsSectionProps = {};
 
 const DailyRewardsSection: FC<DailyRewardsSectionProps> = () => {
-  const { account, isWalletConnected } = useAccount();
-  const [activeItem, setActiveItem] = useState<any | null>(null);
-  const [activeDay, setActiveDay] = useState<number>(0);
-  const [isActivityDisabled, setIsActivityDisabled] = useState(false);
-
-  const [errorMessage, setErrorMessage] = useState('');
-  const [isLoadingRewards, setIsLoadingRewards] = useState(false);
-
-  // Getting user Id by wallet address
-  const caip10WalletAddress = walletToCAIP10({ account });
-  const { data: userDetails } = useGetUserRewardsDetails({
-    caip10WalletAddress: caip10WalletAddress,
-  });
-  const { data: rewardActivitiesResponse, isLoading: isLoadingActivities } = useGetRewardsActivities({ pageSize: 50 });
-
-  const isLoading = isLoadingActivities;
-
-  // If there are activities then render them else render 2 skeletons
-  const activityList = isLoading
-    ? Array(7).fill(0)
-    : rewardActivitiesResponse?.pages.flatMap((page) => page.activities) || [];
-
-  // Filter activities based on the index
-  const dailyActivities = activityList.filter((activity) => activity.index < 0);
-
-  // Sort the activities based on the extracted day number
-  const dailyRewardsActivities = dailyActivities?.sort((a, b) => getDayNumber(a) - getDayNumber(b));
+  const {
+    activeItem,
+    activeDay,
+    isActivityDisabled,
+    isLoadingRewards,
+    userDetails,
+    dailyRewardsActivities,
+    handleCheckIn,
+    setErrorMessage,
+  } = useDailyRewards();
 
   const { isLocked } = useLockedStatus();
-
-  const { mutate: sendRecentActivities } = useSendRecentActivities({
-    userId: userDetails?.userId as string,
-  });
-
-  const resetState = useCallback(() => {
-    setActiveDay(0);
-    setActiveItem(null);
-    setIsActivityDisabled(false);
-    setIsLoadingRewards(false);
-  }, []);
-
-  useEffect(() => {
-    if (dailyRewardsActivities.length > 0 && isWalletConnected && userDetails?.userId) {
-      setIsLoadingRewards(true);
-      handleCheckIn();
-    }
-
-    if (!isWalletConnected) {
-      resetState();
-    }
-  }, [userDetails?.userId, isWalletConnected, account]);
-
-  const handleCheckIn = () => {
-    if (userDetails?.userId == undefined) return;
-    // Extract activityType into an array
-    const activityTitles = dailyRewardsActivities?.map((activity) => activity.activityType);
-
-    sendRecentActivities(
-      {
-        userId: userDetails?.userId as string,
-        activities: activityTitles,
-      },
-      {
-        onSuccess: (data) => {
-          handleSuccess(data);
-        },
-        onError: (err) => {
-          console.error('Error', err);
-        },
-      }
-    );
-  };
-
-  const handleSuccess = (data: any) => {
-    let dataActivity = data?.activities;
-    const { isEmpty, firstEmptyActivity, latestActivityKey } = getActivityStatus(data?.activities);
-
-    const targetActivity = isEmpty
-      ? dailyRewardsActivities?.find((activity) => activity.activityType === firstEmptyActivity)
-      : dailyRewardsActivities?.find((activity) => activity.activityType === latestActivityKey);
-
-    const newDay = isEmpty ? 1 : getDayNumber(targetActivity) + 1;
-    const newDayData = dailyRewardsActivities?.find(
-      (activity) => activity.activityType === `daily_check_in_7_days_day${newDay}`
-    );
-
-    if (latestActivityKey && !isEmpty) {
-      const number = checkTimeToCurrent(dataActivity?.[latestActivityKey]?.updatedAt);
-      if (number) {
-        setIsActivityDisabled(true);
-      }
-    }
-
-    setActiveDay(newDay);
-    setActiveItem(newDayData);
-    setIsLoadingRewards(false);
-  };
 
   return (
     <Box
---
+++ src/modules/rewards/hooks/useDailyRewards.tsx
@@ -0,0 +1,124 @@
+// React and other libraries
+import { useCallback, useEffect, useState } from 'react';
+
+// hooks
+import { useGetRewardsActivities, useGetUserRewardsDetails, useSendRecentActivities } from 'queries';
+import { useAccount } from 'hooks';
+
+// helpers
+import { walletToCAIP10 } from 'helpers/w2w';
+import { checkTimeToCurrent, getActivityStatus, getDayNumber } from '../utils/getDailyActivityStatus';
+
+const useDailyRewards = () => {
+  const { account, isWalletConnected } = useAccount();
+
+  // State variables
+  const [activeItem, setActiveItem] = useState<any | null>(null);
+  const [activeDay, setActiveDay] = useState<number>(0);
+  const [isActivityDisabled, setIsActivityDisabled] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isLoadingRewards, setIsLoadingRewards] = useState(false);
+
+  // Getting user ID by wallet address
+  const caip10WalletAddress = walletToCAIP10({ account });
+  const { data: userDetails } = useGetUserRewardsDetails({
+    caip10WalletAddress,
+  });
+  const { data: rewardActivitiesResponse, isLoading: isLoadingActivities } = useGetRewardsActivities({ pageSize: 50 });
+
+  const isLoading = isLoadingActivities;
+
+  // Flatten the activities response and filter daily activities
+  const activityList = isLoading
+    ? Array(7).fill(0)
+    : rewardActivitiesResponse?.pages.flatMap((page) => page.activities) || [];
+  const dailyActivities = activityList.filter((activity) => activity.index < 0);
+  const dailyRewardsActivities = dailyActivities?.sort((a, b) => getDayNumber(a) - getDayNumber(b));
+
+  // Mutation for sending recent activities
+  const { mutate: sendRecentActivities } = useSendRecentActivities({
+    userId: userDetails?.userId as string,
+  });
+
+  // Reset state function
+  const resetState = useCallback(() => {
+    setActiveDay(0);
+    setActiveItem(null);
+    setIsActivityDisabled(false);
+    setIsLoadingRewards(false);
+  }, []);
+
+  // Effect for handling check-in when user details change or wallet is connected
+  useEffect(() => {
+    if (dailyRewardsActivities.length > 0 && isWalletConnected && userDetails?.userId) {
+      setIsLoadingRewards(true);
+      handleCheckIn();
+    }
+
+    if (!isWalletConnected) {
+      resetState();
+    }
+  }, [userDetails?.userId, isWalletConnected, account]);
+
+  // Handle check-in function
+  const handleCheckIn = () => {
+    if (!userDetails?.userId) return;
+
+    const activityTitles = dailyRewardsActivities?.map((activity) => activity.activityType);
+
+    sendRecentActivities(
+      {
+        userId: userDetails?.userId as string,
+        activities: activityTitles,
+      },
+      {
+        onSuccess: handleSuccess,
+        onError: (err) => {
+          console.error('Error', err);
+        },
+      }
+    );
+  };
+
+  // Handle success response from sending recent activities
+  const handleSuccess = (data: any) => {
+    const { activities: dataActivity } = data;
+    const { isEmpty, firstEmptyActivity, latestActivityKey } = getActivityStatus(dataActivity);
+
+    const targetActivity = isEmpty
+      ? dailyRewardsActivities?.find((activity) => activity.activityType === firstEmptyActivity)
+      : dailyRewardsActivities?.find((activity) => activity.activityType === latestActivityKey);
+
+    const newDay = isEmpty ? 1 : getDayNumber(targetActivity) + 1;
+    const newDayData = dailyRewardsActivities?.find(
+      (activity) => activity.activityType === `daily_check_in_7_days_day${newDay}`
+    );
+
+    if (latestActivityKey && !isEmpty) {
+      const number = checkTimeToCurrent(dataActivity?.[latestActivityKey]?.updatedAt);
+      if (number) {
+        setIsActivityDisabled(true);
+      }
+    }
+
+    setActiveDay(newDay);
+    setActiveItem(newDayData);
+    setIsLoadingRewards(false);
+  };
+
+  return {
+    account,
+    activeItem,
+    activeDay,
+    isActivityDisabled,
+    errorMessage,
+    isLoadingRewards,
+    userDetails,
+    dailyRewardsActivities,
+    handleCheckIn,
+    setErrorMessage,
+    resetState,
+  };
+};
+
+export default useDailyRewards;
