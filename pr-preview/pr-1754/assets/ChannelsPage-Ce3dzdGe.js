5b9803f779f7b433d01f4aa29e166c67d4b04486
corlard3y <corlard3y@users.noreply.github.com>
2024-07-30 11:26:18 +0000

    Deploy preview for PR 1754 🛫

---
+++ pr-preview/pr-1754/assets/ChannelsPage-Ce3dzdGe.js
@@ -0,0 +1,5 @@
+import{aY as n,aZ as t,a_ as o,a$ as m,__tla as i}from"./index-c_sigsiS.js";import{C as h,__tla as f}from"./ChannelsModule-Cqo3EWld.js";import{__tla as p}from"./DisplayNotice-DWYAZ9Gh.js";import{__tla as u}from"./Skeleton-D-ovgRvT.js";import{__tla as y}from"./cloneDeep-BBUq1Lk-.js";import{__tla as d}from"./index.esm-BegVFwEa.js";import{__tla as x}from"./RedCircle-fcjIoTwO.js";let r,T=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return x}catch{}})()]).then(async()=>{let a;r=({loadTeaser:_,playTeaser:l,channelID:e})=>{let{channelid:s}=n();const c=s||e;return t.jsx(a,{children:t.jsx(h,{channelID:c,loadTeaser:_,playTeaser:l})})},a=o(m)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+`});export{T as __tla,r as default};
--- pr-preview/pr-1754/assets/ChannelsPage-Cnr_z3P_.js
+++
@@ -1,5 +0,0 @@
-import{aY as n,aZ as t,a_ as o,a$ as m,__tla as i}from"./index-BrLfrJER.js";import{C as h,__tla as f}from"./ChannelsModule-BWFleQfU.js";import{__tla as p}from"./DisplayNotice-BxZ9FskU.js";import{__tla as u}from"./Skeleton-BWJzPP8Q.js";import{__tla as y}from"./cloneDeep-DOJfqo3Y.js";import{__tla as d}from"./index.esm-lt6ZVAd3.js";import{__tla as x}from"./RedCircle-DOblDzuM.js";let r,T=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return x}catch{}})()]).then(async()=>{let a;r=({loadTeaser:_,playTeaser:l,channelID:e})=>{let{channelid:s}=n();const c=s||e;return t.jsx(a,{children:t.jsx(h,{channelID:c,loadTeaser:_,playTeaser:l})})},a=o(m)`
-  flex: 1;
-  flex-direction: column;
-  align-self: stretch;
-`});export{T as __tla,r as default};
