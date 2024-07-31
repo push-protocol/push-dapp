286d173ec77392d2f020bed104e71ab884f7e18f
abhishek-01k <abhishek-01k@users.noreply.github.com>
2024-07-31 05:31:08 +0000

    Deploy preview for PR 1782 🛫

---
+++ pr-preview/pr-1782/assets/LoaderSpinner-DiCRzKVm.js
@@ -0,0 +1,1 @@
+import{b1 as s,bs as r,__tla as e}from"./index-DogFBbdN.js";let a,l=Promise.all([(()=>{try{return e}catch{}})()]).then(async()=>{let t;t={height:"45px"},a=()=>s.jsx(r,{color:"#CF1C84",height:9,width:2.5,margin:0,css:t})});export{a as L,l as __tla};
