09f534a7ff3019334456117f7997d867c977f1a6
abhishek-01k <abhishek-01k@users.noreply.github.com>
2024-07-31 08:10:52 +0000

    Deploy preview for PR 1782 🛫

---
+++ pr-preview/pr-1782/assets/ChannelsPage-CIzCjezG.js
@@ -0,0 +1,5 @@
+import{b5 as n,b1 as a,b6 as o,b7 as m,__tla as i}from"./index-BytfNhuk.js";import{C as h,__tla as f}from"./ChannelsModule-B3F9ooZr.js";import{__tla as p}from"./DisplayNotice-D1F4rpyq.js";import{__tla as u}from"./Skeleton-DuTkXWsW.js";import{__tla as y}from"./cloneDeep-CTByuVIG.js";import{__tla as d}from"./index.esm-5AcgWvh7.js";import{__tla as b}from"./RedCircle-BnuxVKra.js";let r,x=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return b}catch{}})()]).then(async()=>{let t;r=({loadTeaser:_,playTeaser:l,channelID:e})=>{let{channelid:s}=n();const c=s||e;return a.jsx(t,{children:a.jsx(h,{channelID:c,loadTeaser:_,playTeaser:l})})},t=o(m)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+`});export{x as __tla,r as default};
