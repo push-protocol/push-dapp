282ade37a4549e81f518d65aab595ec454a375fe
abhishek-01k <abhishek@push.org>
2024-07-31 12:46:00 +0530

    Created New file for edit channel just UI

--- src/modules/channelDashboard/ChannelDashboard.tsx
+++ src/modules/channelDashboard/ChannelDashboard.tsx
@@ -1,19 +1,28 @@
 // React and other libraries
 import { useState } from 'react';
 
-import { Box } from "blocks";
+import { useAccount } from 'hooks';
 
+import { useGetChannelDetails } from 'queries';
+
+// Components
+import { Box } from 'blocks';
+import { ChannelAddSubgraph } from './components/ChannelAddSubgraph';
+import { ChannelAddDelegate } from './components/ChannelAddDelegate';
+import { ReactivateChannel } from './components/ReactivateChannel';
+import { DeactivateChannel } from './components/DeactivateChannel';
 import { UserChannelDashboard } from './components/UserChannelDashboard';
 
-import { DashboardActiveState } from "./ChannelDashboard.types";
-import { ChannelAddDelegate } from './components/ChannelAddDelegate';
-import { DeactivateChannel } from './components/DeactivateChannel';
-import { ChannelAddSubgraph } from './components/ChannelAddSubgraph';
-import { ReactivateChannel } from './components/ReactivateChannel';
+// Types
+import { DashboardActiveState } from './ChannelDashboard.types';
+import { EditChannelV2 } from 'modules/editChannel/EditChannelV2';
 
 const ChannelDashboard = () => {
 
+  const { account } = useAccount();
+
   const [activeState, setActiveState] = useState<DashboardActiveState>('dashboard');
+  const { data: channelDetails } = useGetChannelDetails(account);
 
   return (
     <Box>
@@ -34,6 +43,10 @@ const ChannelDashboard = () => {
 
       {activeState === 'deactivateChannel' && <DeactivateChannel setActiveState={setActiveState} />}
       {activeState === 'reactivateChannel' && <ReactivateChannel setActiveState={setActiveState} />}
+
+      {activeState === 'editChannel' && channelDetails && (
+        <EditChannelV2 setActiveState={setActiveState} channelDetails={channelDetails} />
+      )}
     </Box>
   );
 };
---
+++ src/modules/editChannel/EditChannel.form.tsx
@@ -0,0 +1,53 @@
+import { FC } from 'react';
+
+import { FormikProvider, useFormik, useFormikContext } from 'formik';
+import * as Yup from 'yup';
+
+import { ChannelDetails } from 'queries';
+
+import { getMaxCharLimitFieldMessage, getRequiredFieldMessage, URLRegex } from 'common/Common.form';
+import { EditChannelInfoFormValues } from './EditChannel.types';
+
+type EditChannelFormProviderProps = {
+  children: React.ReactNode;
+  channelDetails: ChannelDetails;
+};
+
+const EditChanelFormProvider: FC<EditChannelFormProviderProps> = ({ children, channelDetails }) => {
+  const channelFormValidation = Yup.object().shape({
+    channelName: Yup.string()
+      .required(getRequiredFieldMessage('Channel Name'))
+      .max(32, getMaxCharLimitFieldMessage(32)),
+    channelDesc: Yup.string()
+      .required(getRequiredFieldMessage('Channel Description'))
+      .max(250, getMaxCharLimitFieldMessage(250)),
+    channelURL: Yup.string()
+      .required(getRequiredFieldMessage('Channel URL'))
+      .test('url', 'Please enter a valid channel url', (value) => URLRegex.test(value))
+  });
+
+  const editChannelForm = useFormik<EditChannelInfoFormValues>({
+    initialValues: {
+      channelName: channelDetails.name,
+      channelDesc: channelDetails.info,
+      channelURL: channelDetails.url,
+      channelIcon: channelDetails.iconV2,
+      imageSrc: '',
+      imageType: ''
+    },
+    validationSchema: channelFormValidation,
+    onSubmit: () => { }
+  });
+
+  return <FormikProvider value={editChannelForm}>{children}</FormikProvider>;
+};
+
+const useEditChannelForm = () => {
+  const context = useFormikContext<EditChannelInfoFormValues>();
+  if (!context) {
+    throw new Error('useEditChannelFormikContext must be used within a FormikChainAliasProvider');
+  }
+  return context;
+};
+
+export { useEditChannelForm, EditChanelFormProvider };
