fd36c67d47f6171f56338b0563acb124513eb502
corlard3y <corlard3y@users.noreply.github.com>
2024-08-01 07:34:46 +0000

    Deploy preview for PR 1754 🛫

---
+++ pr-preview/pr-1754/assets/ReceiveNotifsPage-82nVoqZc.js
@@ -0,0 +1,31 @@
+import{a_ as l,bz as e,bB as n,bD as d,aZ as a,bE as o,bN as S,b3 as c,a$ as T,__tla as A}from"./index-DPiek688.js";let s,E=Promise.all([(()=>{try{return A}catch{}})()]).then(async()=>{function r(){return d.pageview("/receive"),a.jsx(t,{children:a.jsx(o,{alignItems:"stretch",children:a.jsx(S,{onClose:()=>{},closeEnabled:!1})})})}let t,i;t=l(c)`
+  align-items: center;
+  align-self: center;
+  background: ${D=>D.theme.default.bg};
+  border-radius: ${e.ADJUSTMENTS.RADIUS.LARGE};
+  box-shadow: ${e.ADJUSTMENTS.MODULE_BOX_SHADOW};
+  display: flex;
+  flex-direction: column;
+  flex: initial;
+  justify-content: center;
+  max-width: 1200px;
+  width: auto;
+  padding: ${e.ADJUSTMENTS.PADDING.BIG};
+  position: relative;
+  margin: ${e.ADJUSTMENTS.MARGIN.MINI_MODULES.DESKTOP};
+
+  @media ${n.laptop} {
+    margin: ${e.ADJUSTMENTS.MARGIN.MINI_MODULES.TABLET};
+    padding: ${e.ADJUSTMENTS.PADDING.DEFAULT};
+  }
+
+  @media ${n.mobileL} {
+    margin: ${e.ADJUSTMENTS.MARGIN.MINI_MODULES.MOBILE};
+    padding: ${e.ADJUSTMENTS.PADDING.DEFAULT};
+  }
+`,s=()=>a.jsx(i,{children:a.jsx(r,{})}),i=l(T)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+  justify-content: flex-start;
+`});export{E as __tla,s as default};
