6e75d3e0c31a4a74c3f82e45ea487a8a9404b66c
corlard3y <corlard3y@users.noreply.github.com>
2024-08-02 09:26:53 +0000

    Deploy preview for PR 1772 🛫

---
+++ pr-preview/pr-1772/assets/ChannelsPage-BI_A19S0.js
@@ -0,0 +1,5 @@
+import{aY as n,aZ as t,a_ as o,a$ as m,__tla as i}from"./index-81Yn739c.js";import{C as h,__tla as f}from"./ChannelsModule-BqhiUJcl.js";import{__tla as p}from"./DisplayNotice-B44HXuRs.js";import{__tla as u}from"./Skeleton-B1HXG8Wp.js";import{__tla as y}from"./cloneDeep-CdqxBQyA.js";import{__tla as d}from"./index.esm-B2KkPr_t.js";import{__tla as x}from"./RedCircle-Dfc8JPpt.js";let r,T=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return x}catch{}})()]).then(async()=>{let a;r=({loadTeaser:_,playTeaser:l,channelID:e})=>{let{channelid:s}=n();const c=s||e;return t.jsx(a,{children:t.jsx(h,{channelID:c,loadTeaser:_,playTeaser:l})})},a=o(m)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+`});export{T as __tla,r as default};
--- pr-preview/pr-1772/assets/ContentLayout-eHyZj4JW.js
+++
@@ -1,4 +0,0 @@
-import{aZ as e,bg as n,bh as l,__tla as r}from"./index-LigZ0onX.js";let a,s=Promise.all([(()=>{try{return r}catch{}})()]).then(async()=>{a=({children:t})=>e.jsx(n,{alignItems:"center",display:"flex",flexDirection:"column",justifyContent:"center",alignSelf:"center",maxWidth:"1200px",backgroundColor:"surface-transparent",width:"calc(100% - (var(--spacing-sm) * 2))",css:l`
-        flex: initial;
-        margin: 0 0 auto 0;
-      `,children:t})});export{a as C,s as __tla};
