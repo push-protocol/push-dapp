2a78455711527d172eeafe27064eab7ff59d3651
abhishek-01k <abhishek-01k@users.noreply.github.com>
2024-08-05 10:21:34 +0000

    Deploy preview for PR 1782 🛫

--- pr-preview/pr-1782/assets/ChannelsPage-75IQP2ME.js
+++
@@ -1,5 +0,0 @@
-import{b5 as n,b1 as a,b6 as o,b7 as m,__tla as i}from"./index-Ds6ZtPVF.js";import{C as h,__tla as f}from"./ChannelsModule-DXogT_CA.js";import{__tla as p}from"./DisplayNotice-Bh2iXU0G.js";import{__tla as u}from"./Skeleton-HMZah1GD.js";import{__tla as y}from"./cloneDeep-Dsp47rdy.js";import{__tla as d}from"./index.esm-Dzzd3Xb9.js";import{__tla as b}from"./RedCircle-pvrXb4us.js";let r,x=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return b}catch{}})()]).then(async()=>{let t;r=({loadTeaser:_,playTeaser:l,channelID:e})=>{let{channelid:s}=n();const c=s||e;return a.jsx(t,{children:a.jsx(h,{channelID:c,loadTeaser:_,playTeaser:l})})},t=o(m)`
-  flex: 1;
-  flex-direction: column;
-  align-self: stretch;
-`});export{x as __tla,r as default};
---
+++ pr-preview/pr-1782/assets/ChatPage-C32Y6KVA.js
@@ -0,0 +1,5 @@
+import{b5 as s,b1 as a,b8 as r,b6 as c,b7 as n,__tla as i}from"./index-D-LWwSw0.js";let e,o=Promise.all([(()=>{try{return i}catch{}})()]).then(async()=>{let t;e=()=>{let{chatid:l}=s();return a.jsx(t,{children:a.jsx(r,{chatid:l})})},t=c(n)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+`});export{o as __tla,e as default};
