e3de1180f74f1fac6d52adc509cd216c728c0d77
corlard3y <corlard3y@users.noreply.github.com>
2024-08-06 13:32:57 +0000

    Deploy preview for PR 1772 🛫

---
+++ pr-preview/pr-1772/assets/NotFoundPage-B9Jrbgku.js
@@ -0,0 +1,36 @@
+import{bL as x,aZ as e,bM as h,a_ as t,bz as i,__tla as g}from"./index-z7MF4tpS.js";let p,m=Promise.all([(()=>{try{return g}catch{}})()]).then(async()=>{let a,r,n,s,o,l,c;a="/push-dapp/pr-preview/pr-1772/assets/404Graphic-BY-vXDBY.svg",p=()=>{const d=x();return e.jsx(r,{children:e.jsxs(s,{children:[e.jsx(o,{src:a,alt:"Page not found"}),e.jsxs(c,{children:[e.jsx(n,{children:"Oops..."}),e.jsx(l,{children:"The page you're trying to reach doesn't exist."})]}),e.jsx(h,{variant:"primary",size:"large",onClick:()=>d("/channels"),children:"Go to Home"})]})})},r=t.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(100vh - ${i.CONSTANTS.HEADER_HEIGHT}px);
+  width: 80%;
+  border-radius: ${i.ADJUSTMENTS.RADIUS.LARGE};
+  padding: 20px;
+  box-sizing: border-box;
+  // margin: ${i.ADJUSTMENTS.MARGIN.MINI_MODULES.DESKTOP};
+  margin: auto;
+`,n=t.p`
+  font-size: 48px;
+  font-weight: 500;
+  margin: 0;
+  text-align: center;
+  color: black;
+`,s=t.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+`,o=t.img`
+  width: 355.7px;
+  height: 205.79px;
+  border-radius: 16px;
+  margin: 0 auto;
+`,l=t.p`
+  font-size: 24px;
+  font-weight: 400;
+  width: 100%;
+  text-align: center;
+`,c=t.div`
+  gap: 16px;
+`});export{m as __tla,p as default};
