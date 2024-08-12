f58787405a607127460364ed146f8953991864b9
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-08-12 19:09:58 +0530

    tag in progress

---
+++ src/blocks/tag/Tag.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+
+export type TagProps = {};
+
+const StyledTagContainer = styled.div``;
+
+const StyledTagText = styled.span``;
+
+const StyledTagIcon = styled.div``;
+
+const Tag: FC<TagProps> = () => {
+  return <div></div>;
+};
+
+export { Tag };
