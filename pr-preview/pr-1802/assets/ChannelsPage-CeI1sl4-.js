f81c5a9618c3e879d23c8e5e8af48fe97adfffb3
abhishek-01k <abhishek-01k@users.noreply.github.com>
2024-08-12 09:53:57 +0000

    Deploy preview for PR 1802 🛫

--- pr-preview/pr-1802/assets/ChannelsPage-CeI1sl4-.js
+++
@@ -1,5 +0,0 @@
-import{aY as o,aZ as a,a_ as n,a$ as m,__tla as i}from"./index-CYKlReZ3.js";import{C as h,__tla as f}from"./ChannelsModule-BCB48Xli.js";import{__tla as p}from"./DisplayNotice-CwyeI97l.js";import{__tla as u}from"./Skeleton-CqzQo_O4.js";import{__tla as y}from"./cloneDeep-BOlw4Vdn.js";import{__tla as d}from"./index.esm-B5CtPPhr.js";import{__tla as x}from"./RedCircle-D1MBMnsF.js";import{__tla as T}from"./ManageNotifSettingDropdown-fvJ_fkL1.js";import"./notifSetting-DrLKauYy.js";let r,D=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return x}catch{}})(),(()=>{try{return T}catch{}})()]).then(async()=>{let t;r=({loadTeaser:_,playTeaser:l,channelID:e})=>{let{channelid:c}=o();const s=c||e;return a.jsx(t,{children:a.jsx(h,{channelID:s,loadTeaser:_,playTeaser:l})})},t=n(m)`
-  flex: 1;
-  flex-direction: column;
-  align-self: stretch;
-`});export{D as __tla,r as default};
