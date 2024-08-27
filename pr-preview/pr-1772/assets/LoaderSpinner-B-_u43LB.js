3da0feb9ae0f347e11f994461e41fd1568e1bdbf
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-08-27 06:54:27 +0000

    Deploy preview for PR 1772 🛫

---
+++ pr-preview/pr-1772/assets/LoaderSpinner-B-_u43LB.js
@@ -0,0 +1,1 @@
+import{e1 as a,es as s,__tla as r}from"./index-BAf9fR0J.js";let e,l=Promise.all([(()=>{try{return r}catch{}})()]).then(async()=>{let t;t={height:"45px"},e=()=>a.jsx(s,{color:"#CF1C84",height:9,width:2.5,margin:0,css:t})});export{e as L,l as __tla};
