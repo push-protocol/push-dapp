54cfcbe16229d89789257011912d565cb888abdc
abhishek-01k <abhishek-01k@users.noreply.github.com>
2024-08-30 07:51:25 +0000

    Deploy preview for PR 1826 🛫

--- pr-preview/pr-1826/assets/ChannelsPage-CBVe-bvo.js
+++
@@ -1,5 +0,0 @@
-import{e5 as o,e1 as a,e6 as n,e7 as m,__tla as i}from"./index-kz2isoow.js";import{C as h,__tla as f}from"./ChannelsModule-CY5IAjDT.js";import{__tla as p}from"./DisplayNotice-CnZSeEsB.js";import{__tla as u}from"./Skeleton-DrP8cSdr.js";import{__tla as y}from"./cloneDeep-DgOVpHnW.js";import{__tla as d}from"./index.esm-xDi-iDw3.js";import{__tla as x}from"./RedCircle-BGTyGuRo.js";import{__tla as T}from"./ManageNotifSettingDropdown-CsVmotXj.js";import"./notifSetting-DrLKauYy.js";let r,D=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return x}catch{}})(),(()=>{try{return T}catch{}})()]).then(async()=>{let t;r=({loadTeaser:_,playTeaser:e,channelID:l})=>{let{channelid:c}=o();const s=c||l;return a.jsx(t,{children:a.jsx(h,{channelID:s,loadTeaser:_,playTeaser:e})})},t=n(m)`
-  flex: 1;
-  flex-direction: column;
-  align-self: stretch;
-`});export{D as __tla,r as default};
---
+++ pr-preview/pr-1826/assets/ReceiveNotifsPage-BsG0YwwZ.js
@@ -0,0 +1,31 @@
+import{e6 as l,eM as e,eO as n,eQ as o,e1 as a,eR as D,eZ as S,eb as c,e7 as A,__tla as T}from"./index-BYFilq4K.js";let s,M=Promise.all([(()=>{try{return T}catch{}})()]).then(async()=>{function r(){return o.pageview("/receive"),a.jsx(t,{children:a.jsx(D,{alignItems:"stretch",children:a.jsx(S,{onClose:()=>{},closeEnabled:!1})})})}let t,i;t=l(c)`
+  align-items: center;
+  align-self: center;
+  background: ${d=>d.theme.default.bg};
+  border-radius: ${e.ADJUSTMENTS.RADIUS.LARGE};
+  box-shadow: ${e.ADJUSTMENTS.MODULE_BOX_SHADOW};
+  display: flex;
+  flex-direction: column;
+  flex: initial;
+  justify-content: center;
+  max-width: 1200px;
+  width: auto;
+  padding: ${e.ADJUSTMENTS.PADDING.BIG};
+  position: relative;
+  margin: ${e.ADJUSTMENTS.MARGIN.MINI_MODULES.DESKTOP};
+
+  @media ${n.laptop} {
+    margin: ${e.ADJUSTMENTS.MARGIN.MINI_MODULES.TABLET};
+    padding: ${e.ADJUSTMENTS.PADDING.DEFAULT};
+  }
+
+  @media ${n.mobileL} {
+    margin: ${e.ADJUSTMENTS.MARGIN.MINI_MODULES.MOBILE};
+    padding: ${e.ADJUSTMENTS.PADDING.DEFAULT};
+  }
+`,s=()=>a.jsx(i,{children:a.jsx(r,{})}),i=l(A)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+  justify-content: flex-start;
+`});export{M as __tla,s as default};
