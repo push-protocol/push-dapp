d188bec15a6230df8d0019576cfc82d4ddce1016
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-09-02 07:47:23 +0000

    Deploy preview for PR 1772 🛫

--- pr-preview/pr-1772/assets/ContentLayout-BNJQ3cvK.js
+++
@@ -1,4 +0,0 @@
-import{e1 as t,e2 as n,ep as l,__tla as r}from"./index-pk096wc_.js";let a,s=Promise.all([(()=>{try{return r}catch{}})()]).then(async()=>{a=({children:e})=>t.jsx(n,{alignItems:"center",display:"flex",flexDirection:"column",justifyContent:"center",alignSelf:"center",maxWidth:"1200px",backgroundColor:"surface-transparent",width:"calc(100% - (var(--spacing-sm) * 2))",css:l`
-        flex: initial;
-        margin: 0 0 auto 0;
-      `,children:e})});export{a as C,s as __tla};
--- pr-preview/pr-1772/assets/TransitionGroupContext-ME4MB9YR.js
+++
@@ -1,1 +0,0 @@
-import{eg as e,__tla as a}from"./index-pk096wc_.js";let t,l=Promise.all([(()=>{try{return a}catch{}})()]).then(async()=>{t=e.createContext(null)});export{t as T,l as __tla};
