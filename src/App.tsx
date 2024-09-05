8b89fe49871005daba7baec10b19f9442c74bde5
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-09-05 18:07:29 +0530

    reverted debug variable

--- src/App.tsx
+++ src/App.tsx
@@ -356,7 +356,7 @@ export default function App() {
           <ChatUIProvider
             user={userPushSDKInstance}
             theme={darkMode && darkChatTheme}
-            debug={true}
+            debug={false}
             uiConfig={{
               suppressToast: false,
             }}
