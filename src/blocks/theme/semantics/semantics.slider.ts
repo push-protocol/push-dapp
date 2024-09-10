6fc49b6c745d53443862d33e6dd1516741deed44
kalashshah <202051096@iiitvadodara.ac.in>
2024-09-10 14:12:48 +0530

    chore: update slider semantics file

--- src/blocks/theme/semantics/semantics.slider.ts
+++ src/blocks/theme/semantics/semantics.slider.ts
@@ -3,8 +3,8 @@ import { strokeSemantics } from './semantics.stroke';
 import { surfaceSemantics } from './semantics.surface';
 
 export const sliderSemantics = {
-  'stroke-default': strokeSemantics.secondary,
+  'stroke-default': { light: strokeSemantics.secondary.light, dark: strokeSemantics.secondary.dark },
   'icon-default': { light: colorPrimitives['white-100'], dark: colorPrimitives['white-100'] },
-  'background-default': surfaceSemantics.secondary,
-  'background-progress': surfaceSemantics['brand-medium'],
+  'background-default': { light: surfaceSemantics.secondary.light, dark: surfaceSemantics.secondary.dark },
+  'background-progress': { light: surfaceSemantics['brand-medium'].light, dark: surfaceSemantics['brand-medium'].dark },
 };
