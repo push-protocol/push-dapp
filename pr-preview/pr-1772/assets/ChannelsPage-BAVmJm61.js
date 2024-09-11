d82e2a8aff177a01fd0d8912ca4c52f1f83bf40d
corlard3y <corlard3y@users.noreply.github.com>
2024-09-11 12:02:54 +0000

    Deploy preview for PR 1772 🛫

---
+++ pr-preview/pr-1772/assets/ChannelsPage-BAVmJm61.js
@@ -0,0 +1,5 @@
+import{cz as o,cv as a,cA as n,cB as m,__tla as i}from"./index-CsBd_uFp.js";import{C as h,__tla as f}from"./ChannelsModule-Bt7EoqWJ.js";import{__tla as p}from"./DisplayNotice-Dvi0yxRC.js";import{__tla as u}from"./Skeleton-BQVz0F6A.js";import{__tla as y}from"./cloneDeep-C8nMgWUS.js";import{__tla as d}from"./index.esm-uMoSK1Nf.js";import{__tla as x}from"./RedCircle-DQoGS6ZX.js";import{__tla as T}from"./ManageNotifSettingDropdown-qS3pW_Tw.js";import"./notifSetting-DrLKauYy.js";let r,D=Promise.all([(()=>{try{return i}catch{}})(),(()=>{try{return f}catch{}})(),(()=>{try{return p}catch{}})(),(()=>{try{return u}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return x}catch{}})(),(()=>{try{return T}catch{}})()]).then(async()=>{let t;r=({loadTeaser:_,playTeaser:l,channelID:e})=>{let{channelid:c}=o();const s=c||e;return a.jsx(t,{children:a.jsx(h,{channelID:s,loadTeaser:_,playTeaser:l})})},t=n(m)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+`});export{D as __tla,r as default};
---
+++ pr-preview/pr-1772/assets/NotAvailablePage-Bc05MiKa.js
@@ -0,0 +1,16 @@
+import{cD as l,cq as h,cK as m,cu as d,cv as t,cE as x,cF as p,cG as g,cH as f,cL as w,cM as a,cA as $,__tla as u}from"./index-CsBd_uFp.js";let i,_=Promise.all([(()=>{try{return u}catch{}})()]).then(async()=>{i=function(e){const c=l(),{chainId:r}=h(),s=r===a.coreContractChain;return m.useEffect(()=>{if(s){const o=window.location.origin;window.location.replace(`${o}${d.Channels}`)}}),t.jsx(x,{theme:c,children:t.jsx(p,{style:{minHeight:"60vh"},children:t.jsx(g,{padding:"0px 20px 0px",children:t.jsx(f,{align:"center",children:t.jsxs(n,{color:"#333",spacing:"normal",size:"2rem",weight:700,children:["Kindly switch to ",w[a.coreContractChain]," Network to view this page."]})})})})})};const n=$.h2`
+  color: ${e=>e.theme.scheme==="dark"?e.theme.color:e.color||"#000"};
+  text-shadow: 2px 0 ${e=>e.theme.scheme==="dark"?e.theme.color:e.color||"#000"};
+  font-weight: ${e=>e.weight||600};
+  font-size: ${e=>e.size||"2rem"};
+  text-transform: ${e=>e.textTransform||"inherit"};
+  margin: ${e=>e.margin||"20px 0px"};
+  padding: ${e=>e.padding||"0px"};
+  letter-spacing: ${e=>e.spacing||"normal"};
+  font-family: ${e=>e.family||"'FK Grotesk Neu', 'Source Sans Pro', Helvetica, sans-serif"};
+  text-align: ${e=>e.textAlign||"inherit"};
+
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+  }
+`});export{_ as __tla,i as default};
