729a3159d18e49bfb9e186748939890abb5e7067
corlard3y <corlardey@gmail.com>
2024-09-12 10:58:00 +0100

    sort stake section

--- src/modules/rewards/utils/stakeRewardUtilities.ts
+++ src/modules/rewards/utils/stakeRewardUtilities.ts
@@ -50,7 +50,11 @@ export const getActivityData = (
 // and also, doing a filter and sort of all the unis, and all push to get the latest activities in the useStakeRewardsResetTime hook
 export const filterAndSortActivities = (prefix: string, activities: any[], multiplier?: boolean) => {
   return activities
-    .filter((activity) => activity.index.startsWith(multiplier ? `multiplier-${prefix}` : `point-${prefix}`))
+    .filter(
+      (activity) =>
+        activity.index.startsWith(multiplier ? `multiplier-${prefix}` : `point-${prefix}`) &&
+        activity?.status === 'ENABLED'
+    )
     .sort(sortByIndexNumber);
 };
 
