a5453661f39d7221f116b1b2500fb54c8af4a8b5
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-09-20 10:46:19 +0000

    Deploy preview for PR 1861 🛫

---
+++ pr-preview/pr-1861/assets/ChatPage-CkJPAili.js
@@ -0,0 +1,5 @@
+import{e5 as s,e1 as t,e6 as r,e7 as c,e8 as n,__tla as i}from"./index-B_79j7FX.js";let a,o=Promise.all([(()=>{try{return i}catch{}})()]).then(async()=>{let e;a=()=>{let{chatid:l}=s();return t.jsx(e,{children:t.jsx(r,{chatid:l})})},e=c(n)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+`});export{o as __tla,a as default};
--- pr-preview/pr-1861/assets/LoaderSpinner-BeN2LAhC.js
+++
@@ -1,1 +0,0 @@
-import{e1 as a,ew as r,__tla as s}from"./index-BrTh0OjW.js";let e,l=Promise.all([(()=>{try{return s}catch{}})()]).then(async()=>{let t;t={height:"45px"},e=()=>a.jsx(r,{color:"#CF1C84",height:9,width:2.5,margin:0,css:t})});export{e as L,l as __tla};
