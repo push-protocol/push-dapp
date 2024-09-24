78b81af8b1089e1b67679ff7d04cbd8a8246cfcf
corlard3y <corlard3y@users.noreply.github.com>
2024-09-24 11:40:40 +0000

    Deploy preview for PR 1772 🛫

---
+++ pr-preview/pr-1772/assets/Checkbox-Bz57YmEH.js
@@ -0,0 +1,12 @@
+import{cA as h,cv as r,__tla as a}from"./index-BQhG6nQn.js";let c,n=Promise.all([(()=>{try{return a}catch{}})()]).then(async()=>{let e;c=({checked:t,onChange:o})=>r.jsx(e,{id:"Checkbox-switch",type:"checkbox",checked:t,onChange:o}),e=h.input`
+  cursor: pointer;
+  z-index: 1;
+  border-radius: 15px;
+  width: 16px;
+  height: 20px;
+
+  /* Change the color of the checkbox */
+  &:checked {
+    accent-color: #C742DD;
+  }
+`});export{c as C,n as __tla};
