c668c71b59621bf978c27bbc740c99a18a8fb190
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-09-25 09:26:41 +0000

    Deploy preview for PR 1861 🛫

---
+++ pr-preview/pr-1861/assets/ContentLayout-C80ik9FJ.js
@@ -0,0 +1,7 @@
+import{e1 as e,e2 as r,el as t,__tla as n}from"./index-QxDpoTXm.js";let i,c=Promise.all([(()=>{try{return n}catch{}})()]).then(async()=>{i=({children:a,variant:l})=>l==="verticalStretch"?e.jsx(r,{alignSelf:"center",backgroundColor:"surface-primary",borderRadius:"radius-lg",display:"flex",flexDirection:"column",height:"calc(100vh - 76px)",overflow:"hidden",maxWidth:"1200px",width:"calc(100% - (var(--spacing-sm) * 2))",css:t`
+          border-bottom-right-radius: var(--radius-none);
+          border-bottom-left-radius: var(--radius-none);
+        `,children:a}):e.jsx(r,{alignItems:"center",alignSelf:"center",backgroundColor:"surface-transparent",display:"flex",flexDirection:"column",justifyContent:"center",maxWidth:"1200px",width:"calc(100% - (var(--spacing-sm) * 2))",css:t`
+        flex: initial;
+        margin: 0 0 auto 0;
+      `,children:a})});export{i as C,c as __tla};
