e862519a986882cd641dec5bafda60d692fc4dcb
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-09-26 08:38:25 +0000

    Deploy preview for PR 1861 🛫

--- pr-preview/pr-1861/assets/useDisclosure-CkjLcnAs.js
+++
@@ -1,1 +0,0 @@
-import{d_ as t,__tla as o}from"./index-DzCeWrSd.js";let e,n=Promise.all([(()=>{try{return o}catch{}})()]).then(async()=>{e=()=>{const[l,a]=t.useState(!1),s=t.useCallback(()=>a(!0),[]),r=t.useCallback(()=>a(!1),[]);return{isOpen:l,onClose:r,open:s}}});export{n as __tla,e as u};
