66cc818c9718bbb2f75f0fc39ef976fad71a458f
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-09-26 12:29:11 +0000

    Remove preview for PR 1872 🛬

--- pr-preview/pr-1872/assets/ChatPage-DsNUie_I.js
+++
@@ -1,5 +0,0 @@
-import{e5 as s,e1 as t,e8 as r,e6 as c,e7 as n,__tla as i}from"./index-D5e_kufZ.js";let a,o=Promise.all([(()=>{try{return i}catch{}})()]).then(async()=>{let e;a=()=>{let{chatid:l}=s();return t.jsx(e,{children:t.jsx(r,{chatid:l})})},e=c(n)`
-  flex: 1;
-  flex-direction: column;
-  align-self: stretch;
-`});export{o as __tla,a as default};
