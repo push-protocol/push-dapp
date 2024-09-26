42857cab768e4a706a398a8ca19fa95a04fd29e4
abhishek-01k <abhishek-01k@users.noreply.github.com>
2024-09-26 05:22:14 +0000

    Deploy preview for PR 1872 🛫

---
+++ pr-preview/pr-1872/assets/Common.utils-CLm1upOD.js
@@ -0,0 +1,1 @@
+import{eh as s,e1 as e,ev as n,__tla as o}from"./index-CatbrsQn.js";let r,_=Promise.all([(()=>{try{return o}catch{}})()]).then(async()=>{r=a=>a==null?void 0:a.map(t=>{const l=n[t];return{value:t.toString(),label:s[t],icon:e.jsx(l,{})}})});export{_ as __tla,r as g};
---
+++ pr-preview/pr-1872/assets/EmptyNotificationSettings-C-oPcqG5.js
@@ -0,0 +1,23 @@
+import{e1 as e,eY as i,f3 as d,e6 as l,f4 as C,f5 as _,__tla as u}from"./index-CatbrsQn.js";import{F as y,__tla as k}from"./index.esm-xJ4HjmaK.js";let x,m,h,p,v=Promise.all([(()=>{try{return u}catch{}})(),(()=>{try{return k}catch{}})()]).then(async()=>{let a,n,r,s,c;p=({onClick:t})=>e.jsx(i,{leadingIcon:e.jsx(d,{}),onClick:t,size:"small",variant:"tertiary",children:"Add Delegate"}),h=({onClick:t})=>e.jsx(i,{leadingIcon:e.jsx(y,{}),onClick:t,size:"small",variant:"tertiary",children:"Manage Settings"}),a=({onClick:t,title:o})=>e.jsx(i,{variant:"outline",size:"small",onClick:t,children:o||"Modify Settings"}),x=({onClick:t})=>e.jsx(i,{onClick:t,size:"small",leadingIcon:e.jsx(d,{}),variant:"tertiary",children:"Add Setting"}),m=({description:t,onClick:o,title:g,buttonTitle:f,showTopBorder:j=!0})=>e.jsxs(n,{showTopBorder:j,children:[e.jsx(c,{src:_,alt:"No Settings",width:"24px",height:"24px"}),e.jsx(r,{children:g}),e.jsx(s,{children:t}),e.jsx(a,{onClick:o,title:f})]}),n=l.div`
+  border-top: ${t=>t.showTopBorder&&`1px solid ${t.theme.default.borderColor}`};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 16px;
+`,r=l.div`
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: normal;
+  text-align: left;
+  color: ${t=>t.theme.default.color};
+`,s=l.div`
+  margin-top: 1px;
+  margin-bottom: 16px;
+  color: ${t=>t.theme.default.secondaryColor};
+`,c=l(C)`
+  color: ${t=>t.theme.default.color};
+  margin-top: 32px;
+  margin-bottom: 12px;
+`});export{x as A,m as E,h as M,v as __tla,p as a};
