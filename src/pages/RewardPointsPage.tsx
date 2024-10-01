1aa1a61876f5bcf59222a83ff4378d6324aa44a5
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-10-01 13:23:56 +0530

    Changed helmet component

--- src/pages/RewardPointsPage.tsx
+++ src/pages/RewardPointsPage.tsx
@@ -4,55 +4,55 @@ import { ContentLayout } from 'common';
 
 const RewardsPointsPage = () => {
   return (
-    <Helmet>
-      <meta
-        property="og:url"
-        content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1833/points"
-      />
-      <meta
-        property="og:type"
-        content="website"
-      />
-      <meta
-        property="og:title"
-        content="Push App (Previously EPNS)"
-      />
-      <meta
-        property="og:description"
-        content="Push App (Previously EPNS) | Communication Protocol of Web3"
-      />
-      <meta
-        property="og:image"
-        content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1833/previews/rewardsPreview.png"
-      />
-      <meta
-        name="twitter:card"
-        content="summary_large_image"
-      />
-      <meta
-        property="twitter:domain"
-        content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1833/points"
-      />
-      <meta
-        property="twitter:url"
-        content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1833/points"
-      />
-      <meta
-        name="twitter:title"
-        content="Push App (Previously EPNS)"
-      />
-      <meta
-        name="twitter:description"
-        content="Push App (Previously EPNS) | Communication Protocol of Web3"
-      />
-      <meta
-        name="twitter:image"
-        content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1833/previews/rewardsPreview.png"
-      />
-      <ContentLayout>
+    <ContentLayout>
+      <Helmet>
+        <meta
+          property="og:url"
+          content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1833/points"
+        />
+        <meta
+          property="og:type"
+          content="website"
+        />
+        <meta
+          property="og:title"
+          content="Push App (Previously EPNS)"
+        />
+        <meta
+          property="og:description"
+          content="Push App (Previously EPNS) | Communication Protocol of Web3"
+        />
+        <meta
+          property="og:image"
+          content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1833/previews/rewardsPreview.png"
+        />
+        <meta
+          name="twitter:card"
+          content="summary_large_image"
+        />
+        <meta
+          property="twitter:domain"
+          content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1833/points"
+        />
+        <meta
+          property="twitter:url"
+          content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1833/points"
+        />
+        <meta
+          name="twitter:title"
+          content="Push App (Previously EPNS)"
+        />
+        <meta
+          name="twitter:description"
+          content="Push App (Previously EPNS) | Communication Protocol of Web3"
+        />
+        <meta
+          name="twitter:image"
+          content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1833/previews/rewardsPreview.png"
+        />
         <Rewards />
-      </ContentLayout>
-    </Helmet>
+      </Helmet>
+    </ContentLayout>
   );
 };
 export default RewardsPointsPage;
