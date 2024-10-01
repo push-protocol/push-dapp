d33a4236e6de090017587a78b0e1e152e822d93a
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-10-01 07:18:09 +0000

    Deploy preview for PR 1883 🛫

--- pr-preview/pr-1883/assets/ChatPage-BvekxpLm.js
+++
@@ -1,5 +0,0 @@
-import{cz as c,cv as a,cC as s,cA as r,cB as i,__tla as n}from"./index-CfsbKxNy.js";let e,_=Promise.all([(()=>{try{return n}catch{}})()]).then(async()=>{let t;e=()=>{let{chatid:l}=c();return a.jsx(t,{children:a.jsx(s,{chatid:l})})},t=r(i)`
-  flex: 1;
-  flex-direction: column;
-  align-self: stretch;
-`});export{_ as __tla,e as default};
