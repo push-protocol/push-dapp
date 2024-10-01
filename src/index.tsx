a9e0cedec843f2aac8681ea6cb9df18fa5450b12
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-10-01 12:31:52 +0530

    testing tags

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
+import { HelmetProvider } from 'react-helmet-async';
 // Internal Components
 import App from './App';
 import ErrorContextProvider from './contexts/ErrorContext';
@@ -34,24 +34,26 @@ const queryClient = new QueryClient({});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename={previewBasePath || publicURL}>
-    <Provider store={store}>
-      <GlobalContextProvider>
-        <Web3OnboardProvider web3Onboard={web3Onboard}>
-          <ErrorContextProvider>
-            <RewardsContextProvider>
-              <AppContextProvider>
-                <VideoCallContextProvider>
-                  <QueryClientProvider client={queryClient}>
-                    <App />
-                    <ReactQueryDevtools initialIsOpen={false} />
-                  </QueryClientProvider>
-                </VideoCallContextProvider>
-              </AppContextProvider>
-            </RewardsContextProvider>
-          </ErrorContextProvider>
-        </Web3OnboardProvider>
-      </GlobalContextProvider>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <GlobalContextProvider>
+          <Web3OnboardProvider web3Onboard={web3Onboard}>
+            <ErrorContextProvider>
+              <RewardsContextProvider>
+                <AppContextProvider>
+                  <VideoCallContextProvider>
+                    <QueryClientProvider client={queryClient}>
+                      <App />
+                      <ReactQueryDevtools initialIsOpen={false} />
+                    </QueryClientProvider>
+                  </VideoCallContextProvider>
+                </AppContextProvider>
+              </RewardsContextProvider>
+            </ErrorContextProvider>
+          </Web3OnboardProvider>
+        </GlobalContextProvider>
+      </Provider>
+    </HelmetProvider>
   </BrowserRouter>
 );
 
--- src/pages/RewardPointsPage.tsx
+++ src/pages/RewardPointsPage.tsx
@@ -1,12 +1,58 @@
-//components
+import { Helmet } from 'react-helmet-async';
 import { Rewards } from 'modules/rewards';
 import { ContentLayout } from 'common';
 
 const RewardsPointsPage = () => {
   return (
-    <ContentLayout>
-      <Rewards />
-    </ContentLayout>
+    <Helmet>
+      <meta
+        property="og:url"
+        content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1877/points"
+      />
+      <meta
+        property="og:type"
+        content="website"
+      />
+      <meta
+        property="og:title"
+        content="Push App (Previously EPNS)"
+      />
+      <meta
+        property="og:description"
+        content="Push App (Previously EPNS) | Communication Protocol of Web3"
+      />
+      <meta
+        property="og:image"
+        content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1877/previews/rewardsPreview.png"
+      />
+      <meta
+        name="twitter:card"
+        content="summary_large_image"
+      />
+      <meta
+        property="twitter:domain"
+        content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1877/points"
+      />
+      <meta
+        property="twitter:url"
+        content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1877/points"
+      />
+      <meta
+        name="twitter:title"
+        content="Push App (Previously EPNS)"
+      />
+      <meta
+        name="twitter:description"
+        content="Push App (Previously EPNS) | Communication Protocol of Web3"
+      />
+      <meta
+        name="twitter:image"
+        content="https://push-protocol.github.io/push-dapp/pr-preview/pr-1877/previews/rewardsPreview.png"
+      />
+      <ContentLayout>
+        <Rewards />
+      </ContentLayout>
+    </Helmet>
   );
 };
 export default RewardsPointsPage;
