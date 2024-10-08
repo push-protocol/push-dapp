a75bdba3b62c4c76c6daa48c060abeb41a557010
abhishek-01k <abhishek-01k@users.noreply.github.com>
2024-10-08 07:04:03 +0000

    Deploy preview for PR 1893 🛫

--- pr-preview/pr-1893/assets/ContentLayout-DqzP7hUg.js
+++
@@ -1,4 +0,0 @@
-import{cv as e,cw as n,cT as l,__tla as r}from"./index-CFQ4neVb.js";let a,c=Promise.all([(()=>{try{return r}catch{}})()]).then(async()=>{a=({children:t})=>e.jsx(n,{alignItems:"center",display:"flex",flexDirection:"column",justifyContent:"center",alignSelf:"center",maxWidth:"1200px",backgroundColor:"surface-transparent",width:"calc(100% - (var(--spacing-sm) * 2))",css:l`
-        flex: initial;
-        margin: 0 0 auto 0;
-      `,children:t})});export{a as C,c as __tla};
