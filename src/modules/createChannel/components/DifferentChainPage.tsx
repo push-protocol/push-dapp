0ff4e61070e1ab9ea865ea6c0d48cbdba4c3e3de
Abhishek <77395788+abhishek-01k@users.noreply.github.com>
2024-10-11 19:08:49 +0800

    Changed the name from Etheruem sepolia to mainnet on prod (#1918)
    * Changed the name from Etheruem sepolia to mainnet on prod
    
    * REmoved the log

--- src/modules/createChannel/components/DifferentChainPage.tsx
+++ src/modules/createChannel/components/DifferentChainPage.tsx
@@ -6,29 +6,39 @@ import { appConfig } from 'config';
 
 // Components
 import { Box, Button, Text } from 'blocks';
+import { networkName } from 'helpers/UtilityHelper';
 
 const DifferentChainPage = () => {
   const { switchChain } = useAccount();
+
+  const chainId = appConfig.allowedNetworks[0];
+
+  const chainName = networkName[chainId];
+
   return (
     <>
       <Box
-        display='flex'
-        flexDirection='column'
-        padding='spacing-md'
-        backgroundColor='surface-secondary'
-        borderRadius='radius-sm'
-        width='-webkit-fill-available'
+        display="flex"
+        flexDirection="column"
+        padding="spacing-md"
+        backgroundColor="surface-secondary"
+        borderRadius="radius-sm"
+        width="-webkit-fill-available"
       >
-        <Text textAlign='center' variant='h5-semibold' color='text-brand-medium'>
-          Please select Ethereum Sepolia Network in your Wallet to create a channel.
+        <Text
+          textAlign="center"
+          variant="h5-semibold"
+          color="text-brand-medium"
+        >
+          Please select {chainName} Network in your Wallet to create a channel.
         </Text>
-
       </Box>
 
-      <Box display='flex' justifyContent='center'>
-        <Button onClick={() => switchChain(appConfig.coreContractChain)}>
-          Change Network
-        </Button>
+      <Box
+        display="flex"
+        justifyContent="center"
+      >
+        <Button onClick={() => switchChain(appConfig.coreContractChain)}>Change Network</Button>
       </Box>
     </>
   );
