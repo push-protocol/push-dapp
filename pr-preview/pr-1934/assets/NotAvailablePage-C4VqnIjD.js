5c5c737d5782a10cb91253b9db009d66f467e246
corlard3y <corlard3y@users.noreply.github.com>
2024-10-24 10:49:17 +0000

    Deploy preview for PR 1934 🛫

---
+++ pr-preview/pr-1934/assets/NotAvailablePage-C4VqnIjD.js
@@ -0,0 +1,16 @@
+import{e5 as l,dY as h,ec as m,e0 as d,e1 as t,e6 as x,e7 as p,e8 as g,e9 as f,ed as w,ee as a,ea as $,__tla as u}from"./index-CCSq7fiT.js";let i,_=Promise.all([(()=>{try{return u}catch{}})()]).then(async()=>{i=function(e){const r=l(),{chainId:s}=h(),o=s===a.coreContractChain;return m.useEffect(()=>{if(o){const c=window.location.origin;window.location.replace(`${c}${d.Channels}`)}}),t.jsx(x,{theme:r,children:t.jsx(p,{style:{minHeight:"60vh"},children:t.jsx(g,{padding:"0px 20px 0px",children:t.jsx(f,{align:"center",children:t.jsxs(n,{color:"#333",spacing:"normal",size:"2rem",weight:700,children:["Kindly switch to ",w[a.coreContractChain]," Network to view this page."]})})})})})};const n=$.h2`
+  color: ${e=>e.theme.scheme==="dark"?e.theme.color:e.color||"#000"};
+  text-shadow: 2px 0 ${e=>e.theme.scheme==="dark"?e.theme.color:e.color||"#000"};
+  font-weight: ${e=>e.weight||600};
+  font-size: ${e=>e.size||"2rem"};
+  text-transform: ${e=>e.textTransform||"inherit"};
+  margin: ${e=>e.margin||"20px 0px"};
+  padding: ${e=>e.padding||"0px"};
+  letter-spacing: ${e=>e.spacing||"normal"};
+  font-family: ${e=>e.family||"'FK Grotesk Neu', 'Source Sans Pro', Helvetica, sans-serif"};
+  text-align: ${e=>e.textAlign||"inherit"};
+
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+  }
+`});export{_ as __tla,i as default};
---
+++ pr-preview/pr-1934/assets/ray-BKUegCS8.js
@@ -0,0 +1,1 @@
+const p="/push-dapp/pr-preview/pr-1934/assets/ray-BLedBv_k.jpg";export{p as default};
