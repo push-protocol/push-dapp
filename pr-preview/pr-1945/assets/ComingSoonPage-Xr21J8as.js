a29f2a5cb8a016467ad58f9c602d4333e4004e0b
mishramonalisha76 <mishramonalisha76@users.noreply.github.com>
2024-10-29 10:32:37 +0000

    Deploy preview for PR 1945 🛫

---
+++ pr-preview/pr-1945/assets/ComingSoonPage-Xr21J8as.js
@@ -0,0 +1,16 @@
+import{e5 as i,e1 as t,e6 as s,e7 as o,e8 as l,e9 as c,ea as m,__tla as h}from"./index-DiVqeQxc.js";let n,x=Promise.all([(()=>{try{return h}catch{}})()]).then(async()=>{n=function(e){const r=i();return t.jsx(s,{theme:r,children:t.jsx(o,{style:{minHeight:"60vh"},children:t.jsx(l,{padding:"0px 20px 0px",children:t.jsxs(c,{align:"center",children:[console.info(e.theme),t.jsx(a,{color:"#333",spacing:"normal",size:"4rem",weight:700,children:"Coming soon."})]})})})})};const a=m.h2`
+  color: ${e=>e.theme.scheme==="dark"?e.theme.color:e.color||"#000"};
+  text-shadow: 2px 0 ${e=>e.theme.scheme==="dark"?e.theme.color:e.color||"#000"};
+  font-weight: ${e=>e.weight||600};
+  font-size: ${e=>e.size||"2rem"};
+  text-transform: ${e=>e.textTransform||"inherit"};
+  margin: ${e=>e.margin||"20px 0px"};
+  padding: ${e=>e.padding||"0px"};
+  letter-spacing: ${e=>e.spacing||"normal"};
+  font-family: ${e=>e.family||"'FK Grotesk Neu', 'Source Sans Pro', Helvetica, sans-serif"};
+  text-align: ${e=>e.textAlign||"inherit"};
+
+  @media (max-width: 768px) {
+    font-size: 2.8rem;
+  }
+`});export{x as __tla,n as default};
