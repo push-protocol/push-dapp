6fda7d8fdd8a1d17faaa7ca93f2d1b6f6f8d93a7
mishramonalisha76 <mishramonalisha76@users.noreply.github.com>
2024-10-29 10:04:27 +0000

    Deploy preview for PR 1945 🛫

---
+++ pr-preview/pr-1945/assets/ContentLayout-BtaXPb0p.js
@@ -0,0 +1,7 @@
+import{e1 as r,e2 as e,ej as i,__tla as n}from"./index-CtHIn1vx.js";let t,c=Promise.all([(()=>{try{return n}catch{}})()]).then(async()=>{t=({children:a,variant:l})=>l==="verticalStretch"?r.jsx(e,{alignSelf:"center",backgroundColor:"surface-primary",borderRadius:"radius-lg",display:"flex",flexDirection:"column",height:"calc(100vh - 76px)",overflow:"hidden",maxWidth:"1200px",width:{initial:"calc(100% - (var(--spacing-sm) * 2))",ml:"-webkit-fill-available"},css:i`
+          border-bottom-right-radius: var(--radius-none);
+          border-bottom-left-radius: var(--radius-none);
+        `,children:a}):r.jsx(e,{alignItems:"center",alignSelf:"center",backgroundColor:"surface-transparent",display:"flex",flexDirection:"column",justifyContent:"center",maxWidth:"1200px",width:"calc(100% - (var(--spacing-sm) * 2))",css:i`
+        flex: initial;
+        margin: 0 0 auto 0;
+      `,children:a})});export{t as C,c as __tla};
