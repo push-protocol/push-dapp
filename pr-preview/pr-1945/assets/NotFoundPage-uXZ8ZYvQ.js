9acd5ec65610e9196b51e66fee60f086264acab9
mishramonalisha76 <mishramonalisha76@users.noreply.github.com>
2024-11-07 08:19:08 +0000

    Deploy preview for PR 1945 🛫

---
+++ pr-preview/pr-1945/assets/NotFoundPage-uXZ8ZYvQ.js
@@ -0,0 +1,36 @@
+import{dZ as x,e1 as e,eo as h,ea as t,eO as i,__tla as g}from"./index-Cky69wff.js";let p,m=Promise.all([(()=>{try{return g}catch{}})()]).then(async()=>{let r,a,n,s,o,l,c;r="/push-dapp/pr-preview/pr-1945/assets/404Graphic-BY-vXDBY.svg",p=()=>{const d=x();return e.jsx(a,{children:e.jsxs(s,{children:[e.jsx(o,{src:r,alt:"Page not found"}),e.jsxs(c,{children:[e.jsx(n,{children:"Oops..."}),e.jsx(l,{children:"The page you're trying to reach doesn't exist."})]}),e.jsx(h,{variant:"primary",size:"large",onClick:()=>d("/channels"),children:"Go to Home"})]})})},a=t.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(100vh - ${i.CONSTANTS.HEADER_HEIGHT}px);
+  width: 80%;
+  border-radius: ${i.ADJUSTMENTS.RADIUS.LARGE};
+  padding: 20px;
+  box-sizing: border-box;
+  // margin: ${i.ADJUSTMENTS.MARGIN.MINI_MODULES.DESKTOP};
+  margin: auto;
+`,n=t.p`
+  font-size: 48px;
+  font-weight: 500;
+  margin: 0;
+  text-align: center;
+  color: black;
+`,s=t.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+`,o=t.img`
+  width: 355.7px;
+  height: 205.79px;
+  border-radius: 16px;
+  margin: 0 auto;
+`,l=t.p`
+  font-size: 24px;
+  font-weight: 400;
+  width: 100%;
+  text-align: center;
+`,c=t.div`
+  gap: 16px;
+`});export{m as __tla,p as default};
