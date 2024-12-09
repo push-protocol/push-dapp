f232b79b5cf7455bab6967368710c8b6bcf0bf97
rohitmalhotra1420 <rohitmalhotra1420@users.noreply.github.com>
2024-12-09 08:42:18 +0000

    Deploy preview for PR 1988 🛫

---
+++ pr-preview/pr-1988/assets/UserSettingsPage-BgGimAhc.js
@@ -0,0 +1,254 @@
+import{d_ as e,eq as Me,go as ye,eb as Ee,gv as _e,gw as ve,e7 as n,f6 as P,gx as B,dV as O,f1 as A,dX as d,dW as H,f0 as z,gy as R,gz as G,gA as $e,gm as Te,eN as p,fv as Ce,g4 as Ie,e2 as Oe,d$ as J,eh as Ae,gB as De,fm as ke,gC as Ne,fn as Ze,eQ as v,gt as q,gD as Fe,gE as Ue,eS as D,gF as Pe,e0 as W,e1 as K,el as V,gG as Be,eU as He,eL as u,eM as $,eW as ze,__tla as Re}from"./index-6Q99WsQ3.js";import{c as Ge,__tla as Je}from"./cloneDeep-DKd7xvW6.js";import{E as qe,__tla as We}from"./EmptyNotificationSettings-DM5WR6iu.js";import{L as Ke,__tla as Ve}from"./LoaderSpinner-CQwub_-s.js";import{__tla as Xe}from"./index.esm-DPZJLQd-.js";let X,Qe=Promise.all([(()=>{try{return Re}catch{}})(),(()=>{try{return Je}catch{}})(),(()=>{try{return We}catch{}})(),(()=>{try{return Ve}catch{}})(),(()=>{try{return Xe}catch{}})()]).then(async()=>{const Q=t=>{const{svgProps:o,...r}=t;return e.jsx(Me,{componentName:"Metamask",illustration:e.jsxs("svg",{width:r.width??"40",height:r.height??"39",viewBox:"0 0 43 41",fill:"none",xmlns:"http://www.w3.org/2000/svg",...o,children:[e.jsx("path",{d:"M39.9998 0.674622L23.5776 12.968L26.6208 5.72021L39.9998 0.674622Z",fill:"#E27625"}),e.jsx("path",{d:"M2.0835 0.674622L18.3685 13.0795L15.4625 5.72021L2.0835 0.674622Z",fill:"#E27625"}),e.jsx("path",{d:"M34.1053 29.1919L29.7188 35.9379L39.095 38.5583L41.7818 29.3313L34.1053 29.1919Z",fill:"#E27625"}),e.jsx("path",{d:"M0.356445 29.3313L3.01579 38.5583L12.3646 35.9379L8.00549 29.1919L0.356445 29.3313Z",fill:"#E27625"}),e.jsx("path",{d:"M11.8711 17.7627L9.2666 21.749L18.5332 22.1671L18.2316 12.0759L11.8711 17.7627Z",fill:"#E27625"}),e.jsx("path",{d:"M30.2397 17.7627L23.7695 11.9644L23.5776 22.1671L32.8442 21.7489L30.2397 17.7627Z",fill:"#E27625"}),e.jsx("path",{d:"M12.3647 35.9379L17.985 33.206L13.1324 29.4149L12.3647 35.9379Z",fill:"#E27625"}),e.jsx("path",{d:"M24.126 33.206L29.7188 35.9379L28.9512 29.4149L24.126 33.206Z",fill:"#E27625"}),e.jsx("path",{d:"M29.7188 35.9379L24.126 33.206L24.592 36.8856L24.5372 38.4467L29.7188 35.9379Z",fill:"#D7C1B3"}),e.jsx("path",{d:"M12.3647 35.9379L17.5738 38.4467L17.5464 36.8856L17.985 33.206L12.3647 35.9379Z",fill:"#D7C1B3"}),e.jsx("path",{d:"M17.6558 26.9619L13.0225 25.5959L16.3124 24.0627L17.6558 26.9619Z",fill:"#2F343B"}),e.jsx("path",{d:"M24.4277 26.9619L25.7985 24.0627L29.0884 25.5959L24.4277 26.9619Z",fill:"#2F343B"}),e.jsx("path",{d:"M12.3645 35.9379L13.187 29.1919L8.00537 29.3313L12.3645 35.9379Z",fill:"#CC6228"}),e.jsx("path",{d:"M28.9238 29.1919L29.7189 35.9379L34.1054 29.3313L28.9238 29.1919Z",fill:"#CC6228"}),e.jsx("path",{d:"M32.8442 21.749L23.5776 22.1671L24.4275 26.9618L25.7983 24.0627L29.0882 25.5959L32.8442 21.749Z",fill:"#CC6228"}),e.jsx("path",{d:"M13.0226 25.5959L16.3125 24.0627L17.6559 26.9618L18.5332 22.1671L9.2666 21.749L13.0226 25.5959Z",fill:"#CC6228"}),e.jsx("path",{d:"M9.2666 21.749L13.1322 29.4149L13.0226 25.5959L9.2666 21.749Z",fill:"#E27625"}),e.jsx("path",{d:"M29.0883 25.5959L28.9512 29.4149L32.8442 21.749L29.0883 25.5959Z",fill:"#E27625"}),e.jsx("path",{d:"M18.5331 22.1672L17.6558 26.9619L18.7524 32.6486L18.9991 25.1778L18.5331 22.1672Z",fill:"#E27625"}),e.jsx("path",{d:"M23.5779 22.1672L23.1118 25.1499L23.3311 32.6486L24.4278 26.9619L23.5779 22.1672Z",fill:"#E27625"}),e.jsx("path",{d:"M24.4277 26.9619L23.3311 32.6486L24.1261 33.2061L28.9513 29.415L29.0884 25.5959L24.4277 26.9619Z",fill:"#F5841F"}),e.jsx("path",{d:"M13.0225 25.5959L13.1321 29.415L17.9847 33.2061L18.7524 32.6486L17.6558 26.9619L13.0225 25.5959Z",fill:"#F5841F"}),e.jsx("path",{d:"M24.5374 38.4468L24.5922 36.8857L24.1536 36.5233H17.9302L17.5464 36.8857L17.5738 38.4468L12.3647 35.9379L14.1742 37.4432L17.8753 40.0357H24.2084L27.9096 37.4432L29.719 35.9379L24.5374 38.4468Z",fill:"#C0AD9E"}),e.jsx("path",{d:"M24.1262 33.206L23.3311 32.6485H18.7527L17.985 33.206L17.5464 36.8857L17.9302 36.5233H24.1536L24.5923 36.8857L24.1262 33.206Z",fill:"#2F343B"}),e.jsx("path",{d:"M40.7126 13.7764L42.0834 6.97463L39.9998 0.674622L24.126 12.5499L30.2397 17.7627L38.8757 20.3273L40.7674 18.0693L39.945 17.4561L41.2609 16.2574L40.2465 15.449L41.5625 14.4454L40.7126 13.7764Z",fill:"#763E1A"}),e.jsx("path",{d:"M0 6.97469L1.39821 13.7765L0.520902 14.4455L1.83687 15.4769L0.822478 16.2575L2.13844 17.4561L1.31596 18.0694L3.23508 20.3274L11.8711 17.7628L17.9848 12.5499L2.08361 0.674683L0 6.97469Z",fill:"#763E1A"}),e.jsx("path",{d:"M38.8757 20.3273L30.2397 17.7627L32.8442 21.749L28.9512 29.4149L34.1054 29.3313H41.7818L38.8757 20.3273Z",fill:"#F5841F"}),e.jsx("path",{d:"M11.8711 17.7627L3.23512 20.3273L0.356445 29.3313H8.00549L13.1323 29.4149L9.26662 21.749L11.8711 17.7627Z",fill:"#F5841F"}),e.jsx("path",{d:"M23.5775 22.1671L24.1258 12.5499L26.6207 5.72021H15.4624L17.9847 12.5499L18.533 22.1671L18.7523 25.1778V32.6486H23.3308L23.3582 25.1778L23.5775 22.1671Z",fill:"#F5841F"})]}),...r})},Y=async({channel:t})=>{try{return await ye.channels.getChannel({channel:t,env:Ee.appEnv})}catch(o){throw console.error(o),new Error(o.message)}},ee=async({userCaipAddress:t})=>{var r,f;const o=ve.userSubscriptions(t);try{return(f=(r=await _e(o))==null?void 0:r.data)==null?void 0:f.subscriptions}catch(x){throw new Error(x.message)}},te=()=>{const{account:t,chainId:o}=O(),{subscriptionStatus:r,userSettings:f}=A(i=>i.channels),[x,g]=d.useState([]),[w,_]=d.useState(!0),y=H(),T=z(),j=()=>{y("/channels")},b=async i=>{const s=await Y({channel:i});if(s)return{channel:i,id:s.id,icon:s.icon,name:s.name,channel_settings:s.channel_settings}},L=async i=>{const s=await Promise.all(Object.keys(i).map(async h=>{const a=await b(h);if(a)return a}));g(s)};d.useEffect(()=>{t&&async function(){if(_(!0),Object.keys(r).length===0){const i=Te(t,o),s=await ee({userCaipAddress:i}),h={},a={};s.map(({channel:l,user_settings:S})=>{h[l]=!0,a[l]=S?JSON.parse(S):null}),T(R(h)),T(G(a)),await L(h)}else await L(r);_(!1)}()},[t]);const E=d.useMemo(()=>Ge(f),[f]);return e.jsx(e.Fragment,{children:w?e.jsx(e.Fragment,{children:e.jsx(k,{children:e.jsx(Ke,{})})}):e.jsx(e.Fragment,{children:x.length>0?x.map((i,s)=>e.jsx(e.Fragment,{children:i&&e.jsxs(e.Fragment,{children:[e.jsxs(ae,{children:[e.jsxs(ne,{children:[e.jsx(ie,{src:i.icon}),e.jsx(se,{children:i.name})]}),e.jsx($e,{userSetting:E[i.channel],centerOnMobile:!1,channelDetail:i,onSuccessOptout:()=>{g(h=>h.filter(a=>(a==null?void 0:a.id)!==i.id))},children:e.jsx(le,{})})]},i.id),s!==x.length-1&&e.jsx(re,{})]})})):e.jsx(k,{children:e.jsx(qe,{title:"No Channel Opt-ins",description:"Opt-in channels to manage your notification preferences",buttonTitle:"Go to Channels",onClick:j,showTopBorder:!1})})})})},ae=n.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 12px 0px;
+`,ne=n.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`,ie=n(P)`
+  border: 1px solid ${t=>t.theme.default.borderColor};
+  border-radius: 8px;
+  overflow: hidden;
+  margin-right: 16px;
+  width: 28px;
+  height: 28px;
+`,se=n.span`
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 23px;
+  letter-spacing: normal;
+  color: ${t=>t.theme.default.color};
+`,le=n(B)`
+  background: transparent;
+  display: flex;
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  padding: 0px;
+  position: relative;
+  width: 24px;
+  height: 24px;
+  color: ${t=>t.theme.default.color};
+`,re=n.span`
+  background-color: ${t=>t.theme.default.borderColor};
+  width: 100%;
+  display: flex;
+  height: 1px;
+`,k=n.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80%;
+`,oe=()=>{const{account:t,isWalletConnected:o,connect:r,provider:f}=O(),x=Oe(),[g,w]=d.useState(!1),[_,y]=d.useState(!1),[T,j]=d.useState(!1),[b,L]=d.useState(null),[E,i]=d.useState({enabled:!1,hrsLeft:0}),[s,h]=d.useState(!1),a="npm:@pushprotocol/snap";async function l(){if(!o){L("Connect your metamask wallet to install Snap"),h(!1);return}L("");const c=await window.ethereum.request({method:"wallet_getSnaps"});console.debug("Snaps installed",c),Object.keys(c).forEach(m=>{m=="npm:@pushprotocol/snap"&&h(!0)})}async function S(c){return await f.getSigner(c).signMessage(`Add address ${c} to receive notifications through Push Snap`)}async function M(){var c;try{await S(t)?t&&(await((c=window.ethereum)==null?void 0:c.request({method:"wallet_invokeSnap",params:{snapId:a,request:{method:"pushproto_addaddress",params:{address:t}}}})),console.debug("Added",t),y(!0)):console.error("Signature Validation Failed")}catch(m){L(m.message)}}async function C(){var m;const c=await((m=window.ethereum)==null?void 0:m.request({method:"wallet_invokeSnap",params:{snapId:a,request:{method:"pushproto_getaddresses"}}}));console.debug(t),c.includes(t)?(j(!0),y(!0)):j(!1)}d.useEffect(()=>{l(),C()},[t,o,s]);async function je(){var I;if(!o)return;let c=a,m={};await((I=window.ethereum)==null?void 0:I.request({method:"wallet_requestSnaps",params:{[c]:m}})),console.info("Snap Installed"),h(!0)}async function U(){if(!o){L("Connect your metamask wallet to install Snap");return}L(""),w(!0);try{o||await r(),s?await M():(await je(),l()),w(!1)}catch(c){w(!1),console.error("Error",c)}}const be=()=>{const{isModalOpen:c,showModal:m,ModalComponent:I}=ke();return e.jsxs(ce,{children:[e.jsx(I,{InnerComponent:Ne,modalPadding:"0px",modalPosition:Ze.ON_ROOT}),e.jsx(v,{children:e.jsx(q,{src:Fe,width:"276px",height:"202px"})}),e.jsxs(v,{gap:"24px",children:[e.jsxs(v,{gap:"12px",children:[e.jsxs(v,{children:[e.jsx(Ue,{fontSize:"34px",fontWeight:"500",color:x.snapPrimaryText,letterSpacing:"normal",children:"Push Snap"}),e.jsx(D,{fontSize:"12px",fontWeight:"400",color:x.modalIconColor,children:"powered by MetaMask"})]}),e.jsx(v,{children:e.jsx(D,{fontSize:"14px",fontWeight:"400",color:x.snapSecondaryText,children:"You\u2019re about to install Push Snap which allows you to receive notifications from Push directly on MetaMask!"})})]}),b&&e.jsx(Pe,{variant:"error",heading:b,showIcon:!0}),e.jsxs(J,{gap:"spacing-sm",display:"flex",flexDirection:"column",width:"-webkit-fill-available",children:[g&&!s?e.jsx(W,{type:K.SEAMLESS,spinnerSize:44}):e.jsx(V,{disabled:!!s,onClick:()=>U(),variant:"primary",size:"large",children:s?"Step 1: Completed":"Step 1: Install Snap"}),g&&s?e.jsx(W,{type:K.SEAMLESS,spinnerSize:44}):e.jsx(V,{disabled:!s,onClick:()=>U(),trailingIcon:e.jsx(Q,{height:24,width:24}),children:"Step 2: Sign In with Metamask"})]}),e.jsxs(de,{gap:"7px",onClick:m,children:[e.jsx(q,{src:Be,width:16}),e.jsx(D,{color:x.modalIconColor,fontSize:"14px",fontWeight:"400",children:"About this Snap"})]})]})]})};return e.jsx(e.Fragment,{children:_?e.jsx(e.Fragment,{children:e.jsxs(J,{display:"flex",flexDirection:"column",padding:"spacing-none spacing-none spacing-none spacing-xxs",children:[e.jsx(Ae,{variant:"h4-semibold",children:"Push Snap Settings"}),e.jsx(De,{snoozeDuration:E,setSnoozeDuration:i})]})}):e.jsx(be,{})})},ce=n(Ce)`
+  width: 438px;
+  height: auto;
+  border-radius: 32px;
+  background: #fff;
+  background: ${t=>t.theme.default.bg};
+  margin: auto;
+  @media ${p.mobileL} {
+    width: 330px;
+  }
+`,de=n(Ie)`
+  cursor: pointer;
+`;function he(){var s,h;const{account:t,chainId:o}=O(),{userPushSDKInstance:r}=A(a=>a.user),{subscriptionStatus:f,userSettings:x}=A(a=>a.channels),[g,w]=d.useState(0),[_,y]=d.useState([]),[T,j]=d.useState(!0);H();const b=z(),L=async a=>{const l=await r.channel.info(a);if(l)return{channel:a,id:l.id,icon:l.icon,name:l.name,channel_settings:l.channel_settings}},E=async a=>{const l=[];await Promise.all(Object.keys(a).map(async S=>{const M=await L(S);M&&l.push(M)})),y(l)};d.useEffect(()=>{!t||!r||async function(){if(j(!0),Object.keys(f).length===0){const a=await r.notification.subscriptions(),l={},S={};a.map(({channel:M,user_settings:C})=>{l[M]=!0,S[M]=C?JSON.parse(C):null}),b(R(l)),b(G(S)),await E(l)}else await E(f);j(!1)}()},[t,r]);const i=[{value:0,label:"Notification Settings",title:"Notification Settings"},{value:1,label:"Push Snap",title:""}];return e.jsxs(pe,{children:[e.jsx(xe,{children:"Settings"}),e.jsx(ge,{children:"Customize your Push profile or manage your notification preferences"}),e.jsxs(ue,{children:[e.jsx(fe,{children:i.map(a=>e.jsx(Le,{onClick:()=>w(a.value),isSelected:a.value===g,children:a.label},a.value))}),e.jsx(me,{children:e.jsxs(Se,{children:[((s=i[g])==null?void 0:s.title)&&e.jsx(we,{children:(h=i[g])==null?void 0:h.title}),g===0&&e.jsx(te,{}),g===1&&e.jsx(oe,{})]})})]})]})}const pe=n.div`
+  padding: 32px 24px;
+  flex: 1;
+
+  @media ${p.tablet} {
+    padding: 24px 12px;
+  }
+`,xe=n.div`
+  font-size: 32px;
+  font-weight: 500;
+  line-height: 45px;
+  letter-spacing: normal;
+  text-align: left;
+  color: ${t=>t.theme.default.color};
+
+  @media ${p.tablet} {
+    text-align: center;
+    margin-top: 24px;
+  }
+`,ge=n.div`
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 21px;
+  letter-spacing: normal;
+  text-align: left;
+  color: ${t=>t.theme.default.secondaryColor};
+  margin-bottom: 40px;
+
+  @media ${p.tablet} {
+    text-align: center;
+    margin-bottom: 8px;
+  }
+`,ue=n.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  @media ${p.tablet} {
+    flex-direction: column;
+  }
+`,fe=n.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 42px;
+
+  @media ${p.tablet} {
+    margin-right: 0px;
+    flex-direction: row;
+    overflow-x: scroll;
+    justify-content: center;
+  }
+`,Le=n(He)`
+  background-color: ${t=>t.isSelected?t.theme.default.secondaryBg:"transparent"};
+  color: ${t=>t.theme.default.secondaryColor};
+  border-radius: 12px;
+  width: 100%;
+  padding: 14px;
+  margin: 10px 0px;
+  justify-content: flex-start;
+
+  &:hover:after {
+    background-color: ${t=>t.theme.default.secondaryBg};
+  }
+
+  @media ${p.tablet} {
+    padding: 12px;
+    max-width: fit-content;
+  }
+`,me=n.div`
+  border: 1px solid ${t=>t.theme.default.borderColor};
+  padding: 12px;
+  border-radius: 16px;
+  flex-grow: 1;
+
+  @media ${p.tablet} {
+    margin: 8px 0px;
+    padding: 12px 6px;
+  }
+`,Se=n.div`
+  overflow: hidden;
+  overflow-y: scroll;
+  height: 55vh;
+  padding: 12px;
+
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+    position: absolute;
+    right: 10px;
+  }
+
+  &::-webkit-scrollbar {
+    background-color: transparent;
+    width: 4px;
+    position: absolute;
+    right: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #d53a94;
+    border-radius: 99px;
+    width: 4px;
+    position: absolute;
+    right: 10px;
+  }
+
+  @media ${p.tablet} {
+    margin: 8px 0px;
+    padding: 12px 6px;
+  }
+`,we=n.div`
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 33px;
+  letter-spacing: normal;
+  text-align: left;
+  margin-bottom: 20px;
+  color: ${t=>t.theme.default.color};
+
+  @media ${p.tablet} {
+    text-align: center;
+  }
+`;n.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 12px 0px;
+`,n(P)`
+  border: 1px solid ${t=>t.theme.default.borderColor};
+  border-radius: 8px;
+  overflow: hidden;
+  margin-right: 16px;
+  width: 28px;
+  height: 28px;
+`,n.span`
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 23px;
+  letter-spacing: normal;
+  color: ${t=>t.theme.default.color};
+`,n.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`,n.span`
+  background-color: ${t=>t.theme.default.borderColor};
+  width: 100%;
+  display: flex;
+  height: 1px;
+`,n(B)`
+  background: transparent;
+  display: flex;
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  padding: 0px;
+  position: relative;
+  width: 24px;
+  height: 24px;
+  color: ${t=>t.theme.default.color};
+`,n.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80%;
+`;let N,Z,F;N=()=>e.jsx(Z,{children:e.jsx(he,{})}),Z=n.div`
+	align-items: stretch;
+	align-self: stretch;
+  flex: 1;
+	background: ${t=>t.theme.default.bg};
+	border-radius: ${u.ADJUSTMENTS.RADIUS.LARGE};
+	box-shadow: ${u.ADJUSTMENTS.MODULE_BOX_SHADOW};
+	display: flex;
+	flex-direction: column;
+	flex: initial;
+	justify-content: center;
+	position: relative;
+  overflow: hidden;
+  box-sizing: border-box;
+
+  margin: ${u.ADJUSTMENTS.MARGIN.MINI_MODULES.DESKTOP};
+  height: calc(100vh - ${u.CONSTANTS.HEADER_HEIGHT}px - ${$.MINI_MODULES.DESKTOP.TOP} - ${$.MINI_MODULES.DESKTOP.BOTTOM});
+  
+  @media ${p.laptop} {
+    margin: ${u.ADJUSTMENTS.MARGIN.MINI_MODULES.TABLET};
+    height: calc(100vh - ${u.CONSTANTS.HEADER_HEIGHT}px - ${$.MINI_MODULES.TABLET.TOP} - ${$.MINI_MODULES.TABLET.BOTTOM});
+  }
+
+  @media ${p.mobileL} {
+    margin: ${u.ADJUSTMENTS.MARGIN.MINI_MODULES.MOBILE};
+    height: calc(100vh - ${u.CONSTANTS.HEADER_HEIGHT}px - ${$.MINI_MODULES.MOBILE.TOP});
+    border: ${u.ADJUSTMENTS.RADIUS.LARGE};
+    border-radius: ${u.ADJUSTMENTS.RADIUS.LARGE} ${u.ADJUSTMENTS.RADIUS.LARGE} 0 0;
+`,X=()=>e.jsx(F,{children:e.jsx(N,{})}),F=n(ze)`
+  flex: 1;
+  flex-direction: column;
+  align-self: stretch;
+`});export{Qe as __tla,X as default};
