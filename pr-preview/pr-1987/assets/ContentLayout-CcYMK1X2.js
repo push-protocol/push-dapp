63b21d5f3461f86f75e1a824b95f520b62f6d451
abhishek-01k <abhishek-01k@users.noreply.github.com>
2024-12-21 05:38:48 +0000

    Deploy preview for PR 1987 🛫

--- pr-preview/pr-1987/assets/ContentLayout-CcYMK1X2.js
+++
@@ -1,7 +0,0 @@
-import{d_ as e,d$ as r,e3 as i,__tla as n}from"./index-DHs9Pj2c.js";let t,c=Promise.all([(()=>{try{return n}catch{}})()]).then(async()=>{t=({children:a,variant:l})=>l==="verticalStretch"?e.jsx(r,{alignSelf:"center",backgroundColor:"surface-primary",borderRadius:"radius-lg",display:"flex",flexDirection:"column",height:"calc(100vh - 76px)",overflow:"hidden",maxWidth:"1200px",width:{initial:"calc(100% - (var(--spacing-sm) * 2))",ml:"-webkit-fill-available"},css:i`
-          border-bottom-right-radius: var(--radius-none);
-          border-bottom-left-radius: var(--radius-none);
-        `,children:a}):e.jsx(r,{alignItems:"center",alignSelf:"center",backgroundColor:"surface-transparent",display:"flex",flexDirection:"column",justifyContent:"center",maxWidth:"1200px",width:"calc(100% - (var(--spacing-sm) * 2))",css:i`
-        flex: initial;
-        margin: 0 0 auto 0;
-      `,children:a})});export{t as C,c as __tla};
