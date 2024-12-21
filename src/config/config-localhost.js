a318f39264b64fbdaccad49555558f000169a6c7
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-12-21 21:50:26 +0530

    telegram congif fixes for staging, localhost and alpha env

--- src/config/config-localhost.js
+++ src/config/config-localhost.js
@@ -68,6 +68,10 @@ export const config = {
     extension: 'https://chrome.google.com/webstore/detail/epns-staging-protocol-alp/bjiennpmhdcandkpigcploafccldlakj',
     howto: 'https://push.org/docs',
   },
+
+  // social media integration
+  telegramExternalURL: 'https://web.telegram.org/k/#@PushCommDevBot',
+  discordExternalURL: null,
 };
 
 /**
--- src/config/config-staging.js
+++ src/config/config-staging.js
@@ -85,7 +85,7 @@ export const config = {
   },
 
   // social media integration
-  telegramExternalURL: 'https://t.me/PushCommDevBot',
+  telegramExternalURL: 'https://web.telegram.org/k/#@PushCommDevBot',
   discordExternalURL: null,
 };
 
