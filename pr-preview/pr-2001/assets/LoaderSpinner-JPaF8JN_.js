76688fb48a8f0b962fc2b100b106786faeafee3b
abhishek-01k <abhishek-01k@users.noreply.github.com>
2025-01-20 05:46:07 +0000

    Deploy preview for PR 2001 🛫

--- pr-preview/pr-2001/assets/LoaderSpinner-JPaF8JN_.js
+++
@@ -1,1 +0,0 @@
-import{d_ as e,es as s,__tla as r}from"./index-BNFbPSez.js";let a,l=Promise.all([(()=>{try{return r}catch{}})()]).then(async()=>{let t;t={height:"45px"},a=()=>e.jsx(s,{color:"#CF1C84",height:9,width:2.5,margin:0,css:t})});export{a as L,l as __tla};
