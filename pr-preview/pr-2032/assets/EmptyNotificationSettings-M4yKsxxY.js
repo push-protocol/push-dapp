abee44e54092647bd0bda63dd32cff75843e1cf5
corlard3y <corlard3y@users.noreply.github.com>
2025-03-21 18:32:15 +0000

    Deploy preview for PR 2032 🛫

---
+++ pr-preview/pr-2032/assets/EmptyNotificationSettings-M4yKsxxY.js
@@ -0,0 +1,23 @@
+import{fm as u,d_ as e,ep as l,fn as j,e9 as a,fo as k,fp as v,__tla as C}from"./index-CY4ytu15.js";let d,h,x,y=Promise.all([(()=>{try{return C}catch{}})()]).then(async()=>{function p(t){return u({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"circle",attr:{cx:"12",cy:"12",r:"3"}},{tag:"path",attr:{d:"M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"}}]})(t)}let i,r,n,s,c;d=({onClick:t})=>e.jsx(l,{leadingIcon:e.jsx(j,{}),onClick:t,size:"small",variant:"tertiary",children:"Add Delegate"}),x=({onClick:t})=>e.jsx(l,{leadingIcon:e.jsx(p,{}),onClick:t,size:"small",variant:"tertiary",children:"Manage Settings"}),i=({onClick:t,title:o})=>e.jsx(l,{variant:"outline",size:"small",onClick:t,children:o||"Modify Settings"}),h=({description:t,onClick:o,title:g,buttonTitle:m,showTopBorder:f=!0})=>e.jsxs(r,{showTopBorder:f,children:[e.jsx(c,{src:v,alt:"No Settings",width:"24px",height:"24px"}),e.jsx(n,{children:g}),e.jsx(s,{children:t}),e.jsx(i,{onClick:o,title:m})]}),r=a.div`
+  border-top: ${t=>t.showTopBorder&&`1px solid ${t.theme.default.borderColor}`};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 16px;
+`,n=a.div`
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: normal;
+  text-align: left;
+  color: ${t=>t.theme.default.color};
+`,s=a.div`
+  margin-top: 1px;
+  margin-bottom: 16px;
+  color: ${t=>t.theme.default.secondaryColor};
+`,c=a(k)`
+  color: ${t=>t.theme.default.color};
+  margin-top: 32px;
+  margin-bottom: 12px;
+`});export{d as A,h as E,x as M,y as __tla};
