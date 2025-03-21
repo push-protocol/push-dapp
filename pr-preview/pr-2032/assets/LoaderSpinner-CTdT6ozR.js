d24608e12ce01acc7926fdf1c29503428a322d91
corlard3y <corlard3y@users.noreply.github.com>
2025-03-21 15:59:32 +0000

    Deploy preview for PR 2032 🛫

---
+++ pr-preview/pr-2032/assets/LoaderSpinner-CTdT6ozR.js
@@ -0,0 +1,1 @@
+import{d_ as e,ew as r,__tla as s}from"./index-SVBZTWc1.js";let a,l=Promise.all([(()=>{try{return s}catch{}})()]).then(async()=>{let t;t={height:"45px"},a=()=>e.jsx(r,{color:"#CF1C84",height:9,width:2.5,margin:0,css:t})});export{a as L,l as __tla};
